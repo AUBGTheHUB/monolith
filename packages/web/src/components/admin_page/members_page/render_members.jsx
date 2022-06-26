f66a6f81f5b07502ea5a9febbf80f2227ef04481
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-06-26 13:50:50 +0300

    Remove redundant memberid input (made it automatic)

--- packages/web/src/components/admin_page/members_page/render_members.jsx
+++ packages/web/src/components/admin_page/members_page/render_members.jsx
@@ -5,64 +5,69 @@ import { useNavigate } from "react-router-dom";
 const RenderMembers = (members) => {
   // console.log(members.members[0]["firstname"]);
   const history = useNavigate();
+  // console.log(members.members[members.members.length-1]["memberid"]);
   return (
-  <div className="members-box-add-button">
-    <Button variant="primary" onClick={()=>{
-      history("/admin/dashboard/members/add")
-    }}>Add Member</Button>
-    <div className="members-box">
-      {members.members.map((person, index) => (
-        <Card style={{ width: "18rem" }} key={index} className="member-card">
-          <Card.Img
-            variant="top"
-            src={members.members[index]["profilepicture"]}
-            onClick={() => {
-              console.log(members.members[index]["profilepicture"]);
-            }}
-          />
-          <Card.Body>
-            <Card.Title>
-              {members.members[index]["firstname"] +
-                " " +
-                members.members[index]["lastname"] +
-                " (id: " +
-                members.members[index]["memberid"] +
-                ")"}
-            </Card.Title>
-            <Card.Text>
-              {"Position: " + members.members[index]["position"]}
-            </Card.Text>
-            <Card.Text>
-              {"Department: " + members.members[index]["department"]}
-            </Card.Text>
-            <Button
-              variant="primary"
-              onClick={() => {
-                window.open(members.members[index]["sociallink"]);
-              }}
-              className="linkedin-button"
-            >
-              LinkedIn
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => {
-                history("/admin/dashboard/members/actions", {
-                  state: {
-                    member_data: members.members[index],
-                    memberid: members.members[index]["memberid"]
-                  }
-                })
-                console.log(members.members[index]["memberid"]);
-              }}
-            >
-              Actions
-            </Button>
-          </Card.Body>
-        </Card>
-      ))}
+    <div className="members-box-add-button">
+      <Button
+        variant="primary"
+        onClick={() => {
+          history("/admin/dashboard/members/add", {
+            state: {
+              new_member_id:
+                members.members[members.members.length - 1]["memberid"],
+            },
+          });
+        }}
+      >
+        Add Member
+      </Button>
+      <div className="members-box">
+        {members.members.map((person, index) => (
+          <Card style={{ width: "18rem" }} key={index} className="member-card">
+            <Card.Img
+              variant="top"
+              src={members.members[index]["profilepicture"]}
+            />
+            <Card.Body>
+              <Card.Title>
+                {members.members[index]["firstname"] +
+                  " " +
+                  members.members[index]["lastname"]}
+              </Card.Title>
+              <Card.Text>
+                {"Position: " + members.members[index]["position"]}
+              </Card.Text>
+              <Card.Text>
+                {"Department: " + members.members[index]["department"]}
+              </Card.Text>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  window.open(members.members[index]["sociallink"]);
+                }}
+                className="linkedin-button"
+              >
+                LinkedIn
+              </Button>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  history("/admin/dashboard/members/actions", {
+                    state: {
+                      member_data: members.members[index],
+                      memberid: members.members[index]["memberid"],
+                    },
+                  });
+                  console.log(members.members[index]["memberid"]);
+                }}
+              >
+                Actions
+              </Button>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
     </div>
-  </div>
   );
   // return <h1>Hello, {members.members[0]["firstname"]}!</h1>
 };
