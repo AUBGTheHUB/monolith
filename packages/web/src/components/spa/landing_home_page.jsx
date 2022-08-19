eb0c48f059a328a8156e710066d0a72513de7290
michaelbozhilov@gmail.com <michaelbozhilov@gmail.com>
2022-08-20 02:12:22 +0300

    #22 final test

--- packages/web/src/components/spa/landing_home_page.jsx
+++ packages/web/src/components/spa/landing_home_page.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const LandingHome = () => {
   return (
     <div>
-      <h1>This is the home page of the new website</h1>
+      <h1>HOME PAGE</h1>
     </div>
   );
 };
