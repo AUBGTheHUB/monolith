7fbc2fe21e854793dd307ff34da324613545ee39
michaelbozhilov@gmail.com <michaelbozhilov@gmail.com>
2022-08-22 00:16:03 +0300

    #11 check auth and add loading page w gif

--- packages/web/src/components/admin_page/landing_admin_page.jsx
+++ packages/web/src/components/admin_page/landing_admin_page.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
-import { Form, Button } from 'react-bootstrap';
-import './admin.css';
-import { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { url } from '../../Global';
+import React from "react";
+import { Form, Button } from "react-bootstrap";
+import "./admin.css";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { url } from "../../Global";
+import Validate from "../../Global";
+import LoadingPage from "../other/loading_page";
 
 const LandingAdminPage = () => {
   const history = useNavigate();
 
   const [formState, setFormState] = useState({
-    username: '',
-    password: ''
+    username: "",
+    password: "",
   });
 
   const handleInputChange = (e) => {
@@ -21,57 +23,68 @@ const LandingAdminPage = () => {
 
     setFormState({
       ...formState,
-      [name]: value
+      [name]: value,
     });
   };
 
   const handleSubmit = () => {
     axios({
-      method: 'post',
-      url: url + '/api/login',
+      method: "post",
+      url: url + "/api/login",
       headers: {},
-      data: { ...formState }
+      data: { ...formState },
     })
       .then((res) => {
-        localStorage.setItem('auth_token', res.data.data.auth_token);
-        console.log('Logged in!');
-        history('/admin/dashboard');
+        localStorage.setItem("auth_token", res.data.data.auth_token);
+        console.log("Logged in!");
+        history("/admin/dashboard");
       })
       // eslint-disable-next-line no-unused-vars
       .catch((err) => {
-        console.log('Error logging in');
+        console.log("Error logging in");
       });
   };
 
-  return (
-    <div className="login-page">
-      <h3>Welcome to the Admin Page</h3>
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>User</Form.Label>
-          <Form.Control
-            type="text"
-            name="username"
-            placeholder="Enter default user"
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control name="password" type="password" onChange={handleInputChange} />
-        </Form.Group>
-        <Button
-          variant="primary"
-          type="button"
-          onClick={() => {
-            handleSubmit();
-          }}
-        >
-          Submit
-        </Button>
-      </Form>
-    </div>
-  );
+  if (Validate()) {
+    setTimeout(() => {
+      history("/admin/dashboard");
+    }, 2000);
+    return <LoadingPage />;
+  } else {
+    return (
+      <div className="login-page">
+        <h3>Welcome to the Admin Page</h3>
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>User</Form.Label>
+            <Form.Control
+              type="text"
+              name="username"
+              placeholder="Enter default user"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              name="password"
+              type="password"
+              onChange={handleInputChange}
+            />
+          </Form.Group>
+          <Button
+            variant="primary"
+            type="button"
+            onClick={() => {
+              handleSubmit();
+            }}
+          >
+            Submit
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 };
 
 export default LandingAdminPage;
