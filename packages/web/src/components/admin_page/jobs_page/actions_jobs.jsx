a2baa8aaf1465f472e16837db7c49bdefd177d6f
michaelbozhilov@gmail.com <michaelbozhilov@gmail.com>
2022-08-21 22:37:27 +0300

    #9 add validation to actions_job

--- packages/web/src/components/admin_page/jobs_page/actions_jobs.jsx
+++ packages/web/src/components/admin_page/jobs_page/actions_jobs.jsx
@@ -1,8 +1,9 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import { React, useState } from "react";
 import axios from "axios";
-import { url } from "../../../Global";
+import Validate, { url } from "../../../Global";
 import { Card, Form, Button } from "react-bootstrap";
+import InvalidClient from "../invalid_client";
 
 const JobActions = () => {
   const location = useLocation();
@@ -61,100 +62,104 @@ const JobActions = () => {
       });
   };
 
-  return (
-    <div className="actions-single-member">
-      <div className="single-member-box">
-        <Card style={{ width: "18rem" }} className="member-card">
-          <Card.Img variant="top" src={job_data["logo"]} />
-          <Card.Body>
-            <Card.Title>{job_data["position"]}</Card.Title>
-            <Card.Text>{"Company: " + job_data["company"]}</Card.Text>
-            <Card.Text>{job_data["description"]}</Card.Text>
+  if (Validate()) {
+    return (
+      <div className="actions-single-member">
+        <div className="single-member-box">
+          <Card style={{ width: "18rem" }} className="member-card">
+            <Card.Img variant="top" src={job_data["logo"]} />
+            <Card.Body>
+              <Card.Title>{job_data["position"]}</Card.Title>
+              <Card.Text>{"Company: " + job_data["company"]}</Card.Text>
+              <Card.Text>{job_data["description"]}</Card.Text>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  window.open(job_data["link"]);
+                }}
+                className="linkedin-button"
+              >
+                LinkedIn
+              </Button>
+              <Button
+                variant="primary"
+                onClick={() => {
+                  remove_job();
+                }}
+              >
+                Remove
+              </Button>
+            </Card.Body>
+          </Card>
+        </div>
+
+        <div className="member-form-edit">
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Position</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="position"
+                name="position"
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Company</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="company"
+                name="company"
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="description"
+                name="description"
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Logo</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="logo"
+                name="logo"
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicText">
+              <Form.Label>Link</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="link"
+                name="link"
+                onChange={handleInputChange}
+              />
+            </Form.Group>
             <Button
               variant="primary"
+              type="button"
               onClick={() => {
-                window.open(job_data["link"]);
-              }}
-              className="linkedin-button"
-            >
-              LinkedIn
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => {
-                remove_job();
+                edit_job();
               }}
             >
-              Remove
+              Edit job
             </Button>
-          </Card.Body>
-        </Card>
+          </Form>
+        </div>
       </div>
-
-      <div className="member-form-edit">
-        <Form>
-          <Form.Group className="mb-3" controlId="formBasicText">
-            <Form.Label>Position</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="position"
-              name="position"
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicText">
-            <Form.Label>Company</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="company"
-              name="company"
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicText">
-            <Form.Label>Description</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="description"
-              name="description"
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicText">
-            <Form.Label>Logo</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="logo"
-              name="logo"
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-
-          <Form.Group className="mb-3" controlId="formBasicText">
-            <Form.Label>Link</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="link"
-              name="link"
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-          <Button
-            variant="primary"
-            type="button"
-            onClick={() => {
-              edit_job();
-            }}
-          >
-            Edit job
-          </Button>
-        </Form>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return <InvalidClient />;
+  }
 };
 
 export default JobActions;
