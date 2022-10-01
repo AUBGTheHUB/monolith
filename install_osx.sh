3b7ebb194d30add6d7866b18eedd30e7b3c90304
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-10-01 22:32:45 +0300

    #37 update tabs

--- install_osx.sh
+++ install_osx.sh
@@ -18,19 +18,18 @@ if [ "$1" != "--post" ]; then
 	fi
  
 	sudo installer -verbose -pkg $HOME/Downloads/go.pkg -target $HOME
-    echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
+    	echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.zshrc
 
-    cd /usr/local/go/src
+    	cd /usr/local/go/src
     
 	git clone git@github.com:AUBGTheHUB/spa-website-2022.git
 
-    cd spa-website-2022
+    	cd spa-website-2022
 
-    exec $SHELL
+    	exec $SHELL
  
 else
-	nvm install --lts
-
+    	nvm install --lts
 	cd /usr/local/go/src/spa-website-2022
 
 	make install-hooks 
