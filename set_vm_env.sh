88afae77929a87462d85947eba2c67d412f69f50
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-10-08 00:43:36 +0300

    #45 add env setter script

---
+++ set_vm_env.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt install docker-compose
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+
+
+wget -O go.tar.gz https://go.dev/dl/go1.19.1.linux-amd64.tar.gz
+
+rm -rf /usr/local/go && tar -C $HOME -xzf go.tar.gz
+rm -rf go.tar.gz
+
+echo "export PATH=\$PATH:/$HOME/go/bin" >> ~/.bashrc
+
+git clone git@github.com:AUBGTheHUB/spa-website-2022.git $HOME/go/src/spa-website-2022
+
+exec $SHELL
