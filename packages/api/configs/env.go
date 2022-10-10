e681e08b34f9f694cfc125f6f7f0b5547945027f
Ivan <ivanobreshkov12@gmail.com>
2022-10-10 13:33:25 +0300

    #53 init structure for gen

--- packages/api/configs/env.go
+++ packages/api/configs/env.go
@@ -7,6 +7,22 @@ import (
 	"github.com/joho/godotenv"
 )
 
+var token string = "hubababuba"
+
+func GenerateToken() string{
+	random_string := "TOKEN"
+	// implement logic for generating a rand 20 char string
+	return random_string
+}
+
+func SetToken() string{
+	token = 
+}
+
+func ReturnAuthToken() string {
+	return token
+}
+
 func EnvMongoURI() string {
 	err := godotenv.Load()
 	if err != nil {
@@ -16,11 +32,4 @@ func EnvMongoURI() string {
 	return os.Getenv("MONGOURI")
 }
 
-func ReturnAuthToken() string {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
 
-	return os.Getenv("AUTH_TOKEN")
-}
