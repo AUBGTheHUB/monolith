22218ea02462bc736721d9f656876e5f32cd0d7e
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-11-16 21:30:03 +0200

    #71 initial tls config

--- packages/api/main.go
+++ packages/api/main.go
@@ -5,6 +5,8 @@ import (
 	"hub-backend/routes"
 	"log"
 
+	"crypto/tls"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 	//fmt, encoding/json, strconv
@@ -25,5 +27,22 @@ func main() {
 	routes.SponsorsRoute(app)
 	routes.PartnersRoute(app)
 	configs.RunCronJobs()
-	log.Fatal(app.Listen(":8000"))
+
+	// ln -s ../../data/certs ./certs
+
+	cer, err := tls.LoadX509KeyPair("certs/devenv.crt", "certs/devenv.key")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	config := &tls.Config{Certificates: []tls.Certificate{cer}}
+
+	// Create custom listener
+	ln, err := tls.Listen("tcp", ":8000", config)
+	if err != nil {
+		panic(err)
+	}
+
+	// Start server with https/ssl enabled on http://localhost:443
+	log.Fatal(app.Listener(ln))
 }
