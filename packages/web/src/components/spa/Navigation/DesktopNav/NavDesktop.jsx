574f4640606d059eefce7ca8815d6664cb31db80
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-11-26 15:39:07 +0200

    Created Navbar for desktop with logo  and anchor links

--- packages/web/src/components/spa/Navigation/DesktopNav/NavDesktop.jsx
+++ packages/web/src/components/spa/Navigation/DesktopNav/NavDesktop.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-// import "desktop_navbar.css";
+import "./desktop_navbar.css";
 
 export const NavDesktop = () => {
     return (
-        <div className="navbar-container">
-            <h1>Desktop</h1>
-            <div className="title-logo-container">
-                <img src="hublogo.png" alt="Hub logo" />
+        <div className="navdesktop-container">
+            <div className="navdesktop-logo">
+                <img src="hublogo.png" className="navdesktop-logo-image" alt="The Hub AUBG" />
+                <p>The HUB</p>
             </div>
-            <div className="nav-buttons">
-
+            <div className="navdesktop-buttons">
+                <a href= "#about">About</a>
+                <a href= "#events">Events</a>
+                <a href= "#articles">Articles</a>
+                <a href= "#team">Team</a>
+                <a href= "#jobs">Jobs</a>
+                {/* <div>Link for the hackathon</div> */}
+                <button className="button" type="button" onClick={() => {
+                                }} >Hack AUBG</button>
             </div>
         </div>
         
