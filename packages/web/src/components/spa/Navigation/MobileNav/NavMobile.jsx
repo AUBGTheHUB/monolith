47ce8e712756f01ddded8c5d9c03e86ca5811cdd
Aleks <al.ivanov1908@gmail.com>
2022-11-27 18:18:24 +0200

    #82 changed X button and aligned it also aligned hamburger and logo

--- packages/web/src/components/spa/Navigation/MobileNav/NavMobile.jsx
+++ packages/web/src/components/spa/Navigation/MobileNav/NavMobile.jsx
@@ -2,8 +2,9 @@ import { Button } from './Button';
 import React from 'react';
 import './style.css';
 // import { GiHamburgerMenu } from 'react-icons/gi';
-import { IoIosClose } from 'react-icons/io';
+// import { IoIosClose } from 'react-icons/io';
 import { CgMenu } from 'react-icons/cg';
+import { MdOutlineClose } from 'react-icons/md';
 
 import { useState } from 'react';
 export const NavMobile = () => {
@@ -46,7 +47,7 @@ export const NavMobile = () => {
                     props={{
                         css: 'navmobile-button',
                         icon: (
-                            <IoIosClose
+                            <MdOutlineClose
                                 className={closeButton}
                                 onClick={() => {
                                     setMenuClass('navmobile-menu-backwards');
