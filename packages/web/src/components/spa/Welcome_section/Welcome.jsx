5c04677d5461416ad86fddf097929d1c2a9ec07e
SuelaCark <77395265+SuelaCark@users.noreply.github.com>
2022-12-10 17:34:12 +0200

    #101 Created welcome_section folder and added jsx and css files (#115)

---
+++ packages/web/src/components/spa/Welcome_section/Welcome.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './welcome_section.css';
+
+export const Welcome = () => {
+    return (
+        <div className="welcome-container">
+            <div className="welcome-text">
+                <h1>Welcome to the Hub!</h1>
+            </div>
+            <div className="hubzzie-image">
+                <p>Hubbzie</p>
+            </div>
+        </div>
+    );
+};
