5fc238cf03def8dcd47944a5a2ea869ccc1b6c42
SuelaCark <suela.qarkaxhija@gmail.com>
2022-12-10 20:05:29 +0200

    #101 Formatted Welcome text, added button and hubzzie image

--- packages/web/src/components/spa/Welcome_section/Welcome.jsx
+++ packages/web/src/components/spa/Welcome_section/Welcome.jsx
@@ -5,10 +5,27 @@ export const Welcome = () => {
     return (
         <div className="welcome-container">
             <div className="welcome-text">
-                <h1>Welcome to the Hub!</h1>
+                <h1>Welcome to The Hub!</h1>
+                <p>
+                    The IT and innovations club at the American University in
+                    Bulgaria.
+                </p>
+                <button
+                    className="aboutUs-btn"
+                    type="button"
+                    onClick={() => {
+                        location.href = '#about';
+                    }}
+                >
+                    About Us
+                </button>
             </div>
-            <div className="hubzzie-image">
-                <p>Hubbzie image</p>
+            <div className="hubzzie">
+                <img
+                    src="hubzzie.png"
+                    className="hubzzie-image"
+                    alt="Hubzzie"
+                />
             </div>
         </div>
     );
