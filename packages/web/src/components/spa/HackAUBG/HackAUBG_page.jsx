ca066a10d494525f0521bc29c9b36b83536e32fa
SuelaCark <suela.qarkaxhija@gmail.com>
2022-12-14 15:26:48 +0200

    #125 initial matrix animation

---
+++ packages/web/src/components/spa/HackAUBG/HackAUBG_page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './hackAUBG.css';
+
+export const HackAUBG_page = () => {
+    return (
+        <div className="main-container">
+            <div className="header-container"></div>
+        </div>
+    );
+};
