94b12f360899c9f6fface669bbea22a8a65bdea4
michaelbozhilov@gmail.com <michaelbozhilov@gmail.com>
2022-12-19 01:08:02 +0200

    #104 add edge case func

--- packages/web/src/components/spa/Members/Carousel.jsx
+++ packages/web/src/components/spa/Members/Carousel.jsx
@@ -8,8 +8,6 @@ export const Carousel = ({ props }) => {
     if (props && props.length != 0) {
         const [sliceIndex, setSliceIndex] = useState(0);
 
-        console.log(`Props len ${props.length}`);
-
         const [firstSlide, setFirstSlide] = useState(
             'carousel-container-slider'
         );
@@ -21,19 +19,49 @@ export const Carousel = ({ props }) => {
         const [firstMap, setFirstMap] = useState(
             props.slice(sliceIndex, sliceIndex + 3)
         );
-        console.log(
-            '🚀 ~ file: Carousel.jsx:24 ~ Carousel ~ firstMap',
-            firstMap
-        );
+
+        // console.log(
+        //     '🚀 ~ file: Carousel.jsx:24 ~ Carousel ~ firstMap',
+        //     firstMap
+        // );
 
         const [secondMap, setSecondMap] = useState(
             props.slice(sliceIndex + 3, sliceIndex + 6)
         );
 
-        console.log(
-            '🚀 ~ file: Carousel.jsx:29 ~ Carousel ~ secondMap',
-            secondMap
-        );
+        // console.log(
+        //     '🚀 ~ file: Carousel.jsx:29 ~ Carousel ~ secondMap',
+        //     secondMap
+        // );
+
+        const increaseSlice = (currentPosIndex, setter) => {
+            console.log(
+                '🚀 ~ file: Carousel.jsx:40 ~ increaseSlice ~ currentPosIndex',
+                currentPosIndex
+            );
+
+            if (currentPosIndex + 6 === props.length) {
+                setter(
+                    props
+                        .slice(currentPosIndex + 3, currentPosIndex + 5)
+                        .append(props[0])
+                );
+                return 1;
+            } else if (currentPosIndex + 5 === props.length) {
+                setter(
+                    props
+                        .slice(currentPosIndex + 3, currentPosIndex + 4)
+                        .concat(props.slice(0, 2))
+                );
+                return 2;
+            } else if (currentPosIndex + 4 === props.length) {
+                setter(props.slice(0, 3));
+                return 3;
+            } else {
+                setter(props.slice(currentPosIndex + 3, currentPosIndex + 6));
+                return currentPosIndex + 6;
+            }
+        };
 
         const slide = (className) => {
             setFirstSlide('carousel-container-slider left');
@@ -41,12 +69,15 @@ export const Carousel = ({ props }) => {
 
             setTimeout(() => {
                 try {
-                    setSliceIndex(sliceIndex + 3);
-                    setFirstMap(props.slice(sliceIndex + 3, sliceIndex + 6));
-                    setSecondMap(props.slice(sliceIndex + 6, sliceIndex + 9));
+                    setSliceIndex(
+                        increaseSlice(
+                            increaseSlice(sliceIndex, setFirstMap),
+                            setSecondMap
+                        )
+                    );
+
                     setFirstSlide('carousel-container-slider');
                     setSecondSlide('carousel-container-slider-hidden');
-                    console.log('HERE');
                 } catch (error) {
                     console.log(error);
                 }
