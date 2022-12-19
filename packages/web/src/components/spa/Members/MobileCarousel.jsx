47b8f712ca544d82528e8d4a82aca8f60e856004
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-12-20 01:53:48 +0200

    #104 Add Members Carousel (Mobile Version) (#131)

---
+++ packages/web/src/components/spa/Members/MobileCarousel.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import { useState } from 'react';
+import { MemberCard } from './MemberCard';
+import { BsChevronDoubleRight as ArrowIcon } from 'react-icons/bs';
+
+export const MobileCarousel = ({ props }) => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    const getLeftMember = (current) => {
+        if (currentIndex - 1 < 0) {
+            setCurrentIndex(props.length - 1);
+        } else {
+            setCurrentIndex(currentIndex - 1);
+        }
+    };
+
+    const getRightMember = () => {
+        if (currentIndex + 1 <= props.length - 1) {
+            setCurrentIndex(currentIndex + 1);
+        } else {
+            setCurrentIndex(0);
+        }
+    };
+
+    return (
+        <div className="mobile-carousel-container">
+            <ArrowIcon
+                className="carousel-arrow-left mobile"
+                onClick={getLeftMember}
+            />
+            <div className="mobile-carousel-container-moving">
+                <MemberCard props={props[currentIndex]} />
+            </div>
+            <ArrowIcon
+                className="carousel-arrow-right mobile"
+                onClick={getRightMember}
+            />
+        </div>
+    );
+};
