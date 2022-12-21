37ec9657505b76baadb6fe2f36789dc70a48e8ce
Michael Bozhilov <michaelbozhilov@gmail.com>
2022-12-21 15:15:55 +0200

    #103 Initial structure for Jobs  (#143)

--- packages/web/src/App.js
+++ packages/web/src/App.js
@@ -28,6 +28,7 @@ import RenderPartners from './components/admin_page/hackathon/hackathon_partners
 import AddPartners from './components/admin_page/hackathon/hackathon_partners.jsx/add_partners';
 import PartnersActions from './components/admin_page/hackathon/hackathon_partners.jsx/actions_partners';
 import './App.css';
+import { JobsSection } from './components/spa/JobsSection/JobsSection';
 
 function App() {
     return (
@@ -113,6 +114,7 @@ function App() {
                 path="/admin/dashboard/partners/actions"
                 element={<PartnersActions />}
             />
+            <Route path="/jobs" element={<JobsSection />} />
         </Routes>
     );
 }
