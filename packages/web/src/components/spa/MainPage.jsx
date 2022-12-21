b85cc3646a3dac07ec2d23a5be92f7c3650aed06
michaelbozhilov@gmail.com <michaelbozhilov@gmail.com>
2022-12-21 12:22:29 +0200

    #140 refactor navbar

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -3,11 +3,20 @@ import './main.css';
 import { MembersSection } from './MembersSection/MembersSection';
 import { NavBar } from './Navigation/NavBar';
 import { LandingSection } from './LandingSection/LandingSection';
+import { Anchor, Props } from './Navigation/NavFactory.js';
 
 const LandingHome = () => {
+    const anchorList = [
+        new Anchor('About', '#about'),
+        new Anchor('Events', '#events'),
+        new Anchor('Articles', '#articles'),
+        new Anchor('Team', '#team'),
+        new Anchor('Jobs', 'jobs')
+    ];
+
     return (
         <div className="main">
-            <NavBar />
+            <NavBar props={new Props(anchorList, true)} />
             <LandingSection />
             <MembersSection />
         </div>
