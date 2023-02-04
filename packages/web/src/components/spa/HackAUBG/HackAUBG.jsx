90107109385a5d026010b889442991e5f6c9c5ff
SuelaCark <suela.qarkaxhija@gmail.com>
2023-02-04 21:41:39 +0200

    #180 finished mentors flexbox

--- packages/web/src/components/spa/HackAUBG/HackAUBG.jsx
+++ packages/web/src/components/spa/HackAUBG/HackAUBG.jsx
@@ -6,12 +6,11 @@ import { AboutHackathon } from './AboutHackathon/AboutHackathon';
 import { Anchor, Props } from '../Navigation/NavFactory.js';
 import { NavBar } from '../Navigation/NavBar';
 import { Footer } from '../Footer/Footer';
+import MentorsSection from './MentorsSection/MentorsSection';
 
 export const HackAUBG = () => {
     const anchorList = [
         new Anchor('About', '#AboutSection'),
-        // new Anchor('Events', '#events'),
-        // new Anchor('Articles', '#articles'),
         new Anchor('Schedule', '#team'),
         new Anchor('Grading criteria', 'jobs'),
         new Anchor('FAQ', 'jobs')
@@ -22,6 +21,7 @@ export const HackAUBG = () => {
             <NavBar props={new Props(anchorList, false, 'rgba(0,0,0,.5)')} />
             <MatrixWindow />
             <AboutHackathon />
+            <MentorsSection />
             <Footer colour={'rgb(25, 183, 0)'} />
         </div>
     );
---
+++ packages/web/src/components/spa/HackAUBG/MentorsSection/MentorsSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './mentors_section.css';
+
+export const MentorsSection = () => {
+    return (
+        <div className="mentors-container">
+            <h1 className="header">Mentors</h1>
+            <div className="mentors-picture">
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"></img>
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/2.jpg"></img>
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/3.jpg"></img>
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/4.jpg"></img>
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/5.jpg"></img>
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/6.jpg"></img>
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/7.jpg"></img>
+                <img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/8.jpg"></img>
+            </div>
+        </div>
+    );
+};
+
+export default MentorsSection;
