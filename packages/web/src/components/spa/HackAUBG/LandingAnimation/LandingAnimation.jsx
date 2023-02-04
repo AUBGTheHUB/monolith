a143a298c2bff7eabe34c468b22981190e2aaf80
SuelaCark <suela.qarkaxhija@gmail.com>
2023-02-04 21:17:29 +0200

    #125 fixed text responsiveness

--- packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
+++ packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
@@ -8,7 +8,7 @@ export const MatrixWindow = () => {
         <div className="animation-container">
             <div className="landing-content">
                 <div className="landing-text">
-                    <h1>HackAUBG</h1>
+                    <h1>HackAUBG 5.0</h1>
                     <p>31st March - 2nd April, AUBG - Blagoevgrad</p>
                 </div>
                 <button className="Reg-btn">Go to Registration</button>
