0826508fa0f8bec4ec3b216a6e71a6c662343353
annakukova <akukova12@gmail.com>
2023-02-04 16:55:13 +0200

    #139-Not-found-page-404 add correct hubzie image and text

--- packages/web/src/components/other/NotFound.jsx
+++ packages/web/src/components/other/NotFound.jsx
@@ -1,26 +1,56 @@
 import React from 'react';
 import './page_not_found.css';
-import { NavBar } from '../spa/Navigation/NavBar';
-import { Anchor, Props } from '../spa/Navigation/NavFactory';
+//import './desktop_navbar.css';
+//import { NavBar } from '../spa/Navigation/NavBar';
+// import { Anchor, Props } from '../spa/Navigation/NavFactory';
 
-const anchorList = [
-    new Anchor('About', '/#about'),
-    new Anchor('Events', '/#events'),
-    new Anchor('Articles', '/#articles'),
-    new Anchor('Team', '/#team'),
-    new Anchor('Jobs', '/jobs')
-];
+// const anchorList = [
+//     new Anchor('About', '/#about'),
+//     new Anchor('Events', '/#events'),
+//     new Anchor('Articles', '/#articles'),
+//     new Anchor('Team', '/#team'),
+//     new Anchor('Jobs', '/jobs')
+// ];
 
 const NotFound = () => {
     return (
         <div className="background-not-found">
-            <NavBar props={new Props(anchorList, true)} />
-            <div className="hubzie-404-image">
-                <img
-                    src="/hubzzie.png"
-                    className="hubzie-404-image"
-                    alt="HubzieNotFound"
-                />
+            {   <div className="navdesktop-container">
+                    <div className="navdesktop-logo">
+                        <img
+                            src="/hublogo.png"
+                            className="navdesktop-logo-image"
+                            alt="The Hub AUBG"
+                        />
+                        <p>The Hub</p>
+                    </div>
+                </div>
+            }
+            <div className="container-text-404">
+                <div>
+                    <h1 className="heading-text-404">404</h1>
+                    <h2 className="mid-heading-404">Page not found</h2>
+                    <p className="paragraph-404">
+                        Seems like the page you are looking for got abducted by
+                        aliens...
+                    </p>
+                    <button
+                        className="button-404"
+                        type="button"
+                        onClick={() => {
+                            location.href = '/';
+                        }}
+                    >
+                        Back to Homepage
+                    </button>
+                </div>
+                <div className="hubzie-UFO-image">
+                    <img
+                        src="/hubzieUFOimage.png"
+                        className="hubzie-UFO-image"
+                        alt="Hubzie being abducted by a UFO"
+                    />
+                </div>
             </div>
         </div>
     );
