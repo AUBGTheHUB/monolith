dcc5e6a6a5f3a247bce92737a842ee8963ad3b0d
peti-krl <petia.dimkina@outlook.com>
2023-02-04 14:05:03 +0200

    Create hackathon teams route

---
+++ packages/api/routes/hackathon_team_route.go
@@ -0,0 +1,15 @@
+package routes
+
+import (
+    "hub-backend/controllers"
+
+    "github.com/gofiber/fiber/v2"
+)
+
+func HackathonRoutes(app *fiber.App) {
+	app.Post("/api/hackathon/teams", controllers.CreateHackathonTeam)
+	app.Get("/api/hackathon/teams", controllers.GetHackathonTeams)
+    app.Get("/api/hackathon/teams/:key", controllers.GetAHackathonTeam)
+	app.Put("/api/hackathon/teams/:key", controllers.EditHackathonTeams)
+    app.Delete("/api/hackathon/teams/:key", controllers.DeleteHackathonTeams)
+}
