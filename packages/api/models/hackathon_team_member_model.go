a10f4ab57b20ff5b8f611ea200eb3bd9d4ceecc2
Dan <a.danny.g.66.88@gmail.com>
2023-02-04 13:44:00 +0200

    #122 Fix hackathon_team_member_model.go file because it was empty

---
+++ packages/api/models/hackathon_team_member_model.go
@@ -0,0 +1,35 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type TeamMember struct {
+	ID             primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+	FullName      string             `json:"fullname, omitempty" validate:"required"`
+	TeamName     string             `json:"teamname, omitempty" validate:"required"`
+	Email       string             `json:"email, omitempty" validate:"required"`
+	School     string             `json:"school, omitempty"`
+	Age         int               `json:"age, omitempty"`
+	Location     string           `json:"location, omitempty"`
+	HeardAboutUs     string            `json:"aboutus, omitempty"`
+	PreviousParticipation   bool   `json:"previouspart, omitempty"`
+	PartDetails    string    `json:"partdetails"`
+	Experience     bool         `json:"experience, omitempty"`
+	ProgrammingLevel    string   `json:"level"`
+	StrongSides         string     `json:"strength"`
+	ShirtSize                string      `json:"size, omitempty"`
+	Internship      bool          `json:"internship"`
+	JobInterests     string    `json:"jobinterests"`
+	SponsorShare     bool      `json:"sponsorshare, omitempty"`
+	NewsLetter    bool     `json:"location, omitempty"`
+	
+}
+
+type EditTeamMember struct {
+	FullName      string `json:"fullname"`
+	TeamName     string `json:"teamname"`
+	Email       string `json:"email"`
+	School     string `json:"school"`
+	Age        int `json:"age"`
+	Location   string `json:"location"`
+	ShirtSize   string `json:"size"`
+}
