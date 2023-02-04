27215b62737901b34fbf78ddd1f33f18c32b1ba4
Radko Mishev <107924021+r-mishev@users.noreply.github.com>
2023-02-04 18:28:16 +0200

    #174 HackAUBG about us section (#198)

--- packages/web/src/components/spa/HackAUBG/HackAUBG.jsx
+++ packages/web/src/components/spa/HackAUBG/HackAUBG.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './hackAUBG.css';
 
 import { MatrixWindow } from './LandingAnimation/LandingAnimation';
+import { AboutHackathon } from './AboutHackathon/AboutHackathon';
 import { Anchor, Props } from '../Navigation/NavFactory.js';
 import { NavBar } from '../Navigation/NavBar';
 
@@ -19,6 +20,7 @@ export const HackAUBG = () => {
         <div className="hackaubg-container">
             <NavBar props={new Props(anchorList, false, 'rgba(0,0,0,.5)')} />
             <MatrixWindow />
+            <AboutHackathon />
         </div>
     );
 };
