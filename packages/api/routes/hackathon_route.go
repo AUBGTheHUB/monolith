93d6d843cea3ebbbdb0d948828d011c64e19fdc7
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-02-04 13:37:00 +0200

    #122 Fix hacathon route func name and api endpoints

--- packages/api/routes/hackathon_route.go
+++ packages/api/routes/hackathon_route.go
@@ -6,11 +6,10 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-func MembersRoute(app *fiber.App) {
-	// club members requests routes
-	app.Post("/api/member", controllers.CreateMember)
-	app.Get("/api/members", controllers.GetAllMembers)
-	app.Get("/api/member/:key", controllers.GetMember)
-	app.Put("/api/member/:key", controllers.EditMember)
-	app.Delete("/api/member/:key", controllers.DeleteMember)
+func HackathonTeamMemberRoute(app *fiber.App) {
+	// Hackathon members requests routes
+	app.Post("/api/hackathon/members", controllers.CreateHackathonMember)
+	app.Get("/api/hackathon/members/:key", controllers.GetHackathonMember)
+	app.Put("/api/hackathon/members/:key", controllers.EditHackathonMember)
+	app.Delete("/api/hackathon/members/:key", controllers.DeleteHackathonMember)
 }
