cd06107722bdb157a3e9004280f1b155ce7bf26b
Radko Mishev <rm.mishev@gmail.com>
2023-02-04 17:17:39 +0200

    #174 Add HackAUBG About section

--- packages/web/src/components/spa/HackAUBG/HackAUBG.jsx
+++ packages/web/src/components/spa/HackAUBG/HackAUBG.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './hackAUBG.css';
 import { MatrixWindow } from './LandingAnimation/LandingAnimation';
+import { AboutHackathon } from './AboutHackathon/AboutHackathon';
 
 export const HackAUBG = () => {
     return (
         <div className="hackaubg-container">
             <MatrixWindow />
+            <AboutHackathon />
         </div>
     );
 };
