c56c1188b40d3e332851f9f9e644c6440b69d453
SuelaCark <suela.qarkaxhija@gmail.com>
2023-02-04 20:37:55 +0200

    #125 added gif as background

---
+++ packages/web/src/components/spa/HackAUBG/LandingAnimation/InvokeAnimation.jsx
@@ -0,0 +1,6 @@
+import { AnimateMatrix } from './landing_animate';
+
+export const InvokeAnimation = () => {
+    AnimateMatrix();
+    return null;
+};
--- packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
+++ packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
@@ -1,21 +1,26 @@
 import React from 'react';
+import './landing_animation.css';
+// import { InvokeAnimation } from './InvokeAnimation';
+// import './matrix.gif';
 
 export const MatrixWindow = () => {
-    // use this component as the one which is going to be rendering the
-    // matrix animation
-
-    // I see that you are trying to implement vanilla js in react
-    // this is going to be a huge pain in the ass ...
-    // trust me
-    // better use an npm library
-    // e.g.
-    // https://www.npmjs.com/package/cmatrix
-    // https://www.npmjs.com/package/matrix-rain
-    // https://www.npmjs.com/package/react-matrix-effect
-
     return (
         <div className="animation-container">
-            <h1>WORKING</h1>
+            <div className="landing-content">
+                <div className="landing-text">
+                    <h1>HackAUBG</h1>
+                    <p>31st March - 2nd April, AUBG - Blagoevgrad</p>
+                </div>
+                <button>Go to Registration</button>
+            </div>
+            {/* <canvas>
+                <InvokeAnimation/>
+            </canvas> */}
+            {/* <img
+                    src="gif.gif"
+                    className="matrix-image"
+                    alt="Matrix animation"
+                /> */}
         </div>
     );
 };
