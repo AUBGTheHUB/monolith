814b32b6e595737701f2dba1cd7c50c0b3219c4c
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-02-06 15:08:20 +0200

    Fix css coupling and remove Articles Section (#220)
    * Apply fix
    
    * Delete Articles folder
    
    * Remove node_modules

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -4,9 +4,9 @@ import { MembersSection } from './MembersSection/MembersSection';
 import { NavBar } from './Navigation/NavBar';
 import { LandingSection } from './LandingSection/LandingSection';
 import { Anchor, Props } from './Navigation/NavFactory.js';
-import { ArticlesSection } from './ArticlesSection/ArticlesSection';
 import { Footer } from './Footer/Footer';
 import { AboutSection } from './AboutSection/AboutSection';
+import '../../../node_modules/react-hovering-cards-carousel/dist/style.css';
 
 const LandingHome = () => {
     const anchorList = [
@@ -20,7 +20,6 @@ const LandingHome = () => {
             <NavBar props={new Props(anchorList, true, 'transparent', false)} />
             <LandingSection />
             <AboutSection />
-            <ArticlesSection />
             <MembersSection />
             <Footer colour={'rgb(21, 76, 121)'} />
         </div>
