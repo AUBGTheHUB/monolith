ed84d168638e3fc6e321c7514533931b39b7e58c
Aleks <al.ivanov1908@gmail.com>
2023-02-06 17:58:47 +0200

    #139 fix missing code

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -3,7 +3,10 @@ import './main.css';
 import { MembersSection } from './MembersSection/MembersSection';
 import { NavBar } from './Navigation/NavBar';
 import { LandingSection } from './LandingSection/LandingSection';
+import { Footer } from './Footer/Footer';
+import { AboutSection } from './AboutSection/AboutSection';
 import { Anchor, Props } from './Navigation/NavFactory.js';
+import '../../../node_modules/react-hovering-cards-carousel/dist/style.css';
 import { useEffect } from 'react';
 
 const LandingHome = () => {
@@ -41,9 +44,11 @@ const LandingHome = () => {
 
     return (
         <div className="main">
-            <NavBar props={new Props(anchorList, true)} />
+            <NavBar props={new Props(anchorList, true, 'transparent', false)} />{' '}
             <LandingSection />
+            <AboutSection />
             <MembersSection />
+            <Footer colour={'rgb(21, 76, 121)'} />
         </div>
     );
 };
