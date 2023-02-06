7b1bde83952a1ea30dd65881e7b94c9a3a00bff6
Aleks <al.ivanov1908@gmail.com>
2023-02-06 17:28:15 +0200

    #139 css error

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -4,24 +4,46 @@ import { MembersSection } from './MembersSection/MembersSection';
 import { NavBar } from './Navigation/NavBar';
 import { LandingSection } from './LandingSection/LandingSection';
 import { Anchor, Props } from './Navigation/NavFactory.js';
-import { Footer } from './Footer/Footer';
-import { AboutSection } from './AboutSection/AboutSection';
-import '../../../node_modules/react-hovering-cards-carousel/dist/style.css';
+import { useEffect } from 'react';
 
 const LandingHome = () => {
     const anchorList = [
-        new Anchor('About', '#AboutSection'),
+        new Anchor('About', '#about'),
+        new Anchor('Events', '#events'),
+        new Anchor('Articles', '#articles'),
         new Anchor('Team', '#team'),
         new Anchor('Jobs', 'jobs')
     ];
 
+    useEffect(() => {
+        let hasHash = !!location.hash;
+        if (hasHash) {
+            /*if (document.getElementById != null)
+            {
+            /*
+                check if document.getElementById is null
+                -> if null -> skip
+                -> if not null -> trigger .scrollIntoView()
+
+                ---
+
+                try to fix not scrolling into full view
+            */
+
+            setTimeout(() => {
+                document
+                    .getElementById(location.hash.replace('#', ''))
+                    .scrollIntoView();
+            }, 600);
+            //}
+        }
+    }, []);
+
     return (
         <div className="main">
-            <NavBar props={new Props(anchorList, true, 'transparent', false)} />
+            <NavBar props={new Props(anchorList, true)} />
             <LandingSection />
-            <AboutSection />
             <MembersSection />
-            <Footer colour={'rgb(21, 76, 121)'} />
         </div>
     );
 };
