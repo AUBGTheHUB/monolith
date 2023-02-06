fd54e8f2543796213712870d4e1bf998f5931a34
Aleks <al.ivanov1908@gmail.com>
2023-02-06 18:04:36 +0200

    #139 add useeffect scrolling

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -7,6 +7,7 @@ import { Anchor, Props } from './Navigation/NavFactory.js';
 import { Footer } from './Footer/Footer';
 import { AboutSection } from './AboutSection/AboutSection';
 import '../../../node_modules/react-hovering-cards-carousel/dist/style.css';
+import { useEffect } from 'react';
 
 const LandingHome = () => {
     const anchorList = [
@@ -14,6 +15,30 @@ const LandingHome = () => {
         new Anchor('Team', '#team'),
         new Anchor('Jobs', 'jobs')
     ];
+
+    useEffect(() => {
+        let hasHash = !!location.hash;
+        if (hasHash) {
+            /*if (document.getElementById != null)
+            {
+            /*
+                check if document.getElementById is null
+                -> if null -> skip
+                -> if not null -> trigger .scrollIntoView()
+
+                ---
+
+                try to fix not scrolling into full view
+            */
+
+            setTimeout(() => {
+                document
+                    .getElementById(location.hash.replace('#', ''))
+                    .scrollIntoView();
+            }, 600);
+            //}
+        }
+    }, []);
 
     return (
         <div className="main">
