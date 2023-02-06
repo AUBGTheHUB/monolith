40fa77122f8e40411ec01d7e7f9660f5326c6e66
Aleks <al.ivanov1908@gmail.com>
2023-02-06 18:16:29 +0200

    #139 remove useeffect comments

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -19,24 +19,11 @@ const LandingHome = () => {
     useEffect(() => {
         let hasHash = !!location.hash;
         if (hasHash) {
-            /*if (document.getElementById != null)
-            {
-            /*
-                check if document.getElementById is null
-                -> if null -> skip
-                -> if not null -> trigger .scrollIntoView()
-
-                ---
-
-                try to fix not scrolling into full view
-            */
-
             setTimeout(() => {
                 document
                     .getElementById(location.hash.replace('#', ''))
                     .scrollIntoView();
             }, 600);
-            //}
         }
     }, []);
 
