2a0126f34e994fe3efaa76b02cbd0b60c75387ca
Aleks <al.ivanov1908@gmail.com>
2023-02-06 15:09:45 +0200

    #175 component for rendering steps

--- packages/web/src/components/spa/HackAUBG/JourneySection/JourneyMobile/JourneyMobile.jsx
+++ packages/web/src/components/spa/HackAUBG/JourneySection/JourneyMobile/JourneyMobile.jsx
@@ -1,20 +1,13 @@
 import React from 'react';
 import './journey_mobile.css';
-import { AiOutlineArrowDown } from 'react-icons/ai';
-import { useState } from 'react';
+import { JourneyStep } from './JourneyStep';
+// import { useState } from 'react';
 
 export const JourneyMobile = () => {
-    const [stepTitle, setStepTitle] = useState('container-title-displayed');
-    const [stepContent, setStepContent] = useState(
-        'container-content-not-displayed'
-    );
-
-    // const closeTitle = () => {
-    //     setStepTitle('container-title-displayed');
-    // };
-    // const closeContent = () => {
-    //     setStepContent('container-content-displayed');
-    // };
+    // const [stepTitle, setStepTitle] = useState('container-title-displayed');
+    // const [stepContent, setStepContent] = useState(
+    //     'container-content-not-displayed'
+    // );
 
     return (
         <div className="journey-mobile-section">
@@ -22,7 +15,7 @@ export const JourneyMobile = () => {
                 <h1>The HackAUBG Journey</h1>
             </div>
             <div className="journey-mobile-content">
-                <div className="mobile-step-container">
+                {/* <div className="mobile-step-container">
                     <div
                         className={stepTitle}
                         onClick={() => {
@@ -43,6 +36,7 @@ export const JourneyMobile = () => {
                         }}
                     >
                         <h2>Gather a team and register</h2>
+                        <AiOutlineArrowUp />
                         <p>
                             Get together with some fellow enthusiasts and
                             register in the form below! Teams must be between 3
@@ -54,28 +48,10 @@ export const JourneyMobile = () => {
                             competition.
                         </p>
                     </div>
-                </div>
-                <div className="mobile-step-container">
-                    <div
-                        className={stepTitle}
-                        onClick={() => {
-                            setStepContent('container-content-displayed');
-                            setStepTitle('container-title-not-displayed');
-                            console.log(stepContent);
-                        }}
-                    >
-                        <h2>Gather a team and register</h2>
-                        <AiOutlineArrowDown />
-                    </div>
-                    <div
-                        className={stepContent}
-                        onClick={() => {
-                            setStepContent('container-content-not-displayed');
-                            setStepTitle('container-title-displayed');
-                            console.log(stepContent);
-                        }}
-                    >
-                        <h2>Gather a team and register</h2>
+                </div> */}
+                <JourneyStep
+                    title={<h1>Gather a team and register</h1>}
+                    text={
                         <p>
                             Get together with some fellow enthusiasts and
                             register in the form below! Teams must be between 3
@@ -86,8 +62,53 @@ export const JourneyMobile = () => {
                             waitlisted until the last week before the
                             competition.
                         </p>
-                    </div>
-                </div>
+                    }
+                />
+                <JourneyStep
+                    title={<h1>Gather a team and register</h1>}
+                    text={
+                        <p>
+                            Get together with some fellow enthusiasts and
+                            register in the form below! Teams must be between 3
+                            and 6 people. However, if you have less, the Hub
+                            team will help you link up with other hackers
+                            waiting to participate. If the number of registered
+                            teams becomes larger than 10, your team will be
+                            waitlisted until the last week before the
+                            competition.
+                        </p>
+                    }
+                />
+                <JourneyStep
+                    title={<h1>Gather a team and register</h1>}
+                    text={
+                        <p>
+                            Get together with some fellow enthusiasts and
+                            register in the form below! Teams must be between 3
+                            and 6 people. However, if you have less, the Hub
+                            team will help you link up with other hackers
+                            waiting to participate. If the number of registered
+                            teams becomes larger than 10, your team will be
+                            waitlisted until the last week before the
+                            competition.
+                        </p>
+                    }
+                />
+                <JourneyStep
+                    title={<h1>Gather a team and register</h1>}
+                    text={
+                        <p>
+                            Get together with some fellow enthusiasts and
+                            register in the form below! Teams must be between 3
+                            and 6 people. However, if you have less, the Hub
+                            team will help you link up with other hackers
+                            waiting to participate. If the number of registered
+                            teams becomes larger than 10, your team will be
+                            waitlisted until the last week before the
+                            competition.
+                        </p>
+                    }
+                />
             </div>
         </div>
     );
---
+++ packages/web/src/components/spa/HackAUBG/JourneySection/JourneyMobile/JourneyStep.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useState } from 'react';
+import { AiOutlineArrowDown } from 'react-icons/ai';
+import { AiOutlineArrowUp } from 'react-icons/ai';
+import './journey_mobile.css';
+
+export const JourneyStep = (props) => {
+    const [stepTitle, setStepTitle] = useState('container-title-displayed');
+    const [stepContent, setStepContent] = useState(
+        'container-content-not-displayed'
+    );
+
+    return (
+        <div className="mobile-step-container">
+            <div
+                className={stepTitle}
+                onClick={() => {
+                    setStepContent('container-content-displayed');
+                    setStepTitle('container-title-not-displayed');
+                    console.log(stepContent);
+                }}
+            >
+                {props.title}
+                <AiOutlineArrowDown />
+            </div>
+            <div
+                className={stepContent}
+                onClick={() => {
+                    setStepContent('container-content-not-displayed');
+                    setStepTitle('container-title-displayed');
+                    console.log(stepContent);
+                }}
+            >
+                {props.title}
+                <AiOutlineArrowUp />
+                {props.text}
+            </div>
+        </div>
+    );
+};
