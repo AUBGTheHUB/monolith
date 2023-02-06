53c6a0b2e44cefcd5e0ee9ed06c0964dd19a8df4
Aleks <al.ivanov1908@gmail.com>
2023-02-06 18:02:50 +0200

    #139 fix mainpage.jsx

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -3,11 +3,10 @@ import './main.css';
 import { MembersSection } from './MembersSection/MembersSection';
 import { NavBar } from './Navigation/NavBar';
 import { LandingSection } from './LandingSection/LandingSection';
+import { Anchor, Props } from './Navigation/NavFactory.js';
 import { Footer } from './Footer/Footer';
 import { AboutSection } from './AboutSection/AboutSection';
-import { Anchor, Props } from './Navigation/NavFactory.js';
 import '../../../node_modules/react-hovering-cards-carousel/dist/style.css';
-import { useEffect } from 'react';
 
 const LandingHome = () => {
     const anchorList = [
@@ -15,30 +14,6 @@ const LandingHome = () => {
         new Anchor('Team', '#team'),
         new Anchor('Jobs', 'jobs')
     ];
-
-    useEffect(() => {
-        let hasHash = !!location.hash;
-        if (hasHash) {
-            /*if (document.getElementById != null)
-            {
-            /*
-                check if document.getElementById is null
-                -> if null -> skip
-                -> if not null -> trigger .scrollIntoView()
-
-                ---
-
-                try to fix not scrolling into full view
-            */
-
-            setTimeout(() => {
-                document
-                    .getElementById(location.hash.replace('#', ''))
-                    .scrollIntoView();
-            }, 600);
-            //}
-        }
-    }, []);
 
     return (
         <div className="main">
