1ee251880bf1c86ff8cda71a0657d006fee5cdc5
Aleks <al.ivanov1908@gmail.com>
2023-02-06 16:44:42 +0200

    #139 fixed merging error

--- packages/web/src/components/spa/MainPage.jsx
+++ packages/web/src/components/spa/MainPage.jsx
@@ -15,30 +15,6 @@ const LandingHome = () => {
         new Anchor('Jobs', 'jobs')
     ];
 
-    useEffect(() => {
-        let hasHash = !!location.hash;
-        if (hasHash) {
-            /*if (document.getElementById != null)
-            {
-            /*
-                check if document.getElementById is null
-                -> if null -> skip
-                -> if not null -> trigger .scrollIntoView()
-
-                ---
-
-                try to fix not scrolling into full view
-            */
-
-                setTimeout(() => {
-                    document
-                        .getElementById(location.hash.replace('#', ''))
-                        .scrollIntoView();
-                }, 600);
-            //}
-        }
-    }, []);
-
     return (
         <div className="main">
             <NavBar props={new Props(anchorList, true, 'transparent', false)} />
