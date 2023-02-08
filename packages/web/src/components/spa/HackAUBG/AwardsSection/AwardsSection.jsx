fa23bfe41f9b8b1f84343fd676837519dbd9ed17
savinadd <savina.dimitrov@gmail.com>
2023-02-08 16:11:20 +0200

    #181 Change colors of award images and award amounts

--- packages/web/src/components/spa/HackAUBG/AwardsSection/AwardsSection.jsx
+++ packages/web/src/components/spa/HackAUBG/AwardsSection/AwardsSection.jsx
@@ -8,27 +8,33 @@ export const AwardsSection = () => {
             <div className="awards-section-images-container">
                 <div className="awards-pic-text">
                     <img
-                        src="awardIcon.png"
+                        src="awardIconSilver.png"
                         alt="Award Silver"
                         id="silver-img"
                     ></img>
-                    <p id="award-money">2000 BGN</p>
+                    <p className="award-money" id="silver-money-amt">
+                        2000 BGN
+                    </p>
                 </div>
                 <div className="awards-pic-text">
                     <img
-                        src="awardIcon.png"
+                        src="awardIconGold.png"
                         alt="Award Gold"
                         id="gold-img"
                     ></img>
-                    <p id="award-money">3000 BGN</p>
+                    <p className="award-money" id="gold-money-amt">
+                        3000 BGN
+                    </p>
                 </div>
                 <div className="awards-pic-text">
                     <img
-                        src="awardIcon.png"
+                        src="awardIconBronze.png"
                         alt="Award Bronze"
                         id="bronze-img"
                     ></img>
-                    <p id="award-money">1000 BGN</p>
+                    <p className="award-money" id="bronze-money-amt">
+                        1000 BGN
+                    </p>
                 </div>
             </div>
             <div className="awards-section-info-text">
