93e99c5722fb51fef20a772f1e5fbc4cfb594201
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-02-11 14:57:58 +0200

    #122 Fix data type error in EditTeam struct and syntax error in both structs

--- packages/api/models/hackathon_team_model.go
+++ packages/api/models/hackathon_team_model.go
@@ -5,11 +5,10 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 type Team struct {
 	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
 	TeamName    string             `json:"teamname" validate:"required"`
-	TeamMembers []string           `json:"teammember" validate:"required" `
+	TeamMembers []string           `json:"teammembers" validate:"required" `
 }
 
 type EditTeam struct {
-	ID          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
-	TeamName    string             `json:"teamname" validate:"required`
-	TeamMembers []TeamMember       `json:"teammember" validate:"required`
+	TeamName    string   `json:"teamname"`
+	TeamMembers []string `json:"teammembers"`
 }
