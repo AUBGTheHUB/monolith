30a39563df83e3001e7cf682d22c102d8fc56c87
Eldi Hoxha <elday@LAPTOP-RTLLIQ51.localdomain>
2023-02-11 15:26:38 +0200

    #248 Simplify the Grading Criteria Section

--- packages/web/src/components/spa/HackAUBG/GradingCriteria/GradingCriteria.jsx
+++ packages/web/src/components/spa/HackAUBG/GradingCriteria/GradingCriteria.jsx
@@ -1,13 +1,48 @@
-import { GradingCriteriaDesktop } from './GradingCriteriaDesktop/GradingCriteriaDesktop';
-import { GradingCriteriaMobile } from './GradingCriteriaMobile/GradingCriteriaMobile';
-import { useMediaQuery } from 'react-responsive';
+import React from 'react';
+import './grading_criteria.css';
 
 export const GradingCriteria = () => {
-    const isMobile = useMediaQuery({ query: '(max-width: 1000px)' });
-
-    if (!isMobile) {
-        return <GradingCriteriaDesktop />;
-    } else {
-        return <GradingCriteriaMobile />;
-    }
+    return (
+        <div className="grading-criteria-table-container">
+            <h1 className="grading-criteria-header">GRADING CRITERIA</h1>
+            <div className="grading-criteria-table">
+                <div className="grading-criteria-column">
+                    <h2>
+                        COMPLEXITY OF THE PROJECT<br></br> (20-points)
+                    </h2>
+                    <p>Scale (6-points)</p>
+                    <p>Project Interface Design (3-points)</p>
+                    <p>Relevance and Simplicity (8-points)</p>
+                    <p>Scalability (3-points)</p>
+                </div>
+                <div className="grading-criteria-column">
+                    <h2>
+                        PROJECT REALIZATION<br></br> (10-points)
+                    </h2>
+                    <p>Scale (6-points)</p>
+                    <p>Project Interface Design (3-points)</p>
+                    <p>Relevance and Simplicity (8-points)</p>
+                    <p>Scalability (3-points)</p>
+                </div>
+                <div className="grading-criteria-column">
+                    <h2>
+                        PROJECT IDEA<br></br> (10-points)
+                    </h2>
+                    <p>Scale (6-points)</p>
+                    <p>Project Interface Design (3-points)</p>
+                    <p>Relevance and Simplicity (8-points)</p>
+                    <p>Scalability (3-points)</p>
+                </div>
+                <div className="grading-criteria-column">
+                    <h2>
+                        PRESENTATION<br></br> (10-points)
+                    </h2>
+                    <p>Scale (6-points)</p>
+                    <p>Project Interface Design (3-points)</p>
+                    <p>Relevance and Simplicity (8-points)</p>
+                    <p>Scalability (3-points)</p>
+                </div>
+            </div>
+        </div>
+    );
 };
