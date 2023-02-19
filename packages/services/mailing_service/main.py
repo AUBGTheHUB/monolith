5d76ce16a094a708b18571559e33fab249aa8561
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-02-19 16:21:02 +0200

    Update handler

--- packages/services/mailing_service/main.py
+++ packages/services/mailing_service/main.py
@@ -1,9 +1,44 @@
+import json
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 import functions_framework
 from dotenv import dotenv_values
+import smtplib as smtp
 
 config = dotenv_values('.env')
 
 
+def send_mail(receiver, subject, html):
+    port = 465  # SSL
+    email = config['HUB_MAIL']
+    password = config['HUB_PSW']
+
+    mime_msg = MIMEMultipart('alternative')
+
+    mime_msg['Subject'] = subject
+    mime_msg.attach(MIMEText(html, "html"))
+
+    server = smtp.SMTP_SSL('smtp.gmail.com', port)
+    try:
+        server.login(email, password)
+        server.sendmail(email, receiver, mime_msg.as_string())
+
+        server.close()
+
+        return json.dumps({"message": "Email sent!"}), 200
+    except Exception as e:
+        return json.dumps({"message": e}), 500
+
+
 @functions_framework.http
-def send_mail(request):
-    return config["HUB_MAIL"]
+def handler(request):
+    if request.form:
+        data = request.form
+    else:
+        data = json.loads(request.data)
+
+    html = data["html"]
+    subject = data["subject"]
+    receiver = data["receiver"]
+
+    return send_mail(receiver, subject, html)
