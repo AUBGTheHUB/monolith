2e596b5a37f5077bd8ac913127aaaf0ec9b32adc
SuelaCark <suela.qarkaxhija@gmail.com>
2023-02-23 19:31:46 +0200

    #176 added placeholders

--- packages/web/src/components/spa/HackAUBG/RegistrationForm/RegistrationForm.jsx
+++ packages/web/src/components/spa/HackAUBG/RegistrationForm/RegistrationForm.jsx
@@ -7,17 +7,20 @@ const RegistrationForm = () => {
             <h1>Register for HackAUBG 5.0</h1>
             <div className="registration-column-left">
                 <p>Full name</p>
-                <input type="text" />
+                <input type="text" placeholder="Enter your name" />
                 <p>Age</p>
-                <input type="text" />
+                <input type="text" placeholder="Enter your age" />
                 <p>School/University </p>
-                <input type="text" />
+                <input type="text" placeholder="Choose an institution" />
             </div>
             <div className="registration-column-right">
                 <p>Email</p>
-                <input type="email" />
+                <input type="email" placeholder="Enter your email" />
                 <p>Location</p>
-                <input type="text" />
+                <input
+                    type="text"
+                    placeholder="Enter the place where you currently live"
+                />
             </div>
         </div>
     );
