55d87f90e0286da0646d4ba500218a4e2edf6dcb
Aleks Ivanov <114522737+Aleks1908@users.noreply.github.com>
2023-02-25 23:19:25 +0200

    #280 Do not extend paragraphs in jobs cards (#311)

--- packages/web/src/components/spa/JobsSection/JobsCard.jsx
+++ packages/web/src/components/spa/JobsSection/JobsCard.jsx
@@ -1,6 +1,17 @@
 import React from 'react';
 
 export const JobsCard = (props) => {
+    const displayDescription = () => {
+        var n = 345;
+        if (props.description.length > n) {
+            while (props.description[n] != ' ') {
+                n -= 1;
+            }
+            return <p>{props.description.slice(0, n)}...</p>;
+        }
+        return <p>{props.description}</p>;
+    };
+
     return (
         <div className="job-card-content">
             <div className="job-logo">
@@ -9,9 +20,8 @@ export const JobsCard = (props) => {
             <div className="job-position">
                 <h3>{props.position}</h3>
             </div>
-            <div className="job-description">
-                <p>{props.description}</p>
-            </div>
+            <div className="job-description">{displayDescription()}</div>
+
             <div className="job-button">
                 <a href={props.link}>Apply Now</a>
             </div>
