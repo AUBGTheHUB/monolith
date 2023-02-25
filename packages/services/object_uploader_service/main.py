994aec9579db95e946e2f1fc4ef5e26c7e16f6ae
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-02-25 15:43:49 +0200

    Add new functionality

--- packages/services/object_uploader_service/main.py
+++ packages/services/object_uploader_service/main.py
@@ -26,9 +26,8 @@ def upload_to_s3(file, file_name):
         s3.upload_file(f'{tmpdir}/{file.filename}', AWS_BUCKET_NAME, saved_file)
 
         location = s3.get_bucket_location(Bucket=AWS_BUCKET_NAME)['LocationConstraint']
-        url = "https://s3-%s.amazonaws.com/%s/%s" % (location, AWS_BUCKET_NAME, saved_file)
 
-        return json.dumps({"message":"Upload Successful", "url": url}), 200
+        return json.dumps({"message":"Upload Successful", "url": get_url_of_obj(location, AWS_BUCKET_NAME, saved_file)}), 200
 
     except NoCredentialsError:
         return json.dumps({"message": "Process authentication failed"}), 500
@@ -38,13 +37,34 @@ def upload_to_s3(file, file_name):
     
     except Exception as e:
         return json.dumps({"message": "Unexpected error", "exception": str(e)}), 500
+    
 
+def get_url_of_obj(loc, bucket_name, url_name):
+    return "https://s3-%s.amazonaws.com/%s/%s" % (loc, bucket_name, url_name)
+
+
+def dump_objects():
+    session = boto3.Session(aws_access_key_id=AWS_PUB_KEY, aws_secret_access_key=AWS_PRIV_KEY)
+    s3 = session.resource('s3')
+    client = session.client('s3')
+    bucket = s3.Bucket(AWS_BUCKET_NAME)
+    location = client.get_bucket_location(Bucket=AWS_BUCKET_NAME)['LocationConstraint']
+
+    objects = []
+
+    for object in bucket.objects.all():
+        objects.append(get_url_of_obj(location, AWS_BUCKET_NAME, object.key))
+
+    return json.dumps({'message':objects})
 
 @functions_framework.http
 def object_uploader(request):
 
     if request.headers.get("Authorization") != environ.get("BEARER"):
         return json.dumps({"message": "Wrong or missing Bearer token"}), 401
+    
+    if (show := request.args.get('show')):
+        return dump_objects() if show == "true" else json.dumps({"message": "show argument was not correctly set"})
 
     if not request.files:
         return json.dumps({"message": "No file has been transmitted"}), 400
