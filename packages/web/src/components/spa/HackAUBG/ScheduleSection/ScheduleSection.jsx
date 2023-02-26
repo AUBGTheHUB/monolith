5d9a7a396044cf34625cc8eecf92e54f23253abb
SuelaCark <suela.qarkaxhija@gmail.com>
2023-02-26 21:52:29 +0200

    #314 removed unnecessary linting space

--- packages/web/src/components/spa/HackAUBG/ScheduleSection/ScheduleSection.jsx
+++ packages/web/src/components/spa/HackAUBG/ScheduleSection/ScheduleSection.jsx
@@ -11,148 +11,104 @@ export const ScheduleHackathon = () => {
             <div className="schedule-container-hack-aubg">
                 <div className="schedule-table-container-hack-aubg">
                     <h1 className="schedule-table-title-hack-aubg">FRIDAY</h1>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">17:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Registration{' '}
+                            Registration
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">18:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Opening{' '}
+                            Opening
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">20:00</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Brainstorming{' '}
+                            Brainstorming
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">21:00</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Idea Pitching{' '}
+                            Idea Pitching
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">21:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Start Coding{' '}
+                            Start Coding
                         </p>
                     </div>
                 </div>
-
                 <div className="schedule-table-container-hack-aubg">
                     <h1 className="schedule-table-title-hack-aubg">SATURDAY</h1>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">09:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Breakfast{' '}
+                            Breakfast
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">11:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Mentor Meetings{' '}
+                            Mentor Meetings
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">13:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Lunch & HR{' '}
+                            Lunch & HR
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">14:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Mentorship + Coding{' '}
+                            Mentorship + Coding
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">19:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Dinner{' '}
-                        </p>
+                        <p className="schedule-event-title-hack-aubg">Dinner</p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">20:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Coding{' '}
-                        </p>
+                        <p className="schedule-event-title-hack-aubg">Coding</p>
                     </div>
                 </div>
-
                 <div className="schedule-table-container-hack-aubg">
                     <h1 className="schedule-table-title-hack-aubg">SUNDAY</h1>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">09:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Breakfast{' '}
+                            Breakfast
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">10:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Coding{' '}
-                        </p>
+                        <p className="schedule-event-title-hack-aubg">Coding</p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">13:00</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Presentation Submission Deadline{' '}
+                            Presentation Submission Deadline
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">13:00</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Lunch{' '}
-                        </p>
+                        <p className="schedule-event-title-hack-aubg">Lunch</p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">14:30</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Presentations Begin{' '}
+                            Presentations Begin
                         </p>
                     </div>
-
                     <div className="schedule-table-row-hack-aubg">
                         <h2 className="schedule-time-hack-aubg">19:00</h2>
                         <p className="schedule-event-title-hack-aubg">
-                            {' '}
-                            Award Ceremony{' '}
+                            Award Ceremony
                         </p>
                     </div>
                 </div>
