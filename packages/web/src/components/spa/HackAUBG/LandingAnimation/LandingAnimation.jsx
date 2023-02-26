6d7e7eaa31c7790689ba662c5a7ca666a6cfbfce
Aleks <al.ivanov1908@gmail.com>
2023-02-26 17:58:15 +0200

    #305 added animation on hackaubg title

--- packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
+++ packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
@@ -2,10 +2,40 @@ import React from 'react';
 import './landing_animation.css';
 
 export const MatrixWindow = () => {
+    window.onload = function () {
+        const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+        let interval = null;
+
+        document.querySelector('h1').onmouseover = (event) => {
+            let iteration = 0;
+
+            clearInterval(interval);
+
+            interval = setInterval(() => {
+                event.target.innerText = event.target.innerText
+                    .split('')
+                    .map((letter, index) => {
+                        if (index < iteration) {
+                            return event.target.dataset.value[index];
+                        }
+
+                        return letters[Math.floor(Math.random() * 26)];
+                    })
+                    .join('');
+
+                if (iteration >= event.target.dataset.value.length) {
+                    clearInterval(interval);
+                }
+
+                iteration += 1 / 3;
+            }, 30);
+        };
+    };
     return (
         <div className="animation-container">
             <div className="landing-content">
-                <h1>HackAUBG 5.0</h1>
+                <h1 data-value="HACKAUBG 5.0">HACKAUBG 5.0</h1>
                 <p>31st March - 2nd April, AUBG - Blagoevgrad</p>
                 <button className="Reg-btn">Go to Registration</button>
             </div>
