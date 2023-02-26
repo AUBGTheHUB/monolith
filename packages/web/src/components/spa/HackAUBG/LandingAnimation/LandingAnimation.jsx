7ac12f1e6856ec09a4420ae597e1ccbcde51a738
Aleks <al.ivanov1908@gmail.com>
2023-02-26 20:13:46 +0200

    #305 added the animation to play when page is loaded

--- packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
+++ packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
@@ -2,14 +2,43 @@ import React from 'react';
 import './landing_animation.css';
 
 export const MatrixWindow = () => {
+    // window.onload = function () {
+    //     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+    //     let interval = null;
+
+    //     document.querySelector('h1').onmouseover = (event) => {
+    //         let iteration = 0;
+
+    //         clearInterval(interval);
+
+    //         interval = setInterval(() => {
+    //             event.target.innerText = event.target.innerText
+    //                 .split('')
+    //                 .map((letter, index) => {
+    //                     if (index < iteration) {
+    //                         return event.target.dataset.value[index];
+    //                     }
+
+    //                     return letters[Math.floor(Math.random() * 26)];
+    //                 })
+    //                 .join('');
+
+    //             if (iteration >= event.target.dataset.value.length) {
+    //                 clearInterval(interval);
+    //             }
+
+    //             iteration += 1 / 3;
+    //         }, 30);
+    //     };
+    // };
     window.onload = function () {
         const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
         let interval = null;
+        const h1 = document.querySelector('h1');
 
-        document.querySelector('h1').onmouseover = (event) => {
+        function startAnimation(event) {
             let iteration = 0;
-
             clearInterval(interval);
 
             interval = setInterval(() => {
@@ -30,7 +59,10 @@ export const MatrixWindow = () => {
 
                 iteration += 1 / 3;
             }, 30);
-        };
+        }
+
+        h1.addEventListener('mouseover', startAnimation);
+        startAnimation({ target: h1 });
     };
     return (
         <div className="animation-container">
