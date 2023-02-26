5a6a933205c11b7e8f107dd209ed4daf07032d9a
Pessy1 <slavcho.balabanov9@gmail.com>
2023-02-26 23:53:15 +0200

    #212 changed the welcome to the hub part of landing section

--- packages/web/src/components/spa/LandingSection/LandingSection.jsx
+++ packages/web/src/components/spa/LandingSection/LandingSection.jsx
@@ -5,7 +5,8 @@ export const LandingSection = () => {
     return (
         <div className="welcome-container">
             <div className="welcome-text">
-                <h1>Welcome to The Hub!</h1>
+                <h1 id="line1">Launch into the &#123;future&#125;</h1>
+                <h1 id="line2">Welcome to The Hub!</h1>
                 <p>
                     The IT and innovations club at the American University in
                     Bulgaria.
