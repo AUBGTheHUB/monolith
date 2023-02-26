8fa5361c90cc5be025b9e8cce4d15c01369cea54
SuelaCark <suela.qarkaxhija@gmail.com>
2023-02-26 22:07:02 +0200

    #314 simplified class namings and their css

--- packages/web/src/components/spa/HackAUBG/ScheduleSection/ScheduleSection.jsx
+++ packages/web/src/components/spa/HackAUBG/ScheduleSection/ScheduleSection.jsx
@@ -3,113 +3,84 @@ import './schedule_section.css';
 
 export const ScheduleHackathon = () => {
     return (
-        <div
-            className="schedule-section-container-hack-aubg"
-            id="ScheduleSection"
-        >
-            <h1 className="schedule-section-title-hack-aubg">THE SCHEDULE</h1>
-            <div className="schedule-container-hack-aubg">
-                <div className="schedule-table-container-hack-aubg">
-                    <h1 className="schedule-table-title-hack-aubg">FRIDAY</h1>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">17:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Registration
-                        </p>
+        <div className="schedule-section-container">
+            <h1 className="schedule-section-title">THE SCHEDULE</h1>
+            <div className="schedule-text-container">
+                <div className="schedule-table-container">
+                    <h1 className="schedule-table-title">FRIDAY</h1>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">17:30</h2>
+                        <p>Registration</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">18:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Opening
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">18:30</h2>
+                        <p>Opening</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">20:00</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Brainstorming
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">20:00</h2>
+                        <p>Brainstorming</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">21:00</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Idea Pitching
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">21:00</h2>
+                        <p>Idea Pitching</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">21:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Start Coding
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">21:30</h2>
+                        <p>Start Coding</p>
                     </div>
                 </div>
-                <div className="schedule-table-container-hack-aubg">
-                    <h1 className="schedule-table-title-hack-aubg">SATURDAY</h1>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">09:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Breakfast
-                        </p>
+                <div className="schedule-table-container">
+                    <h1 className="schedule-table-title">SATURDAY</h1>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">09:30</h2>
+                        <p>Breakfast</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">11:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Mentor Meetings
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">11:30</h2>
+                        <p>Mentor Meetings</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">13:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Lunch & HR
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">13:30</h2>
+                        <p>Lunch & HR</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">14:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Mentorship + Coding
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">14:30</h2>
+                        <p>Mentorship + Coding</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">19:30</h2>
-                        <p className="schedule-event-title-hack-aubg">Dinner</p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">19:30</h2>
+                        <p>Dinner</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">20:30</h2>
-                        <p className="schedule-event-title-hack-aubg">Coding</p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">20:30</h2>
+                        <p>Coding</p>
                     </div>
                 </div>
-                <div className="schedule-table-container-hack-aubg">
-                    <h1 className="schedule-table-title-hack-aubg">SUNDAY</h1>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">09:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Breakfast
-                        </p>
+                <div className="schedule-table-container">
+                    <h1 className="schedule-table-title">SUNDAY</h1>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">09:30</h2>
+                        <p>Breakfast</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">10:30</h2>
-                        <p className="schedule-event-title-hack-aubg">Coding</p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">10:30</h2>
+                        <p>Coding</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">13:00</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Presentation Submission Deadline
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">13:00</h2>
+                        <p>Presentation Submission Deadline</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">13:00</h2>
-                        <p className="schedule-event-title-hack-aubg">Lunch</p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">13:00</h2>
+                        <p>Lunch</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">14:30</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Presentations Begin
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">14:30</h2>
+                        <p>Presentations Begin</p>
                     </div>
-                    <div className="schedule-table-row-hack-aubg">
-                        <h2 className="schedule-time-hack-aubg">19:00</h2>
-                        <p className="schedule-event-title-hack-aubg">
-                            Award Ceremony
-                        </p>
+                    <div className="schedule-table-row">
+                        <h2 className="schedule-time">19:00</h2>
+                        <p>Award Ceremony</p>
                     </div>
                 </div>
             </div>
