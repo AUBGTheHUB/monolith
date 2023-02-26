23ca247c374306fc845e732cb35294ad524eb090
Aleks <al.ivanov1908@gmail.com>
2023-02-26 22:56:42 +0200

    #305 fix animation delay

--- packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
+++ packages/web/src/components/spa/HackAUBG/LandingAnimation/LandingAnimation.jsx
@@ -1,41 +1,12 @@
 import React from 'react';
+import { useEffect } from 'react';
 import './landing_animation.css';
 
 export const MatrixWindow = () => {
-    // window.onload = function () {
-    //     const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-
-    //     let interval = null;
-
-    //     document.querySelector('h1').onmouseover = (event) => {
-    //         let iteration = 0;
-
-    //         clearInterval(interval);
-
-    //         interval = setInterval(() => {
-    //             event.target.innerText = event.target.innerText
-    //                 .split('')
-    //                 .map((letter, index) => {
-    //                     if (index < iteration) {
-    //                         return event.target.dataset.value[index];
-    //                     }
-
-    //                     return letters[Math.floor(Math.random() * 26)];
-    //                 })
-    //                 .join('');
-
-    //             if (iteration >= event.target.dataset.value.length) {
-    //                 clearInterval(interval);
-    //             }
-
-    //             iteration += 1 / 3;
-    //         }, 30);
-    //     };
-    // };
-    window.onload = function () {
+    useEffect(() => {
         const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
         let interval = null;
-        const h1 = document.querySelector('h1');
+        const title = document.getElementById('title');
 
         function startAnimation(event) {
             let iteration = 0;
@@ -61,13 +32,25 @@ export const MatrixWindow = () => {
             }, 30);
         }
 
-        h1.addEventListener('mouseover', startAnimation);
-        startAnimation({ target: h1 });
-    };
+        if (title) {
+            title.addEventListener('mouseover', startAnimation);
+            startAnimation({ target: title });
+        }
+
+        return () => {
+            if (title) {
+                title.removeEventListener('mouseover', startAnimation);
+            }
+            clearInterval(interval);
+        };
+    }, []);
+
     return (
         <div className="animation-container">
             <div className="landing-content">
-                <h1 data-value="HACKAUBG 5.0">HACKAUBG 5.0</h1>
+                <h1 data-value="HACKAUBG 5.0" id="title">
+                    HACKAUBG 5.0
+                </h1>
                 <p>31st March - 2nd April, AUBG - Blagoevgrad</p>
                 <button className="Reg-btn">Go to Registration</button>
             </div>
