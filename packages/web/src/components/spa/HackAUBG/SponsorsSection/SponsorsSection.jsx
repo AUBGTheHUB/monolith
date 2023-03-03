cd92fcd9f21c0e5ee82665811cf8356710b12b50
annakukova <akukova12@gmail.com>
2023-03-03 11:42:10 +0200

    #182 make logos stay within borders on responsive

--- packages/web/src/components/spa/HackAUBG/SponsorsSection/SponsorsSection.jsx
+++ packages/web/src/components/spa/HackAUBG/SponsorsSection/SponsorsSection.jsx
@@ -10,34 +10,52 @@ export const Sponsors = () => {
                     <h1>Platinum</h1> {/* Add here the background box design*/}
                 </div>
                 <div className="platinum-box">
-                    {/* This will hold all the logos */}
-                    <img
-                        src="https://1000logos.net/wp-content/uploads/2017/09/Uber-logo.jpg"
-                        alt="Uber logo"
-                    />
-                    <img
-                        src="https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Chaos_logo.svg/1200px-Chaos_logo.svg.png"
-                        alt="Chaos logo"
-                    />
-                    <img
-                        src="https://www.shapeblue.com/wp-content/uploads/2020/11/VMware-logo.jpg"
-                        alt="vmware logo"
-                    />
-                    <img
-                        src="https://1000logos.net/wp-content/uploads/2016/10/Bosch-Logo-1925.png"
-                        alt="Bosch logo"
-                    />
+                    <div className="logo-container">
+                        {/* This will hold all the logos */}
+                        <img
+                            src="https://1000logos.net/wp-content/uploads/2017/09/Uber-logo.jpg"
+                            alt="Uber logo"
+                            onClick={() =>
+                                (window.location.href = 'https://www.uber.com')
+                            }
+                        />
+                        <img
+                            src="https://upload.wikimedia.org/wikipedia/en/thumb/7/7b/Chaos_logo.svg/1200px-Chaos_logo.svg.png"
+                            alt="Chaos logo"
+                            onClick={() =>
+                                (window.location.href = 'https://www.chaos.com')
+                            }
+                        />
+                        <img
+                            src="https://www.shapeblue.com/wp-content/uploads/2020/11/VMware-logo.jpg"
+                            alt="vmware logo"
+                            onClick={() =>
+                                (window.location.href = 'https://vmware.com')
+                            }
+                        />
+                        <img
+                            src="https://1000logos.net/wp-content/uploads/2016/10/Bosch-Logo-1925.png"
+                            alt="Bosch logo"
+                            onClick={() =>
+                                (window.location.href = 'https://bosch.com')
+                            }
+                        />
+                    </div>
                 </div>
+                
                 <div className="gold-container">
                     <h1>Gold</h1>
                 </div>
                 <div className="gold-box">
+                    <div className="logo-container">
                     {/* This will hold all the logos */}
                     <img
                         src="https://1000logos.net/wp-content/uploads/2017/09/Uber-logo.jpg"
                         alt="Uber logo"
                     />
+                    </div>
                 </div>
+                
                 <div className="custom-container">
                     <h1>Custom</h1>
                 </div>
