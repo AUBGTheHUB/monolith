eaa30cfad667e0644b7afe139efbb3672fd397fc
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-03-04 22:22:47 +0200

    Fix hackathon participants struct field names (#358)

--- packages/api/controllers/hackathon_no_team_participants_controller.go
+++ packages/api/controllers/hackathon_no_team_participants_controller.go
@@ -53,7 +53,6 @@ func CreateNoTeamHackathonParticipant(c *fiber.Ctx) error {
 		Location:              member.Location,
 		HeardAboutUs:          member.HeardAboutUs,
 		PreviousParticipation: member.PreviousParticipation,
-		PartDetails:           member.PartDetails,
 		Experience:            member.Experience,
 		ProgrammingLevel:      member.ProgrammingLevel,
 		StrongSides:           member.StrongSides,
