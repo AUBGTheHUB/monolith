77daa3d5a4483a37a55d3410e36e6c312ac67481
Viktor Logodazhki <v.logodazhki@abv.bg>
2023-03-06 10:44:39 +0200

    #182 Added onClick for the sponsors

--- packages/web/src/components/spa/HackAUBG/SponsorsSection/SponsorCard.jsx
+++ packages/web/src/components/spa/HackAUBG/SponsorsSection/SponsorCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './sponsors_section.css';
+import { openNewTab } from '../../../../Global';
 
 const SponsorCard = ({ sponsor }) => {
-    return <img src={sponsor.profilepicture} alt={sponsor.name} />;
+    return (
+        <img
+            src={sponsor.profilepicture}
+            alt={sponsor.name}
+            onClick={() => {
+                openNewTab(sponsor.sociallink);
+            }}
+        />
+    );
 };
 
 export default SponsorCard;
