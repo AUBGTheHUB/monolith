db75e6265b9348755a35ea8f4b1d77322bdda774
Nikolay Ninov <nikininov1@gmail.com>
2023-03-13 00:59:15 +0200

    #310 Add edit team member component

--- packages/web/src/components/admin_page/hackathon/hackathon_team_members/single_team_member.jsx
+++ packages/web/src/components/admin_page/hackathon/hackathon_team_members/single_team_member.jsx
@@ -13,11 +13,23 @@ const TeamMemberActions = () => {
 
     const [formState, setFormState] = useState({
         fullname: '',
-        teamname: '',
+        hasteam: true,
+        teamname: member_data.teamname,
         email: '',
-        school: '',
-        experiance: '',
-        jobinterests: ''
+        university: '',
+        age: 0,
+        location: '',
+        heardaboutus: '',
+        prevhackathonparticipation: false,
+        prevhackaubgparticipation: false,
+        hasexperience: false,
+        programminglevel: '',
+        strongsides: '',
+        shirtsize: '',
+        wantinternship: false,
+        jobinterests: '',
+        shareinfowithsponsors: false,
+        wantjoboffers: false
     });
 
     const handleInputChange = (e) => {
@@ -34,9 +46,10 @@ const TeamMemberActions = () => {
     // pushing useNavigate -1 otherwise the previously passed state does not get saved (hence auth token)
 
     const remove_team_member = () => {
+        console.log(member_data['id']);
         axios({
             method: 'delete',
-            url: url + '/api/dashboard/hackathon/teams/members/' + member_data['id'] + '/',
+            url: url + '/api/hackathon/members/' + member_data['id'],
             headers: { 'BEARER-TOKEN': localStorage.getItem('auth_token') }
         })
             // eslint-disable-next-line no-unused-vars
@@ -52,10 +65,7 @@ const TeamMemberActions = () => {
     const edit_team_member = () => {
         axios({
             method: 'put',
-            url:
-                url +
-                '/api/dashboard/hackathon/teams/members/actions/' +
-                member_data['id'],
+            url: url + '/api/hackathon/members/' + member_data['id'],
             headers: { 'BEARER-TOKEN': localStorage.getItem('auth_token') },
             data: { ...formState }
         })
@@ -78,14 +88,44 @@ const TeamMemberActions = () => {
                         src={member_data['profilepicture']}
                     />
                     <Card.Body>
-                        <Card.Title>
-                            {member_data['fullname']}
-                        </Card.Title>
+                        <Card.Title>{member_data['fullname']}</Card.Title>
                         <Card.Text>
-                            {'Team: ' + member_data['teamname']}
+                            <b>Team: </b> {member_data['teamname']}
                         </Card.Text>
                         <Card.Text>
-                            {'School: ' + member_data['school']}
+                            <b>Email: </b> {member_data['email']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>School: </b>
+                            {member_data['university']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>Age: </b>
+                            {member_data['age']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>Location: </b>
+                            {member_data['location']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>How they heard about us: </b>
+                            {member_data['heardaboutus']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>Programming Level: </b>
+                            {member_data['programminglevel']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>Strong sides: </b>
+                            {member_data['strongsides']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>Job interests: </b>
+                            {member_data['jobinterests']}
+                        </Card.Text>
+                        <Card.Text>
+                            <b>T-shirt Size: </b>
+                            {member_data['shirtsize']}
                         </Card.Text>
                         <Button
                             variant="primary"
@@ -110,28 +150,94 @@ const TeamMemberActions = () => {
                             onChange={handleInputChange}
                         />
                     </Form.Group>
-
-
                     <Form.Group className="mb-3" controlId="formBasicText">
-                        <Form.Label>Team</Form.Label>
+                        <Form.Label>Email</Form.Label>
                         <Form.Control
                             type="text"
-                            placeholder="teamname"
-                            name="teamname"
+                            placeholder="email"
+                            name="email"
                             onChange={handleInputChange}
                         />
                     </Form.Group>
-
                     <Form.Group className="mb-3" controlId="formBasicText">
                         <Form.Label>School</Form.Label>
                         <Form.Control
                             type="text"
-                            placeholder="school"
-                            name="school"
+                            placeholder="university"
+                            name="university"
                             onChange={handleInputChange}
                         />
                     </Form.Group>
 
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Age</Form.Label>
+                        <Form.Control
+                            type="number"
+                            placeholder="age"
+                            name="age"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Location</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="location"
+                            name="location"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Heard about us?</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="heardaboutus"
+                            name="heardaboutus"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Programming level</Form.Label>
+                        <Form.Control
+                            type="text"
+                            label="programminglevel"
+                            name="programminglevel"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Strong Sides</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="strongsides"
+                            name="strongsides"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Shirt Size</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="shirtsize"
+                            name="shirtsize"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicText">
+                        <Form.Label>Job Interests</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="jobinterests"
+                            name="jobinterests"
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
                     <Button
                         variant="primary"
                         type="button"
