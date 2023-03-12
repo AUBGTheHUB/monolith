78c9af073c1462da2853878cc8947c5996db8492
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-03-12 13:03:36 +0200

    #167 update conditional check

--- cli.sh
+++ cli.sh
@@ -3,7 +3,7 @@
 current_directory=${PWD##*/} 
 
 set_vm_ip () {
-    if [[ ! -z "${HUB_VM}" ]]; then
+    if [[ HUB_VM ]]; then
         echo "A default $(gum style --foreground 212 "public ip") has been previously set up. Do you wish to $(gum style --foreground 212 "reuse") it?"
         USE_DEFAULT=$(gum choose --limit 1 "yes" "no")
         if [ "$USE_DEFAULT" == "yes" ]; then
@@ -22,7 +22,7 @@ set_vm_ip () {
 
     echo "Would you like to remember the $(gum style --foreground 212 "Virtual Machine")?"
     USE_DEFAULT=$(gum choose --limit 1 "yes" "no")
-    if [ "$USE_DEFAULT" == "yes" ]; then
+    if [ $USE_DEFAULT == "yes" ]; then
         echo "export HUB_VM=\"$VM_IP\"" >> ~/.bashrc
     fi
 }
@@ -87,7 +87,7 @@ elif [ "$ACTIONS" == "$DEPLOY" ]; then
     fi
 fi
 
-if [ -z "${HUB_VM}" ] && [! -z "${VM_IP}"]; then
+if [ -z $HUB_VM ] && [ $VM_IP ]; then
     echo "Saving the $(gum style --foreground 212 "Virtual Machine")..."
     exec $SHELL
 fi
