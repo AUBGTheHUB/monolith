fa5359f808469f901d9c688f34333aa93d8f5416
SuelaCark <suela.qarkaxhija@gmail.com>
2023-03-15 20:05:29 +0200

    #414 enabled jobs section

--- packages/web/src/feature_switches.js
+++ packages/web/src/feature_switches.js
@@ -1,6 +1,6 @@
 // easily disable sections globally
 const FEATURE_SWITCHES = {
-    jobs: false,
+    jobs: true,
     team: true,
     regForm: true
     // TODO: add feature switches for important components such as the registration form
