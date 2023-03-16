78c4044969afde52a64629237e467edd5f27ca05
SuelaCark <suela.qarkaxhija@gmail.com>
2023-03-16 03:17:31 +0200

    #414 added basic logic for delete image button (needs work)

--- packages/web/src/components/admin_page/s3_page/obj_card.jsx
+++ packages/web/src/components/admin_page/s3_page/obj_card.jsx
@@ -1,10 +1,27 @@
 import { Button } from 'react-bootstrap';
+import axios from 'axios';
 
 export const ObjectCard = ({ url }) => {
     const filename = url.slice(url.lastIndexOf('/') + 1);
 
     const saveToClipboard = () => {
         navigator.clipboard.writeText(url);
+    };
+
+    const deleteImage = () => {
+        axios({
+            method: 'delete'
+            // url: url + '/api/hackathon/teams/' + team_data['id'] + '/',
+            // headers: { 'BEARER-TOKEN': localStorage.getItem('auth_token') }
+        })
+            // eslint-disable-next-line no-unused-vars
+            .then((res) => {
+                console.log('Image was deleted');
+                history(-2);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     };
 
     return (
@@ -15,7 +32,9 @@ export const ObjectCard = ({ url }) => {
                 {' '}
                 Get image url
             </Button>
-            <Button variant="primary">Delete image</Button>
+            <Button variant="primary" onClick={deleteImage}>
+                Delete image
+            </Button>
         </div>
     );
 };
