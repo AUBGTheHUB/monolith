14142d15430ac9a26886c59c3fb2195d67cacf39
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-03-19 15:28:28 +0200

    #421 disable tests when env is not set correctly and link global env (#424)

--- packages/api/tests/utils.go
+++ packages/api/tests/utils.go
@@ -3,16 +3,26 @@ package tests
 import (
 	"context"
 	"hub-backend/configs"
+	"log"
+	"os"
 	"time"
 )
 
+func CheckEnv() {
+	// exit test case if not in correct env
+	if !configs.IsTestENV() {
+		log.Fatalf("IS_TEST is missing or false!")
+		os.Exit(1)
+	}
+}
+
 func CleanUpCollection() {
 	collections := configs.GetCollectionsStartingWith(configs.DB, "_tests")
-	
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
 	for _, collection := range collections {
-		collection.Drop(ctx)	
+		collection.Drop(ctx)
 	}
 }
