5a1d2c2507e9c88d435d09001ffefe0dfb918a6f
Suela C <77395265+SuelaCark@users.noreply.github.com>
2023-03-26 17:48:50 +0300

    #441 Enable jobs and disable reg form (#443)

--- packages/web/src/feature_switches.js
+++ packages/web/src/feature_switches.js
@@ -1,8 +1,8 @@
 // easily disable sections globally
 const FEATURE_SWITCHES = {
-    jobs: false,
+    jobs: true,
     team: true,
-    regForm: true
+    regForm: false
     // TODO: add feature switches for important components such as the registration form
 };
 
