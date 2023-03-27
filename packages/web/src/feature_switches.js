e4c3597e65eb5811a523801fe61616940cd4fa04
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-03-27 12:08:31 +0300

    Close registration

--- packages/web/src/feature_switches.js
+++ packages/web/src/feature_switches.js
@@ -2,7 +2,7 @@
 const FEATURE_SWITCHES = {
     jobs: false,
     team: true,
-    regForm: true
+    regForm: false 
     // TODO: add feature switches for important components such as the registration form
 };
 
