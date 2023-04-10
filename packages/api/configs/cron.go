ce8dc6da7ce752f0f94fe1c84aefb3cc5362e61f
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-04-11 00:02:10 +0300

    Fix token which is not being rotated (#449)

--- packages/api/configs/cron.go
+++ packages/api/configs/cron.go
@@ -9,8 +9,8 @@ import (
 // Sets a new random value of the BEARER TOKEN every 24 hours
 func RunCronJobs() {
 	schedule := gocron.NewScheduler(time.UTC)
-    schedule.Every(1).Day().At("7:00").Do(func() {
-		SetToken()
+	schedule.Every(1).Day().At("7:00").Do(func() {
+		token = SetToken()
 	})
 
 	schedule.StartAsync()
