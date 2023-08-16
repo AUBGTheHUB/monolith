019809fe3524e49c84bd57db1ca6b0322308c2ba
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-16 16:59:43 +0300

    Fix bug with air not working at first load

--- packages/api/reload.sh
+++ packages/api/reload.sh
@@ -3,7 +3,8 @@
 if ! hash air &> /dev/null
 then
     echo "AIR could not be found, running installation script"
-    cd ../.. && make install-air
+    cd ../.. && make install-air && cd packages/api
 fi
 
+echo $PWD
 air -c .air.toml
