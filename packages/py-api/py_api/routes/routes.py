178ad86dd6e9bfcbc00d8d5a04194e570ee7341e
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-24 13:27:17 +0300

    Add comment about setting up unverified routes

--- packages/py-api/py_api/routes/routes.py
+++ packages/py-api/py_api/routes/routes.py
@@ -1,5 +1,11 @@
 from py_api.routes.utility import UtilityRoutes
 
+"""
+    If you need to disable request verification for a particular endpoint,
+    you can do that in packages/py-api/py_api/middleware/auth.py
+    by adding them to the BYPASSED_ENDPOINTS dictionary.
+"""
+
 
 class Routes:
     def __init__(self, app):
