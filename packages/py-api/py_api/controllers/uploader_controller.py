916918e445a99c65da98dd26db83c1dc81d591cd
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-24 22:43:56 +0300

    stage changes

--- packages/py-api/py_api/controllers/uploader_controller.py
+++ packages/py-api/py_api/controllers/uploader_controller.py
@@ -1,8 +1,10 @@
-from os import getenv
+from os import getenv, path
+from tempfile import gettempdir
 from typing import Any, Dict
 
 import boto3
 from dotenv import load_dotenv
+from fastapi import Request, UploadFile
 
 load_dotenv()
 
@@ -15,6 +17,40 @@ class UploaderControllers:
     @classmethod
     def get_url_of_object(cls, location: str, bucket_name: str, url_name: str) -> str:
         return "https://s3-%s.amazonaws.com/%s/%s" % (location, bucket_name, url_name)
+
+    @classmethod
+    def upload_object(cls, request: Request, file: UploadFile) -> Dict[str, Any]:
+        s3 = boto3.client(
+            's3', aws_access_key_id=AWS_PUB_KEY,
+            aws_secret_access_key=AWS_PRIV_KEY,
+        )
+
+        file_name = request.body["filename"]
+
+        try:
+            tmpdir = gettempdir()
+            file.save(path.join(tmpdir, file.filename))
+
+            saved_file = file_name + "." + file.filename.rsplit('.')[1]
+            s3.upload_file(
+                f'{tmpdir}/{file.filename}',
+                AWS_BUCKET_NAME, saved_file, ExtraArgs={'ContentType': 'image/jpeg'},
+            )
+
+            location = s3.get_bucket_location(Bucket=AWS_BUCKET_NAME)[
+                'LocationConstraint'
+            ]
+
+            return {"message": "Upload Successful", "url": cls.get_url_of_obj(location, AWS_BUCKET_NAME, saved_file)}
+
+        # except NoCredentialsError:
+        #     return json.dumps({"message": "Process authentication failed"}), 500
+
+        # except FileNotFoundError:
+        #     return json.dumps({"message": "File upload failed"}), 500
+
+        except:
+            pass
 
     @classmethod
     def dump_objects(cls) -> Dict[str, Any]:
--- packages/py-api/py_api/routes/uploader_routes.py
+++ packages/py-api/py_api/routes/uploader_routes.py
@@ -1,15 +1,16 @@
 from typing import Any, Dict
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Request
 from py_api.controllers import UploaderControllers
 
 
 class UploaderRoutes:
     @staticmethod
     def bind(router: APIRouter) -> None:
-        @router.post('/uploader')
-        async def upload_object() -> Dict[str, Any]:
-            return {}
+        # @router.post('/uploader')
+        # async def upload_object() -> Dict[str, Any]:
+        #     return {"message": "here"}
+        #     # return UploaderControllers.upload_object()
 
         @router.get('/uploader')
         async def get_objects() -> Dict[str, Any]:
