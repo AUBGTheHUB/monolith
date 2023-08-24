797539b44459ffd8c2f1d46375e6b81c2f82ea10
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-24 20:50:17 +0300

    Update comment for importing db

--- packages/py-api/py_api/database/initialize.py
+++ packages/py-api/py_api/database/initialize.py
@@ -5,12 +5,12 @@ from pymongo import MongoClient
 
 load_dotenv()
 
-# How to use - https://pymongo.readthedocs.io/en/stable/tutorial.html
+# * How to use - https://pymongo.readthedocs.io/en/stable/tutorial.html
 # e.g. db.jobs.find() where jobs is a collection in the database
 
-# whenever you need to query the database or do any transcations
+# whenever you need to read from or write to the database
 # you should import the db variable as follows:
-# from qb_api.database import db
+# * from qb_api.database import db
 
 client = MongoClient(getenv("MONGOURI"))
 db = client["TheHubDB"]
