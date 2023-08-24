815f88584a04dd7b12c545b6c7ca11ce061ff490
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-24 17:51:07 +0300

    Add type anon for routes

--- packages/py-api/py_api/main.py
+++ packages/py-api/py_api/main.py
@@ -1,13 +1,12 @@
-from uvicorn import run
 from fastapi import FastAPI
-
 from py_api.middleware import AuthMiddleware
 from py_api.routes import Routes
+from uvicorn import run
 
 app = FastAPI()
 AuthMiddleware.bind(app)
 Routes.bind(app)
 
 
-def start():
+def start() -> None:
     run("py_api.main:app", host="0.0.0.0", port=6969, reload=True)
