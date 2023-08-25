ed651c0a4188912c13407e43b314d3e3a1a71f97
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-25 10:55:41 +0300

    Add exception handler

--- packages/py-api/py_api/middleware/__init__.py
+++ packages/py-api/py_api/middleware/__init__.py
@@ -1,5 +1,7 @@
-from py_api.middleware.auth import AuthMiddleware 
+from py_api.middleware.auth import AuthMiddleware
+from py_api.middleware.exception_handler import ExceptionHandler
 
 __all__ = [
-    "AuthMiddleware"
+    "AuthMiddleware",
+    "ExceptionHandler",
 ]
---
+++ packages/py-api/py_api/middleware/exception_handler.py
@@ -0,0 +1,27 @@
+from os import getenv
+from traceback import format_exc
+
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+
+
+class ExceptionHandler:
+    @staticmethod
+    def bind(app: FastAPI) -> None:
+        @app.exception_handler(Exception)
+        async def general_exception_handler(request: Request, exc: Exception) -> JSONResponse:
+            content = {
+                "message": "Something went wrong! Please, contact The Hub!",
+            }
+
+            if bool(getenv("IS_OFFLINE")):
+                content = {
+                    "message": "Hey there, bud! Don't be sad! Here's some info which might help you debug your issue 🎉",
+                    "exception": str(exc),
+                    "stacktrace": str(format_exc()) if bool(getenv("IS_OFFLINE")) else "<REDACTED>",
+                }
+
+            return JSONResponse(
+                status_code=500,
+                content=content,
+            )
