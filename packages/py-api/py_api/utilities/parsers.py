156237ad2c095bdcc5e187d7b1dc419cd84babba
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-25 10:28:17 +0300

    Add callable

--- packages/py-api/py_api/utilities/parsers.py
+++ packages/py-api/py_api/utilities/parsers.py
@@ -2,6 +2,6 @@ from json import loads
 from typing import Any, Callable, Coroutine, Dict
 
 
-async def parse_request_body(body: Callable[Coroutine[Any, Any, bytes]]) -> Dict[Any, Any]:
+async def parse_request_body(body: Callable[..., bytes]) -> Dict[Any, Any]:
     parsed_body: Dict[Any, Any] = loads(await body())
     return parsed_body
