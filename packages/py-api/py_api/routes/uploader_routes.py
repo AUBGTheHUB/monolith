15fef8e6482681a60918daad96d670ae11119ad5
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-25 23:19:36 +0300

    Remove mypy comment

--- packages/py-api/py_api/routes/uploader_routes.py
+++ packages/py-api/py_api/routes/uploader_routes.py
@@ -13,17 +13,6 @@ async def get_objects() -> Dict[str, Any]:
 
 @router.post("")
 async def upload_object(file: UploadFile, filename: Annotated[str, Form()]) -> Dict[str, Any]:
-    """
-        MyPy's capabilities are insufficient to detect the possibility that the
-        upload_object controller could potentially return a JSONResponse instead
-        of the expected typed Dict.
-
-        It appears that the majority of codebases
-        do not specify return types for their endpoints.
-
-        However, due to our enforcement of rigorous --strict checking,
-        we must manage with typed dictionaries.
-    """
     return c.upload_object(file, filename)
 
 
