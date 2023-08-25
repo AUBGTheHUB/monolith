2562a055188b6cc8b764271246df82339d8e3561
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-25 11:06:06 +0300

    Add comments for parse_request_body

--- packages/py-api/py_api/utilities/parsers.py
+++ packages/py-api/py_api/utilities/parsers.py
@@ -1,7 +1,8 @@
 from json import loads
-from typing import Any, Callable, Coroutine, Dict
+from typing import Any, Callable, Dict
 
 
 async def parse_request_body(body: Callable[..., bytes]) -> Dict[Any, Any]:
+    """ Only works for fastapi.requests.Request.body callables """
     parsed_body: Dict[Any, Any] = loads(await body())
     return parsed_body
