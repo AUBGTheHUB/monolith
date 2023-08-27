4231bc7b70dfa88729a040fee2e372dc9cfd39b0
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-27 14:45:28 +0300

    Add functionality for adding new url

--- packages/web/src/components/admin_page/url_shortener/table.jsx
+++ packages/web/src/components/admin_page/url_shortener/table.jsx
@@ -1,16 +1,88 @@
-import { Table } from 'react-bootstrap';
+import { Alert, Button, Form, OverlayTrigger, Popover, Table } from 'react-bootstrap';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { parseToNewAPI, urlShortenerURL } from '../../../Global';
 import UrlRow from './row';
 
+const popover = (onUpdate, errorMessage) => {
+    return (
+        <Popover id="popover-basic">
+            <Popover.Header as="h3">Want to update or remove?</Popover.Header>
+            <Popover.Body>
+                {errorMessage !== undefined ? (
+                    <Alert variant="warning">Not a viable URL - {errorMessage}!</Alert>
+                ) : null}
+                <UpdateUrlForm onUpdate={onUpdate} />
+            </Popover.Body>
+        </Popover>
+    );
+};
+
+const UpdateUrlForm = ({ onUpdate }) => {
+    const [newShortenedUrl, setNewShortenedUrl] = useState({});
+
+    const handleChange = e => {
+        setNewShortenedUrl({
+            ...newShortenedUrl,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    return (
+        <>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Set a url:</Form.Label>
+                <Form.Control type="text" placeholder="Paste the url" onChange={handleChange} name="url" />
+                <Form.Text className="text-muted">Add the new url you want this endpoint to redirect to</Form.Text>
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Set an endpoint:</Form.Label>
+                <Form.Control type="text" placeholder="Add the endpoint" onChange={handleChange} name="endpoint" />
+                <Form.Text className="text-muted">
+                    For example `shorted` if you want it to be thehub-aubg.com/s/shorted
+                </Form.Text>
+            </Form.Group>
+
+            <Button
+                variant="primary"
+                onClick={() => {
+                    onUpdate(newShortenedUrl);
+                }}>
+                Add
+            </Button>
+        </>
+    );
+};
+
 const UrlsTable = () => {
     const [shortenedUrls, setShortenedUrls] = useState([]);
     const [selected, setSelected] = useState('');
     const [trigger, setTrigger] = useState(0);
+    const [errorMessage, setErrorMessage] = useState(undefined);
+    const [showAddOverlay, setShowAddOverlay] = useState(false);
 
     const triggerFetch = () => {
         setTrigger(prev => prev + 1);
+    };
+
+    const onUpdate = data => {
+        axios(parseToNewAPI(urlShortenerURL), {
+            headers: {
+                'BEARER-TOKEN': localStorage.getItem('auth_token'),
+            },
+            method: 'put',
+            data,
+        })
+            .then(() => {
+                triggerFetch();
+                if (errorMessage) {
+                    setErrorMessage(undefined);
+                }
+                setShowAddOverlay(false);
+            })
+            .catch(err => {
+                setErrorMessage(err?.response?.data?.detail[0]?.ctx?.error);
+            });
     };
 
     useEffect(() => {
@@ -24,26 +96,38 @@ const UrlsTable = () => {
     }, [trigger]);
 
     return (
-        <Table style={{ color: 'white' }}>
-            <thead>
-                <tr>
-                    <th>entry</th>
-                    <th>url</th>
-                </tr>
-            </thead>
-            <tbody>
-                {shortenedUrls.map(item => (
-                    <UrlRow
-                        endpoint={item.endpoint}
-                        url={item.url}
-                        key={item.endpoint}
-                        selected={selected}
-                        setSelected={setSelected}
-                        triggerFetch={triggerFetch}
-                    />
-                ))}
-            </tbody>
-        </Table>
+        <>
+            <OverlayTrigger show={showAddOverlay} placement="bottom" overlay={popover(onUpdate, errorMessage)}>
+                <Button
+                    variant="primary"
+                    style={{ width: '100vw' }}
+                    onClick={() => {
+                        setShowAddOverlay(prev => !prev);
+                    }}>
+                    Create a shortened URL
+                </Button>
+            </OverlayTrigger>
+            <Table style={{ color: 'white' }}>
+                <thead>
+                    <tr>
+                        <th>entry</th>
+                        <th>url</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {shortenedUrls.map(item => (
+                        <UrlRow
+                            endpoint={item.endpoint}
+                            url={item.url}
+                            key={item.endpoint}
+                            selected={selected}
+                            setSelected={setSelected}
+                            triggerFetch={triggerFetch}
+                        />
+                    ))}
+                </tbody>
+            </Table>
+        </>
     );
 };
 
