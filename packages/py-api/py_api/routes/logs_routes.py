a97373a4f3e735cb764109f18c7dc5ce3e2ea0af
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-28 15:20:58 +0300

    Fix return type

--- packages/py-api/py_api/routes/logs_routes.py
+++ packages/py-api/py_api/routes/logs_routes.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
-from fastapi.responses import JSONResponse
+from fastapi.responses import FileResponse, JSONResponse
 from py_api.controllers.logs_controller import LogsController as c
 
 router = APIRouter()
 
 
 @router.get("/logs")
-async def get_logs() -> JSONResponse:
+async def get_logs() -> FileResponse | JSONResponse:
     return c.get_log_file()
