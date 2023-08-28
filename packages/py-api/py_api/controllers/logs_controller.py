f7c0664a5bb4510623d12ade5b06ef3640aa2d7e
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-08-28 15:12:24 +0300

    Encapsulate constant for file_path

--- packages/py-api/py_api/controllers/logs_controller.py
+++ packages/py-api/py_api/controllers/logs_controller.py
@@ -5,10 +5,10 @@ from typing import Final
 from fastapi.responses import FileResponse, JSONResponse
 from py_api.environment import IS_OFFLINE
 
-LOGFILE_PATH: Final = f"{Path(__file__).parent.resolve().parent}/shared/logfile.log"
-
 
 class LogsController:
+    LOGFILE_PATH: Final = f"{Path(__file__).parent.resolve().parent}/shared/logfile.log"
+
     @classmethod
     def get_log_file(cls) -> JSONResponse | FileResponse:
 
@@ -20,11 +20,11 @@ class LogsController:
 
             return JSONResponse(content=content, status_code=status_code)
 
-        if not exists(LOGFILE_PATH):
+        if not exists(cls.LOGFILE_PATH):
             return JSONResponse(content={"error": "The logs folder is empty"}, status_code=404)
 
         return FileResponse(
-            LOGFILE_PATH, headers={
+            cls.LOGFILE_PATH, headers={
                 "Content-Disposition": f"attachment; filename=logfile.log",
             },
         )
