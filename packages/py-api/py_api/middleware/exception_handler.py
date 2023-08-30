f5fff071542a0971e1597db4e56bff0c264af25f
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-08-31 01:16:42 +0300

    Initial version of POC

--- packages/py-api/py_api/middleware/exception_handler.py
+++ packages/py-api/py_api/middleware/exception_handler.py
@@ -3,6 +3,7 @@ from traceback import format_exc
 
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
+from py_api.utilities.parsers import parse_request_body
 
 
 class ExceptionHandler:
@@ -11,12 +12,18 @@ class ExceptionHandler:
         async def general_exception_handler(request: Request, exc: Exception) -> JSONResponse:
             content = {
                 "message": "Something went wrong! Please, contact The Hub!",
-            }
+            }   
+
+            body = {}
+
+            if hasattr(exc, "handle_body"):
+                body = await parse_request_body(exc.handle_body)
 
             if bool(getenv("IS_OFFLINE")):
                 content = {
                     "message": "Hey there, bud! Don't be sad! Here's some info which might help you debug your issue 🎉",
                     "exception": str(exc),
+                    "body": body,
                     "stacktrace": format_exc(),
                 }
 
--- packages/py-api/py_api/routes/utility_routes.py
+++ packages/py-api/py_api/routes/utility_routes.py
@@ -7,13 +7,33 @@ from py_api.utilities.memory import get_current_memory_usage_in_mbs
 router = APIRouter()
 logger = getLogger("health")
 
+def handle_exception(func):
+    def wrapper(*arg, **kwargs):
+        print("here")
+        request = arg[0]
+        try:
+            response = func(*arg, **kwargs)
+            print(response)
+            return response
+        except Exception as e:
+            e.handle_body = request.body
+            raise e
 
-@router.get('/health')
-async def health() -> Dict[str, Any]:
+    return wrapper
+
+@handle_exception        
+def health_controller(request: Request):
+    raise Exception('/health')
     return {
         "status": "healthy",
         "currentMemoryUsage": f"{get_current_memory_usage_in_mbs()} MB",
     }
+
+@router.get('/health')
+# @handle_exception
+async def health(request: Request) -> Dict[str, Any]:
+    result = health_controller(request)
+    return result
 
 
 @router.get('/routes')
