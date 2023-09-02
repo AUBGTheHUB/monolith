4991dc6cdf7fb660e97bd9d7796c3e7848741b4c
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-02 14:47:40 +0300

    #507 fix bug with error messages

---
+++ packages/web/src/components/admin_page/url_shortener/requests.js
@@ -0,0 +1,10 @@
+export const updateErrorMessage = (err, setErrorMessage) => {
+    if (err.code === 'ERR_NETWORK') {
+        setErrorMessage('API is not responding!');
+    } else if (err?.response?.data?.detail?.[0]?.ctx?.error) {
+        setErrorMessage('Not a viable URL - ' + err?.response?.data?.detail[0]?.ctx?.error + '!');
+    } else {
+        const message = err?.response?.data?.message ? err.response.data.message : 'Something went wrong!';
+        setErrorMessage(message);
+    }
+};
