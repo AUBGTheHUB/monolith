42c6b36329c0966bf9c770612ed60bad79d7b99b
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-04 04:22:32 +0300

    Swap tilde for comment

--- packages/services/questionnaire/src/routes/+page.svelte
+++ packages/services/questionnaire/src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     /* TODO:
         1. add a database connection - https://github.com/stolinski/svelte-kit-mongodb-example
-        2. add post endpoint in ~server.js - https://learn.svelte.dev/tutorial/post-handlers
+        2. add post endpoint in +server.js - https://learn.svelte.dev/tutorial/post-handlers
         3. add a submit button for making the POST request
     */
     import questions from "$lib/inputs/questions.json"
