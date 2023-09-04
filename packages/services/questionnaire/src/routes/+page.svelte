196df7c4fe4d4c68933dbd055f865ac88e975025
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-04 04:22:10 +0300

    Add comment for next steps

--- packages/services/questionnaire/src/routes/+page.svelte
+++ packages/services/questionnaire/src/routes/+page.svelte
@@ -1,4 +1,9 @@
 <script lang="ts">
+    /* TODO:
+        1. add a database connection - https://github.com/stolinski/svelte-kit-mongodb-example
+        2. add post endpoint in ~server.js - https://learn.svelte.dev/tutorial/post-handlers
+        3. add a submit button for making the POST request
+    */
     import questions from "$lib/inputs/questions.json"
     import Input from "$lib/inputs/input.svelte";
 
