adea21dc82b162b85a372bfac61714268ed6f218
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-07 03:26:05 +0300

    POC: Server.js

---
+++ packages/services/questionnaire/server.js
@@ -0,0 +1,20 @@
+import { handler } from './build/handler.js';
+import express from 'express';
+import fs from 'fs';
+import https from 'https';
+
+const privateKey = fs.readFileSync('./certs/devenv.key', 'utf8');
+const certificate = fs.readFileSync('./certs/devenv.crt', 'utf8');
+const credentials = { key: privateKey, cert: certificate };
+
+const app = express();
+
+const httpsServer = https.createServer(credentials, app);
+
+const SSLPORT = 6420;
+
+httpsServer.listen(SSLPORT, function () {
+    console.log('HTTPS Server is running on: https://localhost:%s', SSLPORT);
+});
+
+app.use(handler);
