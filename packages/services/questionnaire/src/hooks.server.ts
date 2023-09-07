67a6601d30a14171a62bad8b883605b6880a6039
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-08 00:11:47 +0300

    Rollback hooks message

--- packages/services/questionnaire/src/hooks.server.ts
+++ packages/services/questionnaire/src/hooks.server.ts
@@ -2,6 +2,6 @@
 export function handleError({ error }: { error: Error }) {
     console.log(error);
     return {
-        message: error,
+        message: 'Something went wrong! Are you missing a query parameter?',
     };
 }
