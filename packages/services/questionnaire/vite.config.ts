9bfa07fae72e592c3c77a02587814ed97e756b0e
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-07 12:35:00 +0300

    #487 Fix SSL certificates issue with node server and incorrect static files proxying on nginx (#523)

--- packages/services/questionnaire/vite.config.ts
+++ packages/services/questionnaire/vite.config.ts
@@ -1,14 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import fs from 'fs';
 
 export default defineConfig({
     plugins: [sveltekit()],
-    //TODO: Add check for ENV - if PROD load the certs
-    server: {
-        https: {
-            key: fs.readFileSync(`${__dirname}/devenv.key`),
-            cert: fs.readFileSync(`${__dirname}/devenv.crt`),
-        },
-    },
 });
