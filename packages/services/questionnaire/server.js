33ec5fdc9ab2aef94842bd03791c085f94ab4b09
Michael Bozhilov <michaelbozhilov@gmail.com>
2023-09-08 01:11:23 +0300

    Add cors to packages

--- packages/services/questionnaire/server.js
+++ packages/services/questionnaire/server.js
@@ -2,17 +2,14 @@ import { handler } from './build/handler.js';
 import express from 'express';
 import fs from 'fs';
 import https from 'https';
+import cors from 'cors';
 
 const privateKey = fs.readFileSync('./certs/devenv.key', 'utf8');
 const certificate = fs.readFileSync('./certs/devenv.crt', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
 const app = express();
-
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
+app.use(cors());
 
 const httpsServer = https.createServer(credentials, app);
 
