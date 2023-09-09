f137fb79f5a54ef522f36fb7b0bba12e00b51ea8
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-09-09 23:52:39 +0300

    Remove exc from py-api

--- packages/py-api/py_api/routes/utility_routes.py
+++ packages/py-api/py_api/routes/utility_routes.py
@@ -10,7 +10,6 @@ logger = getLogger("health")
 
 @router.get('/health')
 async def health() -> Dict[str, Any]:
-    raise Exception("test exception")
     return {
         "status": "healthy",
         "currentMemoryUsage": f"{get_current_memory_usage_in_mbs()} MB",
