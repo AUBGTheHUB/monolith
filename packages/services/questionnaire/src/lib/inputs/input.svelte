e0625c537753867aa3264f7fa1559572a5490eab
Aleks Ivanov <al.ivanov1908@gmail.com>
2023-09-17 00:35:06 +0300

    fix scolling on desktop

--- packages/services/questionnaire/src/lib/inputs/input.svelte
+++ packages/services/questionnaire/src/lib/inputs/input.svelte
@@ -71,29 +71,28 @@
         color: greenyellow;
     }
 
-    :global(pre) {
-        width: 100%;
-        align-self: center;
-        display: flex;
-        justify-content: center;
-    }
-    :global(code) {
-    }
-    :global(code pre) {
-        align-self: center;
-        display: flex;
-        overflow-x: auto;
-        align-self: center;
-        width: 300px;
-        justify-content: flex-start;
-    }
     :global(textarea) {
         width: 100%;
     }
     :global(h1) {
         width: 100%;
     }
-
+    @media (max-width: 900px) {
+        :global(pre) {
+            width: 100%;
+            align-self: center;
+            display: flex;
+            justify-content: center;
+        }
+        :global(code pre) {
+            align-self: center;
+            display: flex;
+            overflow-x: auto;
+            align-self: center;
+            width: 300px;
+            justify-content: flex-start;
+        }
+    }
     @media (max-width: 399px) {
         .custom-width {
             width: 100%;
