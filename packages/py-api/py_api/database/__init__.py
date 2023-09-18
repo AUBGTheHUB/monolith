b68ee166ff592c86e8b8121daf10417f7a708c0d
Nikolay Ninov <nikininov1@gmail.com>
2023-09-18 12:48:16 +0300

    Add the db collections for the questions and the answers of the questionnaires

--- packages/py-api/py_api/database/__init__.py
+++ packages/py-api/py_api/database/__init__.py
@@ -1,6 +1,8 @@
-from py_api.database.initialize import db, su_col
+from py_api.database.initialize import db, su_col, q_col, a_col
 
 __all__ = [
     "db",
     "su_col",
+    "q_col",
+    "a_col",
 ]
