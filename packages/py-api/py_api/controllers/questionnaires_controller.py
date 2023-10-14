c83ee774ac0b8d9058bad0435b361f565ce115f8
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-10-15 02:51:02 +0300

    Add Submition time with timestamp of creation as a new column

--- packages/py-api/py_api/controllers/questionnaires_controller.py
+++ packages/py-api/py_api/controllers/questionnaires_controller.py
@@ -1,8 +1,8 @@
-from fastapi.responses import JSONResponse, StreamingResponse
-
-from py_api.database import q_col, a_col
-import pandas as pd
 import io
+
+import pandas as pd
+from fastapi.responses import JSONResponse, StreamingResponse
+from py_api.database import a_col, q_col
 
 
 class QuestionnairesController:
@@ -15,12 +15,21 @@ class QuestionnairesController:
             return JSONResponse(content={"message": "Questionnaire doesn't exist"}, status_code=404)
 
         questions = [q['title'] for q in questions['questions']]
+        questions.append("Submition time")
 
         answers = a_col.find({'department': dep})
         answers = [list(a['answers'].values()) for a in answers]
 
-        docs = pd.DataFrame(answers, columns=questions).replace(
-            r'^s*$', float('NaN'), regex=True)
+        # I need to query the answers again because once I itereate over them then down below the answers list is empty
+        submission_timestamps = [
+            str(a["_id"].generation_time)
+            for a in a_col.find({'department': dep})
+        ]
+
+        data = [a + [s] for a, s in zip(answers, submission_timestamps)]
+        docs = pd.DataFrame(data, columns=questions).replace(
+            r'^s*$', float('NaN'), regex=True,
+        )
 
         docs.dropna(axis=0, how='all', inplace=True)
 
@@ -28,7 +37,8 @@ class QuestionnairesController:
         docs.to_csv(stream, index=False)
 
         response = StreamingResponse(
-            iter([stream.getvalue()]), media_type='text/csv')
+            iter([stream.getvalue()]), media_type='text/csv',
+        )
         response.headers["Content-Disposition"] = f"attachment; filename={dep}.csv"
 
         return response
