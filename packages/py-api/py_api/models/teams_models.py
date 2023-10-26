a0e9809dd067c49bcd1241fe61f17283f087047d
Aleks Ivanov <al.ivanov1908@gmail.com>
2023-10-26 20:14:27 +0300

    added team_ty[e literal

--- packages/py-api/py_api/models/teams_models.py
+++ packages/py-api/py_api/models/teams_models.py
@@ -1,13 +1,17 @@
-from typing import List, Optional
+from typing import List, Literal, Optional
 
 from pydantic import BaseModel
+
+TEAM_TYPE = Literal["known", "unknown"]
 
 
 class NewTeams(BaseModel):
     team_name: str
     team_members: List[str]
+    team_type: TEAM_TYPE
 
 
 class UpdateTeam(BaseModel):
     team_name: Optional[str] = None
     team_members: Optional[List[str]] = None
+    team_type: Optional[TEAM_TYPE] = None
