d2b7099b42cff5bb49bb73e9a484d9842c314896
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-16 16:11:11 +0200

    remove for loops as they do not reflect the real state of the object in the database

--- packages/py-api/py_api/controllers/hackathon_teams_controller.py
+++ packages/py-api/py_api/controllers/hackathon_teams_controller.py
@@ -3,7 +3,7 @@ import json
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from fastapi.responses import JSONResponse
-from py_api.database.initialize import participants_col, t_col
+from py_api.database.initialize import t_col
 from py_api.models import UpdateTeam
 from py_api.utilities.parsers import filter_none_values
 
@@ -13,37 +13,15 @@ class TeamsController:
     @staticmethod
     def fetch_teams() -> JSONResponse:
         teams = list(t_col.find())
+
         if not teams:
             return JSONResponse(content={"message": "No teams were found in db"}, status_code=404)
-
-        for team in teams:
-            team_name = team.get("team_name")
-            participants = list(
-                participants_col.find(
-                    {"team_name": team_name}, {"_id": 1},
-                ),
-            )
-            team["team_members"] = [
-                str(participant["_id"])
-                for participant in participants
-            ]
 
         return JSONResponse(content={"teams": json.loads(dumps(teams))}, status_code=200)
 
     @staticmethod
     def get_team(object_id: str) -> JSONResponse:
         specified_team = t_col.find_one(filter={"_id": ObjectId(object_id)})
-
-        team_name = specified_team.get("team_name")
-        participants = list(
-            participants_col.find(
-                {"team_name": team_name}, {"_id": 1},
-            ),
-        )
-        specified_team["team_members"] = [
-            str(participant["_id"])
-            for participant in participants
-        ]
 
         if not specified_team:
             return JSONResponse(content={"message": "The team was not found"}, status_code=404)
