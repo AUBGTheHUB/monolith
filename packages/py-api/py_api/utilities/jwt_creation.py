b5ed547f70567a9564abb40bf4d4744e9e3b31c9
Viktor Logodazhki <v.logodazhki@abv.bg>
2023-11-18 16:51:56 +0200

    Created jwt

---
+++ packages/py-api/py_api/utilities/jwt_creation.py
@@ -0,0 +1,29 @@
+import os
+from datetime import datetime, timedelta
+from typing import Any, Dict
+
+from jwt import decode, encode
+from py_api.environment import SECRET_KEY
+
+
+def create_verification_jwt_token(particiapnt: Dict[str, Any]) -> str:
+
+    is_admin: bool = particiapnt["is_admin"]
+    payload = {
+        "sub": particiapnt["_id"],
+        "is_admin": is_admin,
+        "exp": datetime.utcnow() + timedelta(hours=24),
+    }
+
+    return str(encode(payload, os.getenv("SECRET_KEY"), algorithm="HS256"))
+
+
+def invite_link(team: Dict[str, Any]) -> str:
+
+    payload = {
+        "sub": team["_id"],
+        "team_name": team["team_name"],
+        "exp": datetime.utcnow() + timedelta(hours=24),
+    }
+
+    return str(encode(payload, os.getenv("SECRET_KEY"), algorithm="HS256"))
