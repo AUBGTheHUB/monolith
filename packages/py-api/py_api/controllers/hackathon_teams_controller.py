deae7f447a4b1095a2bcdbe1bc3eb292f9da1eaa
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2023-11-18 11:17:32 +0200

    Add logic for moving members from one team to another

--- packages/py-api/py_api/controllers/hackathon_teams_controller.py
+++ packages/py-api/py_api/controllers/hackathon_teams_controller.py
@@ -1,8 +1,7 @@
 import json
 import random
 import string
-from enum import Enum
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from bson.json_util import dumps
 from bson.objectid import ObjectId
@@ -11,19 +10,18 @@ from py_api.database.initialize import t_col
 from py_api.models.hackathon_teams_models import (
     HackathonTeam,
     MoveTeamMembers,
+    TeamType,
     UpdateTeam,
 )
 from py_api.utilities.parsers import filter_none_values
 
 
-class TeamType(Enum):
-    NORMAL = "normal"
-    RANDOM = "random"
-
-
 class TeamsController:
     @classmethod
-    def create_team(cls, team_name: str, user_id: str) -> Dict[str, Any] | None:
+    def create_team(cls, team_name: str, user_id: str = "", members_list: Optional[List[str]] = None) -> (
+            Dict[str, Any] | None
+    ):
+
         team_type = TeamType.NORMAL
 
         if not team_name:
@@ -37,11 +35,19 @@ class TeamsController:
             team_type = TeamType.RANDOM
 
         team = cls.check_if_team_exists(team_name)
+        team_members = []
+
+        if user_id:
+            team_members = [user_id]
+
+        if members_list:
+            team_members = members_list
+            team_type = TeamType.RANDOM
 
         if not team:
             new_team = HackathonTeam(
                 team_name=team_name,
-                team_type=team_type, team_members=[user_id],
+                team_type=team_type, team_members=team_members,
             )
             t_col.insert_one(new_team.model_dump())
 
@@ -130,8 +136,10 @@ class TeamsController:
         move_members_dump: Dict[str, Any] = move_members_model.model_dump()
         old_team_name = move_members_dump["old_team_name"]
         new_team_name = move_members_dump["new_team_name"]
+        team_members_to_move = move_members_dump["team_members"]
 
-        if not cls.check_if_team_exists(old_team_name):
+        old_team = cls.check_if_team_exists(old_team_name)
+        if not old_team:
             return JSONResponse(
                 content={
                     "message": f"The team {old_team_name} was not found, please check the spelling!",
@@ -139,5 +147,65 @@ class TeamsController:
                 status_code=404,
             )
 
-        if not cls.check_if_team_exists(new_team_name):
-            pass
+        new_team = cls.check_if_team_exists(new_team_name)
+        if not new_team:
+            cls.create_team(new_team_name, members_list=team_members_to_move)
+            if content := cls.update_old_team_members_list(old_team, team_members_to_move):
+                return JSONResponse(content={"message": content}, status_code=404)
+
+            return JSONResponse(
+                content={
+                    "message": f"Created a new team with participants {team_members_to_move}",
+                },
+                status_code=201,
+            )
+
+        if content := cls.update_old_team_members_list(old_team, team_members_to_move):
+            return JSONResponse(content={"message": content}, status_code=404)
+
+        cls.update_new_team_members_list(new_team, team_members_to_move)
+
+        return JSONResponse(
+            content={
+                "message": f"Team members {team_members_to_move} moved successfully from team {old_team_name}"
+                           f" to team {new_team_name}",
+            },
+        )
+
+    @classmethod
+    def update_old_team_members_list(
+            cls, old_team: Dict[str, Any],
+            team_members_to_move: List[str],
+    ) -> str | None:
+        old_team_members: List[str] = old_team["team_members"]
+
+        # Remove the passed team_members from their original team
+        for team_member in team_members_to_move:
+            if team_member in old_team_members:
+                old_team_members.remove(team_member)
+            else:
+                return f"Team member with id {team_member} doesn't exist"
+
+        cls.update_team_current_members(
+            old_team["team_name"], old_team_members,
+        )
+        return None
+
+    @classmethod
+    def update_new_team_members_list(cls, new_team: Dict[str, Any], team_members_to_move: List[str]) -> None:
+        new_team_current_members: List[str] = new_team["team_members"]
+
+        # Add the passed team_members to the new team
+        for new_team_member in team_members_to_move:
+            new_team_current_members.append(new_team_member)
+
+        cls.update_team_current_members(
+            new_team["team_name"], new_team_current_members,
+        )
+
+    @classmethod
+    def update_team_current_members(cls, team_name: str, members_list: List[str]) -> None:
+        t_col.update_one(
+            {"team_name": team_name}, {
+                "$set": {"team_members": members_list},
+            }, )
