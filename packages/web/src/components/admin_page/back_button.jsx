6e3637aaf47bf130a4709de13edde651a3e1b81c
Teodor Kovachev <tedox.k@gmail.com>
2023-11-18 10:45:41 +0200

    converted css to module.css

--- packages/web/src/components/admin_page/back_button.jsx
+++ packages/web/src/components/admin_page/back_button.jsx
@@ -1,12 +1,12 @@
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
-import './back_button.css';
+import styles from './back_button.module.css';
 
 const BackBtn = ({ positionButtonOnTop = false }) => {
     const history = useNavigate();
     const className = positionButtonOnTop ? 'top' : 'middle';
     return (
-        <Button className={className} variant="primary" onClick={() => history(-1)}>
+        <Button className={styles[className]} variant="primary" onClick={() => history(-1)}>
             &#8249;
         </Button>
     );
