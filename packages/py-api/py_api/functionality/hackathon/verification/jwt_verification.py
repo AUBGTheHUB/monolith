0961ae23a63d9c184f5437f7cc633b9c67f83000
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-01-08 23:49:08 +0200

    Remove redundant jwt token generating function

--- packages/py-api/py_api/functionality/hackathon/verification/jwt_verification.py
+++ packages/py-api/py_api/functionality/hackathon/verification/jwt_verification.py
@@ -64,20 +64,9 @@ class JWTVerification:
 
 # when create verification jwt token is called it creates the token with argunment the participant
 # must be called when email is being created to create the url
-def create_token_for_verifying_admin(particiapnt: Dict[str, Any]) -> str:
-    is_admin: bool = particiapnt["is_admin"]
-    payload = {
-        "sub": str(particiapnt["_id"]),
-        "is_admin": is_admin,
-        "exp": datetime.utcnow() + timedelta(hours=24),
-    }
-    return str(encode(payload, SECRET_KEY, algorithm="HS256"))
-
-
-def create_token_for_inviting_participants(team_name: str) -> str:
+def create_jwt_token(team_name: str) -> str:
     payload = {
         "team_name": team_name,
         "exp": datetime.utcnow() + timedelta(hours=24),
     }
-
     return str(encode(payload, SECRET_KEY, algorithm="HS256"))
