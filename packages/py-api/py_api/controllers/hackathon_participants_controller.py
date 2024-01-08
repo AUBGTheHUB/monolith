4b54501700a6f53de727bd43360126ceeb95ffdb
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-01-08 03:23:34 +0200

    Add sample code of how creating a team should look like

--- packages/py-api/py_api/controllers/hackathon_participants_controller.py
+++ packages/py-api/py_api/controllers/hackathon_participants_controller.py
@@ -4,9 +4,12 @@ from bson.errors import InvalidId
 from bson.json_util import dumps
 from bson.objectid import ObjectId
 from fastapi.responses import JSONResponse
-from py_api.database.initialize import participants_col
+from py_api.controllers.hackathon_teams_controller import TeamsController
+from py_api.database.initialize import participants_col, t_col
+from py_api.functionality.hackathon.teams.teams_utility_functions import TeamsUtilities
 from py_api.models import NewParticipant, UpdateParticipant
 from py_api.utilities.parsers import filter_none_values
+from pymongo.results import InsertOneResult
 
 
 class ParticipantsController:
@@ -15,10 +18,16 @@ class ParticipantsController:
         participants = list(participants_col.find())
 
         if not participants:
-            return JSONResponse(content={"message": "No participants were found!"}, status_code=404)
+            return JSONResponse(
+                content={"message": "No participants were found!"},
+                status_code=404,
+            )
 
         # the dumps funtion from bson.json_util returns a string
-        return JSONResponse(content={"participants": json.loads(dumps(participants))}, status_code=200)
+        return JSONResponse(
+            content={"participants": json.loads(dumps(participants))},
+            status_code=200,
+        )
 
     def get_specified_participant(object_id: str) -> JSONResponse:
         try:
@@ -26,12 +35,21 @@ class ParticipantsController:
                 filter={"_id": ObjectId(object_id)},
             )
         except (InvalidId, TypeError) as e:
-            return JSONResponse(content={"message": "Invalid object_id format!"}, status_code=400)
+            return JSONResponse(
+                content={"message": "Invalid object_id format!"},
+                status_code=400,
+            )
 
         if not specified_participant:
-            return JSONResponse(content={"message": "The targeted participant was not found!"}, status_code=404)
+            return JSONResponse(
+                content={"message": "The targeted participant was not found!"},
+                status_code=404,
+            )
 
-        return JSONResponse(content={"participant": json.loads(dumps(specified_participant))}, status_code=200)
+        return JSONResponse(
+            content={"participant": json.loads(dumps(specified_participant))},
+            status_code=200,
+        )
 
     def delete_participant(object_id: str) -> JSONResponse:
         deleted_participant = participants_col.find_one_and_delete(
@@ -39,11 +57,20 @@ class ParticipantsController:
         )
 
         if not deleted_participant:
-            return JSONResponse(content={"message": "The targeted participant was not found!"}, status_code=404)
+            return JSONResponse(
+                content={"message": "The targeted participant was not found!"},
+                status_code=404,
+            )
 
-        return JSONResponse(content={"message": "The participant was deleted successfully!"}, status_code=200)
+        return JSONResponse(
+            content={"message": "The participant was deleted successfully!"},
+            status_code=200,
+        )
 
-    def update_participant(object_id: str, participant_form: UpdateParticipant) -> JSONResponse:
+    def update_participant(
+        object_id: str,
+        participant_form: UpdateParticipant,
+    ) -> JSONResponse:
 
         # filters the values set to None in the model
         fields_to_be_updated = filter_none_values(participant_form)
@@ -56,13 +83,43 @@ class ParticipantsController:
             return_document=True,
         )
 
-        return JSONResponse(content={"participant": json.loads(dumps(to_be_updated_participant))}, status_code=200)
+        return JSONResponse(
+            content={
+                "participant": json.loads(dumps(to_be_updated_participant)),
+            },
+            status_code=200,
+        )
 
-    def add_participant(paricipant_model: NewParticipant) -> JSONResponse:
-        participant_form_dump = paricipant_model.model_dump()
+    def add_participant(paricipant: NewParticipant) -> JSONResponse:
 
-        if participants_col.find_one(filter={"email": participant_form_dump["email"]}):
-            return JSONResponse(content={"message": "The email of the participant already exists!"}, status_code=409)
+        if participants_col.find_one(filter={"email": paricipant.email}):
+            return JSONResponse(
+                content={
+                    "message": "The email of the participant already exists!",
+                },
+                status_code=409,
+            )
 
-        participants_col.insert_one(participant_form_dump)
-        return JSONResponse(content={"message": "The participant was successfully added!"}, status_code=200)
+        insert_result: InsertOneResult = participants_col.insert_one(
+            paricipant.model_dump(),
+        )
+
+        # This is a code sample for how creating a team works
+        # new_team = TeamsUtilities.create_team(str(insert_result.inserted_id),
+        #                                       paricipant.team_name)
+        # if not new_team:
+        #     updated_team = TeamsUtilities.add_participant_to_team(
+        #         paricipant.team_name,
+        #         str(insert_result.inserted_id))
+        #
+        #     TeamsController.update_team(
+        #         TeamsUtilities.get_team_id_by_team_name(
+        #             updated_team.team_name),
+        #         updated_team)
+        # else:
+        #     TeamsUtilities.add_team_to_db(team=new_team)
+
+        return JSONResponse(
+            content={"message": "The participant was successfully added!"},
+            status_code=200,
+        )
