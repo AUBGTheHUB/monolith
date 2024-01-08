8680ef3a4e5e0b169d1e4775cfb3b50eb38cdd56
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-01-08 23:47:20 +0200

    Rename token generating functions

--- packages/py-api/py_api/functionality/hackathon/verification/jwt_verification.py
+++ packages/py-api/py_api/functionality/hackathon/verification/jwt_verification.py
@@ -64,7 +64,7 @@ class JWTVerification:
 
 # when create verification jwt token is called it creates the token with argunment the participant
 # must be called when email is being created to create the url
-def create_verification_jwt_token(particiapnt: Dict[str, Any]) -> str:
+def create_token_for_verifying_admin(particiapnt: Dict[str, Any]) -> str:
     is_admin: bool = particiapnt["is_admin"]
     payload = {
         "sub": str(particiapnt["_id"]),
@@ -74,7 +74,7 @@ def create_verification_jwt_token(particiapnt: Dict[str, Any]) -> str:
     return str(encode(payload, SECRET_KEY, algorithm="HS256"))
 
 
-def create_invite_link(team_name: str) -> str:
+def create_token_for_inviting_participants(team_name: str) -> str:
     payload = {
         "team_name": team_name,
         "exp": datetime.utcnow() + timedelta(hours=24),
