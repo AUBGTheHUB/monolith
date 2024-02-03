43fe425ab547b9955dc74f5471beedcd7df0c497
Isak Bego <isakbego@gmail.com>
2024-02-03 17:07:48 +0200

    Made changes to the mobile version

--- packages/web/src/components/spa/HackAUBG/JourneySection/JourneyDesktop/JourneyDesktop.jsx
+++ packages/web/src/components/spa/HackAUBG/JourneySection/JourneyDesktop/JourneyDesktop.jsx
@@ -5,15 +5,17 @@ import { JourneyStep } from './JourneyStep';
 
 export const JourneyDesktop = () => {
     return (
-        <div className="journey-desktop-section">
-            <div className="journey-desktop-title">
-                <h1>The HackAUBG Journey</h1>
+        <>
+            <div className="journey-desktop-section">
+                <div className="journey-desktop-title">
+                    <h1>The HackAUBG Journey</h1>
+                </div>
+                <div className="journey-desktop-content">
+                    {stepsData.map(stepData => (
+                        <JourneyStep props={stepData} key={stepData.title} />
+                    ))}
+                </div>
             </div>
-            <div className="journey-desktop-content">
-                {stepsData.map(stepData => (
-                    <JourneyStep props={stepData} key={stepData.title} />
-                ))}
-            </div>
-        </div>
+        </>
     );
 };
