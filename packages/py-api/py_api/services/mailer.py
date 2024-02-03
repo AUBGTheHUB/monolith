a63cc780afd68eb754f4d56b94ff21b05482e16c
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-02-03 10:16:25 +0200

    Extract email vas to environment.py

--- packages/py-api/py_api/services/mailer.py
+++ packages/py-api/py_api/services/mailer.py
@@ -1,14 +1,14 @@
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from os import environ
 
 from aiosmtplib import SMTP
+from py_api.environment import HUB_MAIL, HUB_PSW
 
 
 async def send_mail(receiver: str, subject: str, html: str) -> None:
     port = 465  # SSL
-    email = environ["HUB_MAIL"]
-    password = environ["HUB_PSW"]
+    email = HUB_MAIL
+    password = HUB_PSW
 
     mime_msg = MIMEMultipart('alternative')
 
