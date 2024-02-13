8c28a590673003582625899998dbbfb899a11eb2
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-02-13 20:40:08 +0200

    remove comments

--- packages/web/src/components/spa/HackAUBG/VideoSection/VideoSection.jsx
+++ packages/web/src/components/spa/HackAUBG/VideoSection/VideoSection.jsx
@@ -10,7 +10,6 @@ export const VideoSection = () => {
                 width="853"
                 height="480"
                 src={'https://www.youtube.com/embed/avP5xho4Yk0'}
-                // frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="Embedded youtube"
