02f4ad3585c53fc8ab4167f363985f32c5c2648b
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-02-17 21:46:35 +0200

    added inital struct

--- packages/web/src/components/spa/HackAUBG/GradingCriteria/GradingCriteria.jsx
+++ packages/web/src/components/spa/HackAUBG/GradingCriteria/GradingCriteria.jsx
@@ -1,51 +1,14 @@
-import React from 'react';
-import './grading_criteria.css';
+import styles from './garding_criteria.module.css';
 
 export const GradingCriteria = () => {
     return (
-        <div className="grading-criteria-container" id="grading">
-            <h1>GRADING CRITERIA</h1>
-            <div className="grading-criteria-table">
-                <div className="grading-criteria-column">
-                    <h2>
-                        PROJECT IDEA<br></br> (15 points)
-                    </h2>
-                    <p>Innovative idea and originality (7 points)</p>
-                    <p>Market Research (6 points)</p>
-                    <p>Usage of external data to verify the idea (2 points)</p>
+        <div className={styles.grading_criteria}>
+            <div className={styles.grading_criteria_row}>
+                <div className={styles.grading_criteria_cell}>
+                    <h2>PROJECT IDEA</h2>
                 </div>
-                <div className="grading-criteria-column">
-                    <h2>
-                        PROJECT REALIZATION<br></br> (30 points)
-                    </h2>
-                    <p>UI/UX Design (10 points)</p>
-                    <p>Scalability (10 points)</p>
-                    <p>Project deployment (6 points)</p>
-                    <p>Structured git repository (4 points)</p>
-                </div>
-                <div className="grading-criteria-column">
-                    <h2>
-                        COMPLEXITY OF THE PROJECT<br></br> (40 points)
-                    </h2>
-                    <p>Code originality (15 points)</p>
-                    <p>Suitable technology stack (10 points)</p>
-                    <p>Clear coding style (10 points)</p>
-                    <p>Security (5 points)</p>
-                </div>
-                <div className="grading-criteria-column">
-                    <h2>
-                        PRESENTATION<br></br> (15 points)
-                    </h2>
-                    <p>Coherency (5 points)</p>
-                    <p>
-                        Clear explanation and defense of project ideas (5
-                        points)
-                    </p>
-                    <p>Demo (3 points)</p>
-                    <p>
-                        Demonstration and explanation of the most complex
-                        feature of the project (2 points)
-                    </p>
+                <div className={styles.grading_criteria_cell}>
+                    <h2>PROJECT REALIZATION</h2>
                 </div>
             </div>
         </div>
