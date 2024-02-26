dbe36f483f43a8ff6291d3439fe484ad4822c8cb
Teodor Kovachev <tedox.k@gmail.com>
2024-02-27 01:29:51 +0200

    gave alex glasses

--- packages/web/src/components/spa/HackAUBG/Carousel.jsx
+++ packages/web/src/components/spa/HackAUBG/Carousel.jsx
@@ -7,40 +7,34 @@ import mentorsStyles from './MentorsSection/mentors_section.module.css';
 const SampleNextArrow = props => {
     const { className, style, onClick } = props;
     return (
-        <svg
+        <img
             onClick={onClick}
             className={className}
-            style={{ ...style, display: 'flex' }}
-            width="44"
-            height="44"
-            viewBox="0 0 44 44"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M42.1666 20.1673V23.834H40.3333V25.6673H38.4999V27.5007H36.6666V29.334H34.8333V31.1673H32.9999V33.0007H31.1666V34.834H29.3333V36.6673H27.4999V38.5007H25.6666V40.334H23.8333V42.1673H21.9999V40.334H20.1666V38.5007H18.3333V36.6673H20.1666V34.834H21.9999V33.0007H23.8333V31.1673H25.6666V29.334H27.4999V27.5007H29.3333V25.6673H1.83325V18.334H29.3333V16.5007H27.4999V14.6673H25.6666V12.834H23.8333V11.0007H21.9999V9.16732H20.1666V7.33398H18.3333V5.50065H20.1666V3.66732H21.9999V1.83398H23.8333V3.66732H25.6666V5.50065H27.4999V7.33398H29.3333V9.16732H31.1666V11.0007H32.9999V12.834H34.8333V14.6673H36.6666V16.5007H38.4999V18.334H40.3333V20.1673H42.1666Z"
-                fill="#FB4298"
-            />
-        </svg>
+            style={{
+                ...style,
+                display: 'block',
+                width: '30px',
+                height: '30px',
+                right: '-4vw',
+            }}
+            src="arrow-right.png"></img>
     );
 };
 
 const SamplePrevArrow = props => {
     const { className, style, onClick } = props;
     return (
-        <svg
+        <img
             onClick={onClick}
             className={className}
-            style={{ ...style, display: 'flex', transform: 'rotate(180deg)' }}
-            width="44"
-            height="44"
-            viewBox="0 0 44 44"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg">
-            <path
-                d="M42.1666 20.1673V23.834H40.3333V25.6673H38.4999V27.5007H36.6666V29.334H34.8333V31.1673H32.9999V33.0007H31.1666V34.834H29.3333V36.6673H27.4999V38.5007H25.6666V40.334H23.8333V42.1673H21.9999V40.334H20.1666V38.5007H18.3333V36.6673H20.1666V34.834H21.9999V33.0007H23.8333V31.1673H25.6666V29.334H27.4999V27.5007H29.3333V25.6673H1.83325V18.334H29.3333V16.5007H27.4999V14.6673H25.6666V12.834H23.8333V11.0007H21.9999V9.16732H20.1666V7.33398H18.3333V5.50065H20.1666V3.66732H21.9999V1.83398H23.8333V3.66732H25.6666V5.50065H27.4999V7.33398H29.3333V9.16732H31.1666V11.0007H32.9999V12.834H34.8333V14.6673H36.6666V16.5007H38.4999V18.334H40.3333V20.1673H42.1666Z"
-                fill="#FB4298"
-            />
-        </svg>
+            style={{
+                ...style,
+                display: 'block',
+                width: '30px',
+                height: '30px',
+                left: '-4vw',
+            }}
+            src="arrow-left.png"></img>
     );
 };
 
@@ -64,7 +58,6 @@ export default class SimpleSlider extends Component {
                         className={mentorsStyles['tedko-image']}
                         style={{
                             objectFit: 'cover',
-                            margin: '10px',
                         }}
                         src={data.profilepicture}
                     />
@@ -105,14 +98,14 @@ export default class SimpleSlider extends Component {
                     },
                 },
                 {
-                    breakpoint: 700,
+                    breakpoint: 900,
                     settings: {
                         slidesToShow: 2,
                         slidesToScroll: 2,
                     },
                 },
                 {
-                    breakpoint: 480,
+                    breakpoint: 700,
                     settings: {
                         slidesToShow: 1,
                         slidesToScroll: 1,
