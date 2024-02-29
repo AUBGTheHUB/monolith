eafb5d206c934650af7adfe175d4fa4d7a07730e
Aleks Ivanov <al.ivanov1908@gmail.com>
2024-02-29 12:47:14 +0200

    remove unused useeffect

--- packages/web/src/components/spa/Navigation/DesktopNav/NavDesktop.jsx
+++ packages/web/src/components/spa/Navigation/DesktopNav/NavDesktop.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './desktop_navbar.css';
 import { navigateTo } from '../../../../Global';
 import { Link } from 'react-router-dom';
