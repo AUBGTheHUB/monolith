1a7cd531bdcf7f4cb02a2bcc17865be6863cf15d
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-03-15 18:54:48 +0200

    Add logic for adding random participants to existing teams

--- packages/py-api/py_api/controllers/hackathon/verification_controller.py
+++ packages/py-api/py_api/controllers/hackathon/verification_controller.py
@@ -4,12 +4,15 @@ from fastapi.responses import JSONResponse
 from py_api.database.database_transaction_handlers import (
     handle_database_session_transaction,
 )
-from py_api.functionality.hackathon.jwt_base import JWTFunctionality
+from py_api.functionality.hackathon.jwt_base import JWTFunctionality, JWTType
 from py_api.functionality.hackathon.mailing_functionality import MailingFunctionality
 from py_api.functionality.hackathon.participants_base import ParticipantsFunctionality
 from py_api.functionality.hackathon.teams_base import TeamFunctionality
 from py_api.models import HackathonTeam
-from py_api.models.hackathon_participants_models import UpdateParticipant
+from py_api.models.hackathon_participants_models import (
+    NewParticipant,
+    UpdateParticipant,
+)
 from pymongo.client_session import ClientSession
 from starlette.background import BackgroundTasks
 
@@ -17,7 +20,7 @@ from starlette.background import BackgroundTasks
 class VerificationController:
     @classmethod
     @handle_database_session_transaction
-    def verify_participants(cls, jwt_token: str, session: ClientSession) -> JSONResponse | Dict[str, str]:
+    def verify_participants(cls, jwt_token: str, session: ClientSession) -> JSONResponse:
         background_tasks = BackgroundTasks()
 
         result = JWTFunctionality.decode_token(jwt_token)
@@ -49,117 +52,85 @@ class VerificationController:
         )
 
         if decoded_token.get("team_name") is None and decoded_token.get("random_participant") is True:
-            new_random_team = cls.create_new_team(
-                new_participant_object_id=str(decoded_token.get("sub")), session=session, generate_random_team=True,
-                team_name=None,
+            # Fetch the teams of type random
+            random_teams = TeamFunctionality.fetch_teams_by_condition(
+                {"team_type": "random"},
             )
 
-            # A confirmation email is send to the random participant
-            background_tasks.add_task(
-                MailingFunctionality.send_confirmation_email, email=verified_participant.get(
-                    "email",
-                ),
-                jwt_token=jwt_token,
-                team_name=None,
-                is_admin=False,
-                participant_name=verified_participant.get("first_name"),
-            )
+            # Find the teams which can accept a new participant and add it if there is space
+            for team in random_teams:
+                if len(team.team_members) < 6:
+                    return cls.add_participant_to_existing_team(
+                        verified_participant, team, session,
+                        decoded_token, background_tasks, jwt_token,
+                    )
 
-            return JSONResponse(
-                content={
-                    "message": f"Random participant was successfully verified and has been added to team "
-                               f"{new_random_team.team_name}",
-                },
-                status_code=200,
-                background=background_tasks,
+            # Create a new random team if needed
+            return cls.create_team_and_send_email(
+                decoded_token, session, background_tasks, verified_participant, True, jwt_token,
             )
 
         if decoded_token.get("team_name") and decoded_token.get("random_participant") is False and decoded_token.get(
                 "invite",
         ) is False:
-            new_team = cls.create_new_team(
-                new_participant_object_id=str(decoded_token.get("sub")), session=session, generate_random_team=False,
-                team_name=decoded_token.get("team_name"),
-            )
-
             # A confirmation email is send to the admin along with the invite link
             jwt_token = JWTFunctionality.create_jwt_token(
-                team_name=new_team.team_name, is_invite=True,
+                team_name=decoded_token.get("team_name"), is_invite=True,
+            )
+            return cls.create_team_and_send_email(
+                decoded_token, session, background_tasks, verified_participant, False, jwt_token,
             )
 
-            background_tasks.add_task(
-                MailingFunctionality.send_confirmation_email, email=verified_participant.get(
-                    "email",
-                ),
-                jwt_token=jwt_token,
-                team_name=verified_participant.get("team_name"),
-                participant_name=verified_participant.get("first_name"), is_admin=True,
-            )
+    @classmethod
+    def create_team_and_send_email(
+        cls, decoded_token: JWTType, session: ClientSession,
+        background_tasks: BackgroundTasks,
+        verified_participant: Dict[str, Any], generate_random_team: bool,
+        jwt_token: str,
+    ) -> JSONResponse:
+        new_team = cls.create_new_team(
+            new_participant_object_id=str(decoded_token.get("sub")), session=session,
+            generate_random_team=generate_random_team,
+            team_name=decoded_token.get("team_name"),
+        )
 
-            return JSONResponse(
-                content={
-                    "message": f"Admin participant was successfully verified and has been added to team "
-                               f"{new_team.team_name}",
-                },
-                status_code=200,
-                background=background_tasks,
+        if generate_random_team:
+            cls.send_confirmation_email(
+                background_tasks, verified_participant, jwt_token, None,
             )
-
-        # team = TeamFunctionality.fetch_team(
-        #     team_name=decoded_token.get("team_name"),
-        # )
-        # if not team:
-        #     return JSONResponse(
-        #         content={
-        #             "message": "Cannot verify participant as such team doesn't exist",
-        #         },
-        #         status_code=404,
-        #     )
-        #
-        # if team.is_verified is not True:
-        #     team.is_verified = True
-        #     TeamFunctionality.update_team_query_using_dump(
-        #         team_payload=team.model_dump(),
-        #     )
-        #
-        # try:
-        #     if team.team_type == team.team_type.NORMAL:
-        #         # A confirmation email is send to the admin along with the invite link
-        #         jwt_token = JWTFunctionality.create_jwt_token(
-        #             team_name=team.team_name, is_invite=True,
-        #         )
-        #
-        #         background_tasks.add_task(
-        #             MailingFunctionality.send_confirmation_email, email=verified_participant.get(
-        #                 "email",
-        #             ),
-        #             jwt_token=jwt_token,
-        #             team_name=verified_participant.get("team_name"),
-        #             participant_name=verified_participant.get("first_name"), is_admin=True,
-        #         )
-        #
-        #     else:
-        # # A confirmation email is send to the random participant
-        #
-        # except Exception as e:
-        #     return JSONResponse(
-        #         content={"error": str(e)},
-        #         status_code=500,
-        #     )
+        else:
+            cls.send_confirmation_email(
+                background_tasks, verified_participant, jwt_token, new_team.team_name,
+            )
 
         return JSONResponse(
-            content={"message": "Participant was successfully verified"}, status_code=200,
+            content={
+                "message": f"Participant was successfully verified and has been added to team "
+                           f"{new_team.team_name}",
+            },
+            status_code=200,
             background=background_tasks,
         )
 
     @classmethod
-    def test_controller(cls, team_name: str) -> Any:
-        return {"token": JWTFunctionality.create_jwt_token(team_name)}
+    def send_confirmation_email(
+        cls, background_tasks: BackgroundTasks, verified_participant: Dict[str, Any],
+        jwt_token: str,
+        team_name: str | None,
+    ) -> None:
+        background_tasks.add_task(
+            MailingFunctionality.send_confirmation_email, email=verified_participant.get(
+                "email",
+            ),
+            jwt_token=jwt_token,
+            team_name=team_name,
+            participant_name=verified_participant.get("first_name"), is_admin=verified_participant.get("is_admin"),
+        )
 
     @classmethod
     def create_new_team(
-        cls, new_participant_object_id: str, session: ClientSession,
-        generate_random_team: bool, team_name: str | None = None,
+            cls, new_participant_object_id: str, session: ClientSession,
+            generate_random_team: bool, team_name: str | None = None,
     ) -> HackathonTeam:
         """
         Creates a new random team with the participant and inserts it into the database
@@ -175,3 +146,27 @@ class VerificationController:
         TeamFunctionality.insert_team(new_team_obj, session)
 
         return new_team_obj
+
+    @classmethod
+    def add_participant_to_existing_team(
+        cls, participant: Dict[str, Any], existing_team: HackathonTeam,
+        session: ClientSession, decoded_token: JWTType,
+        background_tasks: BackgroundTasks, jwt_token: str,
+        is_invite: bool = False,
+    ) -> JSONResponse:
+
+        result = TeamFunctionality.add_participant_to_team_object(
+            existing_team.team_name, decoded_token["sub"],
+        )
+
+        if not isinstance(result, HackathonTeam):
+            return JSONResponse(content=result[0], status_code=result[1])
+
+        existing_team = TeamFunctionality.update_team_query_using_dump(
+            result.model_dump(), session=session,
+        )
+        cls.send_confirmation_email(
+            background_tasks, participant, jwt_token, None,
+        )
+
+        return JSONResponse(content=existing_team.model_dump(), status_code=200, background=background_tasks)
