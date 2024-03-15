48d9d1f92569a73a198eba0e068d82ec9c9644de
Ivan Obeshkov <ivanobreshkov12@gmail.com>
2024-03-15 17:24:16 +0200

    Refactor handling of transactions

--- packages/py-api/py_api/database/database_transaction_handlers.py
+++ packages/py-api/py_api/database/database_transaction_handlers.py
@@ -1,20 +1,18 @@
 from functools import wraps
 from typing import Any, Callable
 
-from fastapi.responses import JSONResponse
 from py_api.database.initialize import client
 
 
+# https://pymongo.readthedocs.io/en/stable/api/pymongo/client_session.html
 def handle_database_session_transaction(func: Callable[..., Any]) -> Callable[..., Any]:
     @wraps(func)
     def wrapper(*args: Any, **kwargs: Any) -> Any:
-        try:
-            # Assuming `client` is an instance of MongoClient
-            with client.start_session() as session:
-                with session.start_transaction():
-                    return func(*args, session=session, **kwargs)
-
-        except Exception as e:
-            return JSONResponse(content={"error": str(e)}, status_code=500)
+        """Upon normal completion of with session.start_transaction() block, the transaction automatically
+        calls ClientSession.commit_transaction().
+        If the block exits with an exception, the transaction automatically calls ClientSession.abort_transaction()."""
+        with client.start_session() as session:
+            with session.start_transaction():
+                return func(*args, session=session, **kwargs)
 
     return wrapper
