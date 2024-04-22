503d4bc320db8c2579e7326d1a6c55f245cf51c0
Teodor Kovachev <tedox.k@gmail.com>
2024-04-22 13:48:47 +0300

    cleaned up code

--- packages/web/src/components/spa/MembersSection/MemberPage.jsx
+++ packages/web/src/components/spa/MembersSection/MemberPage.jsx
@@ -1,23 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {
-    // checkBrowserValid,
-    url,
-} from '../../../Global';
-//import { Custom } from 'react-hovering-cards-carousel';
+import { url } from '../../../Global';
 import SampleSlider from './Carousel';
-//import { MembersCard } from './MembersCard.old';
 import { useMediaQuery } from 'react-responsive';
 import style from './members.module.css';
 
 export const MembersSection = () => {
     const [members, setMembers] = useState();
-    // eslint-disable-next-line
 
     const three = useMediaQuery({ query: '(max-width: 1400px)' });
     const two = useMediaQuery({ query: '(max-width: 1000px)' });
     const one = useMediaQuery({ query: '(max-width: 800px)' });
-    //const isFoldRes = useMediaQuery({ query: '(max-width: 350px)' });
 
     const getMembers = () => {
         axios({
@@ -25,10 +18,6 @@ export const MembersSection = () => {
             url: url + '/api/members',
         })
             .then(res => {
-                // let localMembers = [];
-                // res.data.data.data.map(member => {
-                //     localMembers.push(new Custom(member.profilepicture, <MembersCard prop={member} />));
-                // });
                 setMembers(res.data.data.data);
             })
             // eslint-disable-next-line no-unused-vars
