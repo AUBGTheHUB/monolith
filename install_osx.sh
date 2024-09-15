f25e5f532c65fb07cd8ed6bfc60ec77035043778
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2024-09-15 16:48:28 +0300

    remove unused eslint

--- install_osx.sh
+++ install_osx.sh
@@ -9,7 +9,7 @@ if [ "$1" != "--post" ]; then
     [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion' >> ~/.zshrc
 
 	brew install wget
- 
+
 	cd $HOME/Downloads
  
 	if [[ $(uname -m) == 'arm64' ]]
