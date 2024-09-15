1f60a72f1558b6b3f03b497698f3889e3b0d6fa3
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2024-09-15 15:59:15 +0300

    test

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -1,19 +1,18 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
     const [count, setCount] = useState(0);
-    const a = 10;
 
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
