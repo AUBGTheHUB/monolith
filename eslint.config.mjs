117ef2228d247b0b4cd1d259e7da54be67d7364e
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2024-09-15 16:00:46 +0300

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,8 +6,14 @@ import pluginReact from "eslint-plugin-react";
 
 export default [
   {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+    {
+    rules: {
+      "react/react-in-jsx-scope": "off"
+    }
+  }
 ];
