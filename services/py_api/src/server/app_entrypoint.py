0ff0bf94a6d60af325b97758e03b082a808424e4
Ivan Obreshkov <ivanobreshkov12>
2024-09-15 21:07:21 +0300

    Extend comment about create_app

--- services/py_api/src/server/app_entrypoint.py
+++ services/py_api/src/server/app_entrypoint.py
@@ -2,7 +2,7 @@ from src.server.config.app_factory import create_app
 from src.server.config.server_config import start
 
 # This app is not created in the main guard as uvicorn's run method expects the app passed as an import string to
-# enable 'reload' or 'workers'
+# enable 'reload' or 'workers'. Also, we need it to create the AsyncTestClient.
 app = create_app()
 
 if __name__ == "__main__":
