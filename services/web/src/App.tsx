dc634da53e23b3e1f2b7fd03db2c53b8d0e3ab47
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2024-09-15 17:31:11 +0300

    added shadcn component for test

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@radix-ui/react-accordion';
 
 function App() {
     const [count, setCount] = useState(2);
@@ -24,6 +25,12 @@ function App() {
                 </p>
             </div>
             <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+            <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </>
     );
 }
