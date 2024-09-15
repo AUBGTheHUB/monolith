29ca1c96c9fffcba7bb677d1e8b540bb38012e10
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2024-09-15 16:34:04 +0300

    test

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -4,7 +4,7 @@ import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-    const [count, setCount] = useState(1);
+    const [count, setCount] = useState(2);
 
     return (
         <>
