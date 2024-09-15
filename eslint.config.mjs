df5c01c83f04c2c40c422d41dd4496755541c888
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2024-09-15 16:22:46 +0300

    test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,18 +2,25 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import pluginUnusedImports from "eslint-plugin-unused-imports";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-    {
+  {
+    plugins: {
+      "unused-imports": pluginUnusedImports
+    },
     rules: {
-      "react/react-in-jsx-scope": "off"
+      "react/react-in-jsx-scope": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        { "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+      ]
     }
   }
 ];
--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -2,7 +2,6 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import React from 'react';
 
 function App() {
     const [count, setCount] = useState(0);
@@ -19,7 +18,7 @@ function App() {
             </div>
             <h1>Vite + React</h1>
             <div className="card">
-                <button onClick={() => setCount(count => count + 1)}>count is {count}</button>
+                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
                 <p>
                     Edit <code>src/App.tsx</code> and save to test HMR
                 </p>
