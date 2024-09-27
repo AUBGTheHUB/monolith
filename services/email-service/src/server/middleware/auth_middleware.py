b45c2a5fa2b26e31aebdd3907d6afc31bcc2b541
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-09-28 02:10:32 +0300

    Bypass ping endpoint

--- services/email-service/src/server/middleware/auth_middleware.py
+++ services/email-service/src/server/middleware/auth_middleware.py
@@ -1,5 +1,5 @@
 import os
-from typing import Callable, Any
+from typing import Callable, Any, Final, Tuple, Literal
 
 from fastapi import FastAPI
 from starlette.requests import Request
@@ -9,9 +9,16 @@ from starlette.responses import JSONResponse
 class AuthMiddleware:
     """This middleware is responsible for checking if the requests are coming from PY-API"""
 
+    _BYPASSED_ENDPOINTS: Final = {"/ping": ["GET"]}
+
     def __init__(self, app: FastAPI) -> None:
         @app.middleware("http")
         async def verify_request(request: Request, call_next: Callable[[Any], Any]) -> JSONResponse:
+            _, is_bypassed = self._check_bypassed_endpoint(request.url)
+            if is_bypassed:
+                response = await call_next(request)
+                return response
+
             if os.environ["ENV"] != "PROD":
                 if request.headers.get("X-PY-API-MACHINE-AUTHORIZATION") == "DEV":
                     response = await call_next(request)
@@ -21,3 +28,10 @@ class AuthMiddleware:
             else:
                 # TODO: Implement computing digest of request and comparing it with the incoming one
                 pass
+
+    @classmethod
+    def _check_bypassed_endpoint(cls, url: Request.url) -> Tuple[str, Literal[True]] | Tuple[None, Literal[False]]:
+        for endpoint in cls._BYPASSED_ENDPOINTS.keys():
+            if endpoint in str(url):
+                return endpoint, True
+        return None, False
