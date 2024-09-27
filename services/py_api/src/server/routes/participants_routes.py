2873040a96d4f9d40c9018404cc1c490a7a8f09d
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-09-27 17:04:40 +0300

    Update deps wiring

--- services/py_api/src/server/routes/participants_routes.py
+++ services/py_api/src/server/routes/participants_routes.py
@@ -1,33 +1,49 @@
 from fastapi import APIRouter, Depends
 from starlette.responses import Response
 
-from src.database.db_manager import DB_MANAGER
-from src.database.query_manager import QueryManager
-from src.database.repositories.participants_repository import ParticipantsRepository
+from src.database.db_manager import DB_MANAGER, PARTICIPANTS_COLLECTION, TEAMS_COLLECTION
+from src.database.repository.participants_repository import ParticipantsRepository
+from src.database.repository.teams_repository import TeamsRepository
+from src.database.transaction_manager import TransactionManager
 from src.server.handlers.participants_handlers import ParticipantHandlers
 from src.server.schemas.request_schemas.schemas import ParticipantRequestBody
-from src.server.schemas.response_schemas.schemas import ParticipantResponse, ErrResponse
+from src.server.schemas.response_schemas.schemas import AdminParticipantRegisteredResponse, ErrResponse
+from src.service.participants_service import ParticipantService
 
 participants_router = APIRouter(prefix="/hackathon/participants")
-COLLECTION = "participants"
 
 
 # https://fastapi.tiangolo.com/tutorial/dependencies/sub-dependencies/
-def _create_query_manager(db_manager: DB_MANAGER) -> QueryManager:
-    return QueryManager(db_manager, COLLECTION)
+# Dependency wiring
 
 
-def _create_repository(query_manager: QueryManager = Depends(_create_query_manager)) -> ParticipantsRepository:
-    return ParticipantsRepository(query_manager)
+def _p_repo(db_manager: DB_MANAGER) -> ParticipantsRepository:
+    return ParticipantsRepository(db_manager, PARTICIPANTS_COLLECTION)
 
 
-def _create_handler(repository: ParticipantsRepository = Depends(_create_repository)) -> ParticipantHandlers:
-    return ParticipantHandlers(repository)
+def _t_repo(db_manager: DB_MANAGER) -> TeamsRepository:
+    return TeamsRepository(db_manager, TEAMS_COLLECTION)
+
+
+def _tx_manager(db_manager: DB_MANAGER) -> TransactionManager:
+    return TransactionManager(db_manager)
+
+
+def _service(
+    p_repo: ParticipantsRepository = Depends(_p_repo),
+    t_repo: TeamsRepository = Depends(_t_repo),
+    tx_manager: TransactionManager = Depends(_tx_manager),
+) -> ParticipantService:
+    return ParticipantService(p_repo, t_repo, tx_manager)
+
+
+def _create_handler(service: ParticipantService = Depends(_service)) -> ParticipantHandlers:
+    return ParticipantHandlers(service)
 
 
 @participants_router.post("")
 async def create_participant(
-    response: Response, req_body: ParticipantRequestBody, handler: ParticipantHandlers = Depends(_create_handler)
-) -> ParticipantResponse | ErrResponse:
+    response: Response, input_data: ParticipantRequestBody, handler: ParticipantHandlers = Depends(_create_handler)
+) -> AdminParticipantRegisteredResponse | ErrResponse:
     # TODO: Add docs
-    return await handler.create_participant(response, req_body)
+    return await handler.create_participant(response, input_data)
