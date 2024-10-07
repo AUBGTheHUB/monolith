c2f5f08237741c90057394088566612c86c3f99c
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-10-07 15:09:40 +0300

    #711 Add docs more docs to conn pool and set a lower size of minPoolSize

--- services/py-api/src/database/db_manager.py
+++ services/py-api/src/database/db_manager.py
@@ -1,4 +1,5 @@
 import os
+from math import ceil
 from typing import Annotated
 
 from fastapi import Depends
@@ -21,13 +22,19 @@ class DatabaseManager(metaclass=SingletonMeta):
     _DB_NAME = "TheHubDEV"
 
     def __init__(self) -> None:
-        # The mongo client has a conn pool under the hood, and we set a min number of idle conn that the pool has
-        # to maintain, the default is 0. After maxIdleTimeMS the connection pool replaces the idle conn with a new one.
-        # I set a min number of conn according to the article below. The connection is also removed too fast, after
-        # around 30 sec, that's why I increased maxIdleTimeMS to 5 min.
+        # The mongo client has a conn pool under the hood. We set a min number of idle connections that the pool has
+        # to maintain, the default is 0. This is in order to have some connections ready to be used instead of waiting
+        # for a socket conn to be opened.
+        # After maxIdleTimeMS the connection pool replaces the idle conn with a new one. By default, the value is 0
+        # which means a connection can remain idle indefinitely, but this can cause the connection to become stale.
+        # The config values are set according to the articles below.
+        # https://pymongo.readthedocs.io/en/stable/api/pymongo/mongo_client.html
         # https://alexedwards.net/blog/configuring-sqldb
+        # https://medium.com/@dhanushkasampath.mtr/what-are-the-default-values-for-hikari-connection-pool-if-we-do-not-override-in-application-properti-11932cdbe321
         # https://www.mongodb.com/docs/languages/python/pymongo-driver/current/faq/#how-does-connection-pooling-work-in-pymongo-
-        self._client = AsyncIOMotorClient(host=os.environ["DATABASE_URL"], minPoolSize=25, maxIdleTimeMS=5 * 60 * 1000)
+        self._client = AsyncIOMotorClient(
+            host=os.environ["DATABASE_URL"], minPoolSize=ceil(0.1 * 25), maxConnecting=25, maxIdleTimeMS=5 * 60 * 1000
+        )
 
     def close_all_connections(self) -> Err[str]:
         """Closes all connections in the pool managed by Mongo"""
