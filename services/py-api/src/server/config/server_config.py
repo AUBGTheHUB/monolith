95df2236ddf5db42bebd3f0c231f3fd0252fa0f4
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-10-15 21:50:12 +0300

    Add more docs

--- services/py-api/src/server/config/server_config.py
+++ services/py-api/src/server/config/server_config.py
@@ -27,7 +27,7 @@ def start() -> None:
     """Starts the Uvicorn server with different config based on the env we are in"""
     server_config = load_server_config()
 
-    if server_config.ENV in ("PROD", "DEV", "TEST"):
+    if server_config.ENV in ("PROD", "DEV", "LOCAL", "TEST"):
         configure_app_logger(server_config.ENV)
 
         err = ping_db()
@@ -39,16 +39,17 @@ def start() -> None:
             app="src.server.app_entrypoint:app",
             host=server_config.ADDRESS,
             port=server_config.PORT,
-            reload=server_config.ENV == "DEV",
+            reload=server_config.ENV == "LOCAL",
             log_config=get_uvicorn_logger(server_config.ENV),
             # TODO: Add those to ServerConfig in order for them to be loaded dynamically based on ENV. This should be
             # done before deploying to PROD
             ssl_certfile="src/server/certs/localhost.crt",
             ssl_keyfile="src/server/certs/localhost.key",
             # https://docs.gunicorn.org/en/stable/design.html#how-many-workers
+            # https://stackoverflow.com/questions/65278110/how-does-gunicorn-distribute-requests-across-sync-workers
             # As cpu_count could return None we use 0 instead, as 2 * None would produce an error
-            # Also "workers" flag is ignored when reloading is enabled.
-            workers=(2 * (cpu_count() or 0) + 1) if server_config.ENV == "DEV" else None,
+            # Also "workers" flag is ignored when reloading is enabled (It is ignored for LOCAL)
+            workers=2 * (cpu_count() or 0) + 1,
         )
     else:
-        raise ValueError("The ENV environment variable should be PROD, DEV OR TEST")
+        raise ValueError("The ENV environment variable should be PROD, DEV, LOCAL OR TEST")
