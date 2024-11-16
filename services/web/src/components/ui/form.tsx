1ca90d6dcfaed3248e419280e416c3784575dcbc
Asia Haslam <asiakhaslam@gmail.com>
2024-11-16 16:23:48 +0000

    fix form label color issue

--- services/web/src/components/ui/form.tsx
+++ services/web/src/components/ui/form.tsx
@@ -76,9 +76,9 @@ const FormLabel = React.forwardRef<
     React.ElementRef<typeof LabelPrimitive.Root>,
     React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
 >(({ className, ...props }, ref) => {
-    const { error, formItemId } = useFormField();
+    const { formItemId } = useFormField();
 
-    return <Label ref={ref} className={cn(error && 'text-destructive', className)} htmlFor={formItemId} {...props} />;
+    return <Label ref={ref} className={cn(className)} htmlFor={formItemId} {...props} />;
 });
 FormLabel.displayName = 'FormLabel';
 
