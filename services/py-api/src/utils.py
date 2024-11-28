12f96cd6b0d5eb544da14fbe46aa1f4db0c474d7
plamenapavlova <plamena.ppavlova@outlook.com>
2024-11-28 19:19:02 +0200

    #745 Encode And Decode Methods for JWT  (#783)
    * encode and decode methods for JWT
    
    * Add SECRET_KEY to env.sample
    
    * Create a generic class
    
    * Fix the callable generics problem
    
    * Check if all the fields are present in the decoded token
    
    * Write tests for the jwt utility
    
    ---------
    
    Co-authored-by: Ivan Obreshkov <ivanobreshkov12@gmail.com>
    Co-authored-by: Isak Bego <isakbego@gmail.com>

--- services/py-api/src/utils.py
+++ services/py-api/src/utils.py
@@ -1,8 +1,13 @@
+from collections.abc import Callable
+from os import environ
 from threading import Lock
-from typing import Any, Dict
-
+from typing import Any, Dict, cast
+from jwt import DecodeError, ExpiredSignatureError, InvalidSignatureError, decode, encode
 import httpx
+from result import Err, Ok, Result
 from structlog.stdlib import get_logger
+
+from src.server.schemas.jwt_schemas.jwt_user_data_schema import BaseTypedDict
 
 LOG = get_logger()
 
@@ -43,3 +48,42 @@ class AsyncClient(metaclass=SingletonMeta):
             raise RuntimeError("The AsyncClient has not been initialized!")
 
         await self._async_client.aclose()
+
+
+class JwtUtility:
+    """A class providing generic methods for encoding data in with a predefined format into a JWT token, or decoding a JWT
+    token into a predefined format (TypedDict)"""
+
+    @staticmethod
+    def encode_data[T: BaseTypedDict](data: T) -> str:
+        return str(encode(payload=data, key=environ["SECRET_KEY"], algorithm="HS256"))
+
+    @staticmethod
+    def decode_data[T: BaseTypedDict](token: str, schema: Callable[..., T]) -> Result[T, str]:
+        try:
+            decoded_token: dict[str, Any] = decode(jwt=token, key=environ["SECRET_KEY"], algorithms=["HS256"])
+
+            # schema.__annotations___.keys() gets all the defined fields of the TypedDict without initializing it
+            # this will help us see if all the fields that we have defined in the schema are present in the decoded
+            # jwt token
+            for key in schema.__annotations__.keys():
+                if key not in decoded_token:
+                    return Err("The decoded token does not correspond with the provided schema.")
+
+            # We check both sides since we dont know which of the dictionaries is bigger than the other
+            for key in decoded_token:
+                if key not in schema.__annotations__.keys():
+                    return Err("The decoded token does not correspond with the provided schema.")
+
+            return Ok(cast(T, decoded_token))
+
+        except ExpiredSignatureError:
+            return Err("The JWT token has expired.")
+
+        except InvalidSignatureError:
+            return Err("The JWT token has invalid signature.")
+
+        except DecodeError:
+            # We log the exception DecodeError as we want to be able to trace what exactly has caused the error
+            LOG.exception("There was a a general error while decoding the JWT token.")
+            return Err("There was a a general error while decoding the JWT token. Checks its format again.")
