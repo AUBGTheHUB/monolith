11a8d78090140ce878b9542e121db92f49d1c4b6
MagdalenaZh <magdalenazheleva1@gmail.com>
2024-11-28 14:12:28 +0000

    initial commit

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -11,9 +11,54 @@ import MockDataComponent from './website/mockComponent';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import { useState } from 'react';
+import { InputComponent } from './internal_library/InputComponent/InputComponent';
 
 const FormSchema = z.object({
     notificationPreference: z.string().min(1, { message: 'GRESHKA' }),
+    username: z
+        .string()
+        .min(1, { message: 'Required' })
+        .min(3, { message: 'Username must be at least 3 characters long.' })
+        .max(15, { message: 'Username cannot be more than 15 characters long.' })
+        .regex(/^[a-zA-Z0-9_-]+$/, {
+            message: 'Username can only contain letters, numbers, underscores, and dashes.',
+        }),
+    password: z
+        .string()
+        .min(1, { message: 'Required' })
+        .min(8, { message: 'Password must be at least 8 characters long.' })
+        .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter.' })
+        .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter.' })
+        .regex(/[0-9]/, { message: 'Password must contain at least one number.' })
+        .regex(/[@$!%*?&]/, { message: 'Password must contain at least one special character.' }),
+    email: z
+        .string()
+        .min(1, { message: 'Required' })
+        .email({ message: 'Invalid email format.' })
+        .regex(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, {
+            message: 'Please enter a valid email address.',
+        }),
+    firstName: z
+        .string()
+        .min(1, { message: 'Required' })
+        .min(3, { message: 'First name must be at least 3 characters long.' })
+        .max(20, { message: 'First name cannot be more than 20 characters long.' })
+        .regex(/^[a-zA-Z]+$/, {
+            message: 'Username can only contain letters.',
+        }),
+    lastName: z
+        .string()
+        .min(1, { message: 'Required' })
+        .min(3, { message: 'Last name must be at least 3 characters long.' })
+        .max(20, { message: 'Last name cannot be more than 20 characters long.' })
+        .regex(/^[a-zA-Z]+$/, {
+            message: 'Last name can only contain letters.',
+        }),
+    age: z
+        .number()
+        .int()
+        .min(16, { message: 'Age must be at least 16.' })
+        .max(69, { message: 'Age cannot be more than 69.' }),
 });
 
 const RADIO_OPTIONS = [
@@ -22,15 +67,17 @@ const RADIO_OPTIONS = [
     { label: 'Nothing', value: 'none' },
 ];
 
-import { startPerformanceTools } from 'reactscape-testing-tools';
-
-startPerformanceTools();
-
 function App() {
     const form = useForm<z.infer<typeof FormSchema>>({
         resolver: zodResolver(FormSchema),
         defaultValues: {
             notificationPreference: '',
+            username: '',
+            password: '',
+            email: '',
+            firstName: '',
+            lastName: '',
+            age: undefined,
         },
     });
 
@@ -72,6 +119,51 @@ function App() {
                         name="notificationPreference"
                         options={RADIO_OPTIONS}
                         groupLabel="Notify me about..."
+                    />
+                    <InputComponent
+                        control={form.control}
+                        name="email"
+                        label="Email"
+                        type="email"
+                        placeholder="Enter your email"
+                    />
+                    <InputComponent
+                        control={form.control}
+                        name="username"
+                        label="Username"
+                        type="text"
+                        placeholder="Enter your username"
+                    />
+
+                    <InputComponent
+                        control={form.control}
+                        name="password"
+                        label="Password"
+                        type="password"
+                        placeholder="Enter your password"
+                    />
+
+                    <InputComponent
+                        control={form.control}
+                        name="firstName"
+                        label="First Name"
+                        type="text"
+                        placeholder="Enter your first name"
+                    />
+                    <InputComponent
+                        control={form.control}
+                        name="lastName"
+                        label="Last Name"
+                        type="text"
+                        placeholder="Enter your last name"
+                    />
+
+                    <InputComponent
+                        control={form.control}
+                        name="age"
+                        label="Age"
+                        type="number"
+                        placeholder="Enter your age"
                     />
                     <Button type="submit">Submit</Button>
                 </form>
---
+++ services/web/src/internal_library/InputComponent/InputComponent.tsx
@@ -0,0 +1,52 @@
+import { Control, FieldValues, Path } from 'react-hook-form';
+import { FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { cn } from '@/lib/utils';
+
+type InputProps<T extends FieldValues> = {
+    control: Control<T>;
+    name: Path<T>;
+    label: string;
+    type: 'text' | 'email' | 'password' | 'number';
+    placeholder?: string;
+    formItemClassName?: string;
+    labelClassName?: string;
+    inputClassName?: string;
+};
+
+export const InputComponent = <T extends FieldValues>({
+    control,
+    name,
+    label,
+    type,
+    placeholder,
+    formItemClassName,
+    labelClassName,
+    inputClassName,
+}: InputProps<T>) => {
+    return (
+        <FormField
+            control={control}
+            name={name}
+            render={({ field, fieldState: { error } }) => (
+                <FormItem className={cn('space-y-3', formItemClassName)}>
+                    <FormLabel className={labelClassName}>{label}</FormLabel>
+                    <FormControl>
+                        <Input
+                            {...field}
+                            type={type}
+                            placeholder={placeholder}
+                            className={cn(inputClassName)}
+                            onChange={(e) => {
+                                const value = e.target.value;
+                                field.onChange(type === 'number' ? +value : value); // Convert to number for 'number' type
+                            }}
+                            value={type === 'number' ? field.value || '' : field.value}
+                        />
+                    </FormControl>
+                    <div className="min-h-[24px]">{error && <FormMessage>{error.message}</FormMessage>}</div>
+                </FormItem>
+            )}
+        />
+    );
+};
