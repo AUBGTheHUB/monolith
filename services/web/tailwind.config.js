1d5caf1f1b90f47bbea931318d3eb0d44a28ca6b
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2024-12-22 13:58:12 +0000

    change to tailwind.config

--- services/web/tailwind.config.js
+++ services/web/tailwind.config.js
@@ -1,6 +1,5 @@
 import tailwindcssAnimate from 'tailwindcss-animate';
 
-/** @type {import('tailwindcss').Config} */
 export default {
     darkMode: ['class'],
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
