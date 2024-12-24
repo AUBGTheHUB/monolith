9662589c78300031f4d68631b7d1dc2b4129398e
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2024-12-24 23:05:39 +0200

    Add unit tests for retry_db_operation

--- services/py-api/tests/unit_tests/test_database_layer/test_db_manager.py
+++ services/py-api/tests/unit_tests/test_database_layer/test_db_manager.py
@@ -1,8 +1,8 @@
 from unittest.mock import patch, AsyncMock
 
 import pytest
-from pymongo.errors import ConnectionFailure
-from result import Err
+from pymongo.errors import ConnectionFailure, PyMongoError, OperationFailure
+from result import Err, Ok
 
 from src.database.db_manager import DatabaseManager
 from src.utils import SingletonMeta
@@ -24,6 +24,12 @@ def db_manager() -> DatabaseManager:
         # https://stackoverflow.com/questions/42565304/is-it-possible-to-ping-mongodb-from-pymongo
         with patch("src.database.db_manager.environ", {"DATABASE_URL": "mongodb+srv://test_url"}):
             return DatabaseManager()
+
+
+# Define a custom exception to simulate transient transaction error
+class RetryableWriteError(PyMongoError):
+    def has_error_label(self, label: str) -> bool:
+        return label == "RetryableWriteError"
 
 
 @pytest.fixture
@@ -66,3 +72,71 @@ async def test_close_all_connections_success(db_manager: DatabaseManager) -> Non
 async def test_close_all_connections_err(db_manager_none_client: DatabaseManager) -> None:
     result = db_manager_none_client.close_all_connections()
     isinstance(result, Err)
+
+
+@pytest.mark.asyncio
+async def test_retry_read_db_operation_success(db_manager: DatabaseManager) -> None:
+    # Mock a successful db operation
+    mock_db_operation = AsyncMock(return_value=Ok("Success"))
+    result = await db_manager.retry_db_operation(mock_db_operation, is_read_operation=True)
+
+    assert mock_db_operation.call_count == 1
+    assert result == Ok("Success")
+
+
+@pytest.mark.asyncio
+async def test_retry_read_db_operation_retryable_error(db_manager: DatabaseManager) -> None:
+    # Mock a successful db operation
+    mock_db_operation = AsyncMock(side_effect=[OperationFailure("Test err", 7), Ok("Success")])
+
+    # We patch the sleep function for faster testing
+    with patch("src.database.db_manager.sleep", new=AsyncMock()):
+        result = await db_manager.retry_db_operation(mock_db_operation, is_read_operation=True)
+
+        # On the first call it failed and on the second call it succeeded
+        assert mock_db_operation.call_count == 2
+        assert result == Ok("Success")
+
+
+@pytest.mark.asyncio
+async def test_retry_read_db_operation_retryable_error_exhaust_retries(db_manager: DatabaseManager) -> None:
+    # Simulate a retryable read err that keeps failing
+    mock_db_operation = AsyncMock(side_effect=OperationFailure("Test err", 7))
+    with pytest.raises(PyMongoError):
+        await db_manager.retry_db_operation(mock_db_operation, is_read_operation=True)
+
+    assert mock_db_operation.call_count == 2
+
+
+@pytest.mark.asyncio
+async def test_retry_write_db_operation_success(db_manager: DatabaseManager) -> None:
+    # Mock a successful db operation
+    mock_db_operation = AsyncMock(return_value=Ok("Success"))
+    result = await db_manager.retry_db_operation(mock_db_operation, is_read_operation=False)
+
+    assert mock_db_operation.call_count == 1
+    assert result == Ok("Success")
+
+
+@pytest.mark.asyncio
+async def test_write_read_db_operation_retryable_error(db_manager: DatabaseManager) -> None:
+    # Mock a successful db operation
+    mock_db_operation = AsyncMock(side_effect=[RetryableWriteError(), Ok("Success")])
+
+    # We patch the sleep function for faster testing
+    with patch("src.database.db_manager.sleep", new=AsyncMock()):
+        result = await db_manager.retry_db_operation(mock_db_operation, is_read_operation=False)
+
+        # On the first call it failed and on the second call it succeeded
+        assert mock_db_operation.call_count == 2
+        assert result == Ok("Success")
+
+
+@pytest.mark.asyncio
+async def test_retry_write_db_operation_retryable_error_exhaust_retries(db_manager: DatabaseManager) -> None:
+    # Simulate a retryable read err that keeps failing
+    mock_db_operation = AsyncMock(side_effect=RetryableWriteError())
+    with pytest.raises(PyMongoError):
+        await db_manager.retry_db_operation(mock_db_operation, is_read_operation=False)
+
+    assert mock_db_operation.call_count == 2
