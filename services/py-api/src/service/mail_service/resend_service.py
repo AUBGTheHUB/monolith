48fdef7868d83de34e1dcc9802c5a4c2fef3f7a5
cl3vy <klevi1255@gmail.com>
2024-12-28 16:42:10 +0100

    Adding Result pattern to resend_service implementation

--- services/py-api/src/service/mail_service/resend_service.py
+++ services/py-api/src/service/mail_service/resend_service.py
@@ -5,6 +5,8 @@ from typing import Literal, Any, Coroutine
 from dotenv import load_dotenv
 
 import resend
+from result import Result, Ok, Err
+
 from src.service.mail_service.mail_service import MailService
 from src.service.mail_service.utils import (
     load_email_participant_html_template,
@@ -24,13 +26,14 @@ class ResendMailService(MailService):
     def __init__(self) -> None:
         api_key = getenv("RESEND_API_KEY")
         if not api_key:
+            LOG.exception("RESEND_API_KEY environment variable is not set")
             raise ValueError("RESEND_API_KEY environment variable is not set")
         resend.api_key = api_key
         self.client = resend
 
     async def send_email(
         self, receiver: str, subject: str, body_content: str, content_type: Literal["text", "html"] = "text"
-    ):
+    ) -> Result[resend.Email, str]:
         params: resend.Emails.SendParams = {
             "from": f"THE HUB <{SENDER_EMAIL}>",
             "to": [receiver],
@@ -41,9 +44,10 @@ class ResendMailService(MailService):
         try:
             email: resend.Email = await to_thread(self.client.Emails.send, params)
             LOG.info(f"Email sent successfully: {email}")
-            return email
+            return Ok(email)
         except Exception as e:
-            raise RuntimeError(f"Failed to send email: {str(e)}") from e
+            LOG.exception(f"Failed to send email: {str(e)}")
+            return Err(f"Failed to send email: {str(e)}")
 
     async def send_participant_successful_registration_email(
         self,
@@ -52,35 +56,41 @@ class ResendMailService(MailService):
         participant: str,
         team_name: str,
         invite_link: str,
-    ) -> Coroutine[Any, Any, None]:
+    ) -> Result[resend.Email, str]:
         try:
             body_html = load_email_participant_html_template(participant, team_name, invite_link)
         except ValueError as e:
-            raise ValueError(f"Error loading the HTML template: {str(e)}") from e
+            LOG.exception(f"Error loading the HTML template: {str(e)}")
+            return Err(f"Error loading the HTML template: {str(e)}")
 
-        try:
-            return await self.send_email(
-                receiver=receiver, subject=subject, body_content=body_html, content_type="html"
-            )
-        except Exception as e:
-            raise RuntimeError(f"Failed to send email: {str(e)}") from e
+        send_result = await self.send_email(
+            receiver=receiver, subject=subject, body_content=body_html, content_type="html"
+        )
+
+        if send_result.is_ok():
+            return send_result
+        else:
+            return Err(f"Failed to send participant registration email: {send_result.err()}")
 
     async def send_participant_verification_email(
-        self,
-        receiver: str,
-        subject: str,
-        participant: str,
-        team_name: str,
-        confirmation_link: str,
-    ) -> Coroutine[Any, Any, None]:
+            self,
+            receiver: str,
+            subject: str,
+            participant: str,
+            team_name: str,
+            confirmation_link: str,
+    ) -> Result[resend.Email, str]:
         try:
             body_html = load_email_verify_participant_html_template(participant, team_name, confirmation_link)
         except ValueError as e:
-            raise ValueError(f"Failed to load email template: {str(e)}")
+            LOG.exception(f"Failed to load email template: {str(e)}")
+            return Err(f"Failed to load email template: {str(e)}")
 
-        try:
-            return await self.send_email(
-                receiver=receiver, subject=subject, body_content=body_html, content_type="html"
-            )
-        except Exception as e:
-            raise RuntimeError(f"Failed to send participant confirmation email: {str(e)}") from e
+        send_result = await self.send_email(
+            receiver=receiver, subject=subject, body_content=body_html, content_type="html"
+        )
+
+        if send_result.is_ok():
+            return send_result
+        else:
+            return Err(f"Failed to send participant verification email: {send_result.err()}")
