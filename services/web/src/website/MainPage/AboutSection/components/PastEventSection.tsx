c698342a73962633cba9e9647a512eaede1c18a4
Isak Bego <isakbego@gmail.com>
2024-12-29 16:49:36 +0000

    Create custom carousel

--- services/web/src/website/MainPage/AboutSection/components/PastEventSection.tsx
+++ services/web/src/website/MainPage/AboutSection/components/PastEventSection.tsx
@@ -1,14 +1,16 @@
-import EventTicket from './EventTicket.tsx'
-import pastEvents from '../StaticContent/pastEvents.json'
+import EventTicket from './EventTicket.tsx';
+import pastEvents from '../StaticContent/pastEvents.json';
+import SimpleHorizontalCarousel from '@/components/ui/simpleHorizontalCarousel.tsx';
 
 const PastEventSection = () => {
+    const eventTickets = pastEvents.map((pastEvent, index) => (
+        <EventTicket imgSrc={pastEvent.cover_picture} title={pastEvent.title} tags={pastEvent.tags} key={index} />
+    ));
     return (
         <div className="space-y-7 font-mont">
             <h1 className="font-semibold text-3xl text-primary mb-10">Our Past Events</h1>
-            <div className="flex flex-row gap-4">
-                {pastEvents.map((pastEvent) => (
-                    <EventTicket imgSrc={pastEvent.cover_picture} title={pastEvent.title} tags={pastEvent.tags} />
-                ))}
+            <div>
+                <SimpleHorizontalCarousel items={eventTickets} itemsPerSlide={1} />
             </div>
         </div>
     );
