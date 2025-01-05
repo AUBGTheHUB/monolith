80ccaba7abfec5b024d32ca22077ef46c7ac4ee7
Isak Bego <isakbego@gmail.com>
2025-01-05 17:29:00 +0000

    Fix button colors, fix mobile version

--- services/web/src/website/MainPage/AboutSection/components/EventTicket.tsx
+++ services/web/src/website/MainPage/AboutSection/components/EventTicket.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button.tsx';
 
 export default function EventTicket({ imgSrc, title, tags }: { imgSrc: string; title: string; tags: Array<string> }) {
     return (
-        <div className="flex flex-col min-w-50 max-w-80 h-60 p-2 border rounded-2xl font-semibold">
+        <div className="flex flex-col min-w-50 max-w-80 max-[450px]:max-w-full h-60 p-2 border rounded-2xl font-semibold">
             <div className="h-4/6 w-full">
                 <img src={imgSrc} alt="" className="object-cover w-full h-full rounded-xl" />
             </div>
