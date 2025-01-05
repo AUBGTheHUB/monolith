625f48882926fac84c63f952574d016d6846d0fe
Isak Bego <isakbego@gmail.com>
2025-01-05 16:37:48 +0000

    Remove flex property from body, which was breaking the carousel logic

--- services/web/src/website/MainPage/AboutSection/components/IdentitySection.tsx
+++ services/web/src/website/MainPage/AboutSection/components/IdentitySection.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import identityProposition from '../StaticContent/identityProposition.json';
 import { Button } from '@/components/ui/button.tsx';
 
--- services/web/src/website/MainPage/MainPage.tsx
+++ services/web/src/website/MainPage/MainPage.tsx
@@ -1,12 +1,12 @@
-import IdentitySection from './AboutSection/components/IdentitySection.tsx'
-import PastEventSection from './AboutSection/components/PastEventSection.tsx'
+import IdentitySection from './AboutSection/components/IdentitySection.tsx';
+import PastEventSection from './AboutSection/components/PastEventSection.tsx';
 
 export const MainPage = () => {
     return (
         <>
             <h1>MainPage</h1>
             <div className="about-section rounded-2xl">
-                <div className="w-3/5 mx-auto">
+                <div className="sm:w-3/5 w-11/12 mx-auto">
                     <IdentitySection />
                     <PastEventSection />
                 </div>
