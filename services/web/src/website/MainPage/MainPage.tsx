a0c1f48b91d356385f3e5961443e93a4972eafac
Isak Bego <65165135+Isak-Bego@users.noreply.github.com>
2025-01-06 19:24:41 +0100

    #828 implement About and Past Events Section (#830)

--- services/web/src/website/MainPage/MainPage.tsx
+++ services/web/src/website/MainPage/MainPage.tsx
@@ -1,3 +1,13 @@
+import IdentitySection from './AboutSection/components/IdentitySection.tsx';
+import PastEventSection from './AboutSection/components/PastEventSection.tsx';
+
 export const MainPage = () => {
-    return <h1>MainPage</h1>;
+    return (
+        <div className="about-section rounded-2xl py-8">
+            <div className="sm:w-3/5 w-11/12 mx-auto">
+                <IdentitySection />
+                <PastEventSection />
+            </div>
+        </div>
+    );
 };
