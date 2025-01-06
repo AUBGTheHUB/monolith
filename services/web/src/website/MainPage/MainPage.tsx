0a2505d4f58a62a85131c703a5bcfeb4b394b04f
Isak Bego <isakbego@gmail.com>
2025-01-06 10:13:30 +0000

    Removed uneccesary nits

--- services/web/src/website/MainPage/MainPage.tsx
+++ services/web/src/website/MainPage/MainPage.tsx
@@ -3,14 +3,11 @@ import PastEventSection from './AboutSection/components/PastEventSection.tsx';
 
 export const MainPage = () => {
     return (
-        <>
-            <h1>MainPage</h1>
-            <div className="about-section rounded-2xl">
-                <div className="sm:w-3/5 w-11/12 mx-auto">
-                    <IdentitySection />
-                    <PastEventSection />
-                </div>
+        <div className="about-section rounded-2xl py-8">
+            <div className="sm:w-3/5 w-11/12 mx-auto">
+                <IdentitySection />
+                <PastEventSection />
             </div>
-        </>
+        </div>
     );
 };
