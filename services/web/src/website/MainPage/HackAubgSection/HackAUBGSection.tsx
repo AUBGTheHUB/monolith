3411177615f1d16647ab45d8e3b1bd8d1b0a6e76
MagdalenaZh <magdalenazheleva1@gmail.com>
2025-01-09 18:31:07 +0000

    fix margins

--- services/web/src/website/MainPage/HackAubgSection/HackAUBGSection.tsx
+++ services/web/src/website/MainPage/HackAubgSection/HackAUBGSection.tsx
@@ -3,38 +3,46 @@ import { Button } from '@/components/ui/button';
 export default function HackAUBGSection() {
     return (
         <div className="flex flex-col justify-center">
-            <div className="w-[60%] flex mx-auto mb-[20px]">
-                <p className="font-mont text-[29px] font-semibold text-blue-950	">HackAUBG</p>
+            <div className="sm:w-3/5 w-11/12 flex mx-auto mb-[1.25rem]">
+                <p className="font-mont text-[1.813rem] font-semibold text-blue-950	">HackAUBG</p>
             </div>
-            <div className="flex flex-col items-center gap-5 w-[60%] mx-auto">
-                <div className="flex justify-between  items-center w-full max-w-[1200px] gap-5">
-                    <div className="flex-1 h-[240px] flex flex-col items-center justify-center border border-slate-300 box-border p-2.5 text-center rounded-3xl">
-                        <p className=" font-mont text-[90px] font-semibold text-blue-700">52</p>
-                        <p className="font-mont text-[19px] text-blue-950">Hours to create a solution</p>
+            <div className="flex flex-col items-center gap-5 sm:w-3/5 w-11/12 mx-auto">
+                <div className="flex flex-wrap lg:flex-nowrap justify-between  items-center w-full max-w-[75rem] gap-5 ">
+                    <div className=" w-full lg:flex-1 h-[15rem] flex flex-col items-center justify-center border border-slate-300 box-border p-5 text-center rounded-3xl">
+                        <p className=" font-mont text-[5.625rem] font-semibold text-blue-700">52</p>
+                        <p className="font-mont text-[1.188rem] text-blue-950">Hours to create a solution</p>
                     </div>
-                    <div className="flex-[3] h-[240px] flex flex-row items-end justify-between border rounded-3xl border-slate-300 box-border p-5 bg-[url('/participate.png')] bg-cover bg-center">
-                        <div className="flex flex-col w-[55%] ml-[12px]">
-                            <p className="font-mont text-[19px] font-semibold text-white ">
+                    <div className="w-full lg:flex-[3] h-[15rem] flex flex-col items-baseline justify-end  border rounded-3xl border-slate-300 box-border p-5 bg-[url('/participate.png')] bg-cover bg-center">
+                        <div className="flex flex-col  lg:w-[70%] sm:w-[100%]">
+                            <p className="font-mont text-[1.188rem] font-semibold text-white ">
                                 The biggest student-organized hackathon as of now.
                             </p>
-                            <p className="text-blue-200 font-mont text-[12px] mt-[10px]">
-                                More than 120 people participated last year.
-                            </p>
                         </div>
-                        <div>
-                            <Button className=" bg-transparent text-white " variant="outline_mono" size="round_sm">
-                                Participate
-                            </Button>
+                        <div className="flex flex row w-[100%] justify-between">
+                            <div>
+                                <p className="text-blue-200 font-mont text-[0.75rem] mt-[0.625rem]">
+                                    More than 120 people participated last year.
+                                </p>
+                            </div>
+                            <div className="flex items-end">
+                                <Button
+                                    className="bg-transparent text-white border-white hover:bg-white hover:text-blue-700 transition duration-200"
+                                    variant="outline_mono"
+                                    size="round_sm"
+                                >
+                                    Participate
+                                </Button>
+                            </div>
                         </div>
                     </div>
                 </div>
-                <div className="flex justify-between items-center w-full max-w-[1200px] gap-5 mb-[20px]">
-                    <div className="flex-1 h-[240px] flex flex-col items-start justify-center border  rounded-3xl border-slate-300 box-border p-2.5">
-                        <div className=" w-[70%] font-mont font-semibold text-[19px] text-blue-950 mb-[40px] ml-[20px]">
+                <div className=" flex flex-wrap lg:flex-nowrap justify-between items-center w-full max-w-[75rem] gap-5 mb-[1.25rem]">
+                    <div className="w-full lg:flex-1 h-[15rem]  p-5 flex flex-col items-start justify-center border  rounded-3xl border-slate-300 box-border ">
+                        <div className=" w-[80%] font-mont font-semibold text-[1.188rem] text-blue-950 mb-[2.5rem]">
                             <p>Guidance from current industry leaders</p>
                         </div>
                         <div className="flex flex-row  items-center ">
-                            <div className="flex flex-row w-[70%] space-x-[-50px] ml-[20px] ">
+                            <div className="flex flex-row w-[70%] lg:space-x-[-3.125rem] space-x-[-25px] ">
                                 <div className="h-[45%] w-[45%] border border-black rounded-full ">
                                     <img src="../mentor.png" alt="" />
                                 </div>
@@ -46,22 +54,26 @@ export default function HackAUBGSection() {
                                 </div>
                             </div>
                             <div className="w-[60%] h-full flex items-end justify-center">
-                                <Button className="text-blue-700 w-fit" variant="outline_mono" size="round_sm">
+                                <Button
+                                    className="text-blue-700 border-blue-700 bg-transparent hover:bg-blue-700 hover:text-white transition duration-200"
+                                    variant="outline_mono"
+                                    size="round_sm"
+                                >
                                     See all mentors
                                 </Button>
                             </div>
                         </div>
                     </div>
-                    <div className="flex-1 h-[240px] flex flex-col  border  rounded-3xl border-slate-300 box-border p-2.5 relative text-left">
-                        <div className="flex items-start mb-[5px] font-semibold font-mont text-blue-950 mt-[20px] ml-[15px]">
+                    <div className="w-full lg:flex-1 h-[15rem] flex flex-col  border  rounded-3xl border-slate-300 box-border p-5 relative text-left">
+                        <div className="flex items-start mb-[0.313rem] font-semibold font-mont text-blue-950 mt-[1.25rem] ">
                             <p>Major prize pool</p>
                         </div>
 
                         <div className="flex justify-center  items-baseline">
-                            <p className="font-mont text-[90px]  font-bold bg-gradient-to-r from-blue-700  to-teal-400 bg-clip-text text-transparent">
+                            <p className="font-mont lg:text-[5.625rem] text-[5.2rem] font-bold bg-gradient-to-r from-blue-700  to-teal-400 bg-clip-text text-transparent">
                                 6000
                             </p>
-                            <div className="font-mont text-[16px] text-blue-950">
+                            <div className="font-mont text-[1rem] text-blue-950">
                                 <p>BGN</p>
                             </div>
                         </div>
