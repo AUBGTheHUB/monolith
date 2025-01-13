bac4953ae28c7fbbfad3a23ee6f7a4014b59a4c6
Isak Bego <isakbego@gmail.com>
2025-01-13 21:03:44 +0000

    Fix team repository unit tests

--- services/py-api/tests/unit_tests/test_database_layer/test_team_repository.py
+++ services/py-api/tests/unit_tests/test_database_layer/test_team_repository.py
@@ -9,7 +9,6 @@ from result import Ok, Err
 from src.database.db_manager import TEAMS_COLLECTION
 from src.database.repository.teams_repository import TeamsRepository
 from src.server.exception import DuplicateTeamNameError
-from src.server.schemas.request_schemas.schemas import ParticipantRequestBody
 from src.database.model.team_model import Team
 
 
@@ -21,16 +20,16 @@ def repo(db_manager_mock: Mock) -> TeamsRepository:
 @pytest.mark.asyncio
 async def test_create_team_success(
     ten_sec_window: Tuple[datetime, datetime],
-    mock_input_data: ParticipantRequestBody,
+    mock_normal_team: Team,
     repo: TeamsRepository,
 ) -> None:
     start_time, end_time = ten_sec_window
 
-    result = await repo.create(mock_input_data)
+    result = await repo.create(mock_normal_team)
 
     assert isinstance(result, Ok)
     assert isinstance(result.ok_value, Team)
-    assert result.ok_value.name == mock_input_data.team_name
+    assert result.ok_value.name == mock_normal_team.name
     # Check that created_at and updated_at fall within the 10-second window
     assert start_time <= result.ok_value.created_at <= end_time, "created_at is not within the 10-second window"
     assert start_time <= result.ok_value.updated_at <= end_time, "updated_at is not within the 10-second window"
@@ -38,14 +37,14 @@ async def test_create_team_success(
 
 @pytest.mark.asyncio
 async def test_create_team_duplicate_name_error(
-    db_manager_mock: Mock, mock_input_data: ParticipantRequestBody, repo: TeamsRepository
+    db_manager_mock: Mock, mock_normal_team: Team, repo: TeamsRepository
 ) -> None:
     # Simulate a DuplicateKeyError raised by insert_one to represent a duplicate team name
     db_manager_mock.get_collection.return_value.insert_one = AsyncMock(
         side_effect=DuplicateKeyError("Duplicate team name error")
     )
 
-    result = await repo.create(mock_input_data)
+    result = await repo.create(mock_normal_team)
 
     assert isinstance(result, Err)
     assert isinstance(result.err_value, DuplicateTeamNameError)
@@ -55,12 +54,12 @@ async def test_create_team_duplicate_name_error(
 
 @pytest.mark.asyncio
 async def test_create_team_general_exception(
-    db_manager_mock: Mock, mock_input_data: ParticipantRequestBody, repo: TeamsRepository
+    db_manager_mock: Mock, mock_normal_team: Team, repo: TeamsRepository
 ) -> None:
     # Simulate a general exception raised by insert_one
     db_manager_mock.get_collection.return_value.insert_one = AsyncMock(side_effect=Exception("Test error"))
 
-    result = await repo.create(mock_input_data)
+    result = await repo.create(mock_normal_team)
 
     assert isinstance(result, Err)
     assert isinstance(result.err_value, Exception)
