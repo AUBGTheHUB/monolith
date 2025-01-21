91ffdcb2f4711e73cc7b69f801b1751a54c21e55
Isak Bego <65165135+Isak-Bego@users.noreply.github.com>
2025-01-21 09:52:42 +0100

    Switch db based on env (#842)
    * Assign Database based on the ENV, to avoid clashes between them
    
    * Fix unit tests to follow the same pattern when mocking classes

--- services/py-api/tests/unit_tests/test_handlers/test_utility_handlers.py
+++ services/py-api/tests/unit_tests/test_handlers/test_utility_handlers.py
@@ -1,4 +1,4 @@
-from unittest.mock import Mock, AsyncMock
+from unittest.mock import Mock
 
 import pytest
 from result import Err
@@ -10,7 +10,7 @@ from src.server.schemas.response_schemas.schemas import PongResponse, Response, 
 @pytest.mark.asyncio
 async def test_ping_handler(db_manager_mock: Mock) -> None:
     # Mock that async_ping_db return no error
-    db_manager_mock.async_ping_db = AsyncMock(return_value=None)
+    db_manager_mock.async_ping_db.return_value = None
     handler = UtilityHandlers(db_manager_mock)
 
     result = await handler.ping_services()
@@ -22,7 +22,7 @@ async def test_ping_handler(db_manager_mock: Mock) -> None:
 @pytest.mark.asyncio
 async def test_ping_handler_error(db_manager_mock: Mock) -> None:
     # Mock that async_ping_db return no error
-    db_manager_mock.async_ping_db = AsyncMock(return_value=Err("Test error"))
+    db_manager_mock.async_ping_db.return_value = Err("Test error")
     handler = UtilityHandlers(db_manager_mock)
 
     result = await handler.ping_services()
