3a895dc80a9b8b0e8f54440199a5e6578f7b05c0
aivanov-qb <aivanov-qb@quickbase.com>
2025-01-25 11:26:34 +0000

    fixes on desktop

--- services/web/src/website/MainPage/Navigation/DesktopNav.tsx
+++ services/web/src/website/MainPage/Navigation/DesktopNav.tsx
@@ -1,14 +1,14 @@
 import logo from './images/hublogo.png';
 
-const NAV_ITEM = 'py-[22.5px] px-[10px] text-lg';
-const NAV_ITEM_A = 'text-white font-light';
-
+const NAV_ITEM = 'py-[22.5px] px-[20px] text-lg ';
+const NAV_ITEM_A =
+    'text-white font-light hover:text-white relative after:content-[""] after:absolute after:w-full after:scale-x-0 after:h-[2px] after:bottom-[-4px] after:left-0 after:bg-white after:origin-bottom-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-bottom-left';
 export default function DesktopNav() {
     return (
         <div className="w-full">
             <div className="w-full flex flex-row align-top bg-[#07064d]">
                 <div className="w-[42%] items-center">
-                    <a href="#past-events">
+                    <a href="/">
                         <img src={logo} className="h-[70px] my-[15px] mx-auto" />
                     </a>
                 </div>
--- services/web/src/website/MainPage/Navigation/MobileNav.tsx
+++ services/web/src/website/MainPage/Navigation/MobileNav.tsx
@@ -3,7 +3,7 @@ import hamburgerMenuIcon from './images/hamburger_menu.svg';
 import photoIcon from './images/past_events.svg';
 import threeDots from './images/departments.svg';
 import codeIcon from './images/hackathon.svg';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
+import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
 
 const A = 'text-black hover:text-black';
 const B = 'text-[18pt] text-left leading-[1.5em] my-4';
@@ -14,13 +14,17 @@ export default function MobileNavComponent() {
         <div className="navigation">
             <div className="nav-bar flex align-top bg-[#07064d]">
                 <div className="items-left">
-                    <img src={logo} className="h-[70px] m-[15px]" />
+                    <a href="/">
+                        <img src={logo} className="h-[70px] m-[15px]" />
+                    </a>
                 </div>
             </div>
             <Sheet>
                 <SheetTrigger>
                     <img className="absolute right-[15px] top-[25px] h-[50px] text-right" src={hamburgerMenuIcon} />
                 </SheetTrigger>
+                <SheetTitle />
+                <SheetDescription />
                 <SheetContent>
                     <div className={B}>
                         <a href="#past-events" className={A}>
