0745575a27b8cdbf656cf74400c0eafd13d3d709
MagdalenaZh <magdalenazheleva1@gmail.com>
2025-01-29 10:45:52 +0000

    test rem values for the content section

--- services/web/src/website/MainPage/LandingSection/LandingSection.tsx
+++ services/web/src/website/MainPage/LandingSection/LandingSection.tsx
@@ -44,15 +44,15 @@ export default function LandingSection() {
             />
             <div className="flex flex-col bg-[#0b1224] h-[100vh]">
                 <Navigation />
-                <div className="flex items-center justify-center font-mont z-10 mt-10">
-                    <div className="w-2/5 mt-20 mb-20">
+                <div className="flex items-center justify-center font-mont z-10 h-full">
+                    <div className="sm:w-2/5 w-11/12">
                         <div className="flex flex-col items-center text-center space-y-6">
-                            <div className="space-y-4">
-                                <p className="text-2xl text-blue-200">WELCOME TO</p>
-                                <p className="text-8xl font-bold bg-gradient-to-b from-[#FFFFFF] to-[#33C8FF] bg-clip-text text-transparent">
+                            <div className="">
+                                <p className="text-[1.4rem] sm:text-[1.5rem] text-blue-200">WELCOME TO</p>
+                                <p className="text-[4rem] sm:text-[4.5rem]  font-bold bg-gradient-to-b from-[#FFFFFF] to-[#33C8FF] bg-clip-text text-transparent">
                                     THE HUB
                                 </p>
-                                <p className="text-sm text-white">
+                                <p className="text-[0.8rem] sm:text-[0.875rem] text-white">
                                     A group of passionate IT-oriented students who strive to make technological
                                     innovation thrive within AUBG and beyond.
                                 </p>
@@ -60,15 +60,15 @@ export default function LandingSection() {
                             <div className="flex space-x-4">
                                 <Button
                                     className="border-[0.5px] border-white text-white bg-transparent rounded-3xl 
-               sm:hover:bg-white sm:hover:text-black 
-               sm:transition sm:duration-300 sm:ease-in-out"
+                   sm:hover:bg-white sm:hover:text-black 
+                   sm:transition sm:duration-300 sm:ease-in-out"
                                 >
                                     Find out more
                                 </Button>
                                 <Button
                                     className="border-[0.5px] border-white text-white bg-transparent rounded-3xl 
-               sm:hover:bg-white sm:hover:text-black 
-               sm:transition sm:duration-300 sm:ease-in-out"
+                   sm:hover:bg-white sm:hover:text-black 
+                   sm:transition sm:duration-300 sm:ease-in-out"
                                 >
                                     Contact us
                                 </Button>
