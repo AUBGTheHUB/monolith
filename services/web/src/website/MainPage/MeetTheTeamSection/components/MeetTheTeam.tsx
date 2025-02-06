e670587445d04bc32d577bf81f3628c69e80b249
Teodor <99684754+Pastedko@users.noreply.github.com>
2025-02-06 13:42:12 +0200

    fixed controls display (#852)
    Co-authored-by: Aleks Ivanov <al.ivanov1908@gmail.com>

--- services/web/src/website/MainPage/MeetTheTeamSection/components/MeetTheTeam.tsx
+++ services/web/src/website/MainPage/MeetTheTeamSection/components/MeetTheTeam.tsx
@@ -46,7 +46,7 @@ export default function MeetTheTeamSection() {
         <div className="meet-team">
             <img
                 src="/meetTheTeam/meet_the_team_blob.svg"
-                className=" z-0 absolute blur-[10rem] h-[1490.43px] w-[1505.76px] top-[0rem] right-[-8rem] opacity-65 rotate-210 "
+                className="z-0 absolute blur-[10rem] h-[1490.43px] w-[1505.76px] top-[0rem] right-[-8rem] opacity-65 rotate-210 "
             ></img>
             <div className=" space-y-7 font-mont sm:w-3/5 w-11/12 mx-auto z-10 relative ">
                 <h2 className="font-semibold text-3xl text-[#9cbeff] mb-10 ">Meet the team</h2>
