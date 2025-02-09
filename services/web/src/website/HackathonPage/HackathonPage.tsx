37f940672c7481fa8aa73c127e34e7ccaf74a0e9
MagdalenaZh <magdalenazheleva1@gmail.com>
2025-02-09 12:19:55 +0000

    fix return

--- services/web/src/website/HackathonPage/HackathonPage.tsx
+++ services/web/src/website/HackathonPage/HackathonPage.tsx
@@ -2,9 +2,10 @@ import { Fragment } from 'react/jsx-runtime';
 import ScheduleSection from './ScheduleSection/ScheduleSection';
 
 export const HackathonPage = () => {
-    return;
-    <Fragment>
-        <h1 className="font-distant text-white">SF Distant Galaxy Regular</h1>
-        <ScheduleSection />
-    </Fragment>;
+    return (
+        <Fragment>
+            <h1 className="font-distant text-white">SF Distant Galaxy Regular</h1>
+            <ScheduleSection />
+        </Fragment>
+    );
 };
