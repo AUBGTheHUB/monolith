2f9991d8474ca4dfd678f79fdf57ba9a297b13bc
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2025-02-11 21:02:16 +0200

    Refactor hackathon service to use new mail service

--- services/py-api/src/service/hackathon_service.py
+++ services/py-api/src/service/hackathon_service.py
@@ -1,18 +1,20 @@
-from datetime import timezone, datetime, timedelta
-from math import ceil
-from os import environ
-from typing import Final, Optional, Tuple
 from datetime import datetime, timedelta
+from datetime import timezone
+from math import ceil
+from typing import Final, Optional, Tuple
 
 from fastapi import BackgroundTasks
 from motor.motor_asyncio import AsyncIOMotorClientSession
 from result import Err, is_err, Ok, Result
+from structlog.stdlib import get_logger
 
+from src.database.model.base_model import SerializableObjectId
 from src.database.model.participant_model import Participant, UpdateParticipantParams
 from src.database.model.team_model import Team, UpdateTeamParams
 from src.database.repository.participants_repository import ParticipantsRepository
 from src.database.repository.teams_repository import TeamsRepository
 from src.database.transaction_manager import TransactionManager
+from src.environment import is_test_env, DOMAIN, SUBDOMAIN, is_prod_env, is_dev_env, PORT
 from src.server.exception import (
     DuplicateTeamNameError,
     DuplicateEmailError,
@@ -28,31 +30,44 @@ from src.server.schemas.request_schemas.schemas import (
     AdminParticipantInputData,
     InviteLinkParticipantInputData,
 )
-from src.database.model.base_model import SerializableObjectId
-from src.service.mail_service.resend_service import ResendMailService
+from src.service.mail_service.hackathon_mail_service import HackathonMailService
 from src.utils import JwtUtility
 
-from src.service.mail_service.resend_service import ResendMailService
+LOG = get_logger()
 
 
 class HackathonService:
     """Service layer designed to hold all business logic related to hackathon management"""
 
     MAX_NUMBER_OF_TEAM_MEMBERS: Final[int] = 6
+    """Constraint for max number of participants in a given team"""
+
     MAX_NUMBER_OF_VERIFIED_TEAMS_IN_HACKATHON: Final[int] = 12
-    RATE_LIMIT_SECONDS: Final[int] = 90
+    """Constraint for max number of verified teams in the hackathon. A team is verified when the admin participant who
+    created the team, verified their email. This const also includes the random teams, which are automatically created
+    and marked as verified, once the hackathon registration closes"""
+
+    _RATE_LIMIT_SECONDS: Final[int] = 90
+    """Number of seconds before a participant is allowed to resend their verification email, if they didn't get one."""
+
+    # TODO: Change to front-end
+    _PARTICIPANTS_VERIFICATION_ROUTE = "/api/v3/hackathon/participants/verify"
+    """The front-end route for email verification"""
+
+    _PARTICIPANTS_REGISTRATION_ROUTE = "/hackathon/register"
+    """The front-end route for hackathon registration"""
 
     def __init__(
         self,
         participant_repo: ParticipantsRepository,
         team_repo: TeamsRepository,
         tx_manager: TransactionManager,
-        mailing_service: ResendMailService,
+        mail_service: HackathonMailService,
     ) -> None:
         self._participant_repo = participant_repo
         self._team_repo = team_repo
         self._tx_manager = tx_manager
-        self._mailing_service = mailing_service
+        self._mail_service = mail_service
 
     async def _create_participant_and_team_in_transaction_callback(
         self, input_data: AdminParticipantInputData, session: Optional[AsyncIOMotorClientSession] = None
@@ -244,10 +259,14 @@ class HackathonService:
         """Check if the verification email rate limit has been reached"""
         participant = await self._participant_repo.fetch_by_id(obj_id=participant_id)
 
-        # If there was an error fetching the participant return that error as it is as it will be handled further up by the
-        # handler
+        # If there was an error fetching the participant return that error as it is as it will be handled further up by
+        # the handler
         if is_err(participant):
             return participant
+
+        # Check if the participant is already verified before sending another verification email
+        if participant.ok_value.email_verified:
+            return Err(ParticipantAlreadyVerifiedError())
 
         # We fetch the team so that we take advantage of the team info we have to pass to the mailing service
         team = (
@@ -261,30 +280,47 @@ class HackathonService:
         if is_err(team):
             return team
 
-        # Check if the participant is already verified before sending another verification email
-        if participant.ok_value.email_verified:
-            return Err(ParticipantAlreadyVerifiedError())
-
         # The last_sent_mail field is None when there has not been any email sent previously
-        if participant.ok_value.last_sent_email is None:
+        if participant.ok_value.last_sent_verification_email is None:
             return Ok((participant.ok_value, team.ok_value))
 
         # Calculate the rate limit
-        is_within_rate_limit = datetime.now() - participant.ok_value.last_sent_email >= timedelta(
-            seconds=self.RATE_LIMIT_SECONDS
+        is_within_rate_limit = datetime.now() - participant.ok_value.last_sent_verification_email >= timedelta(
+            seconds=self._RATE_LIMIT_SECONDS
         )
 
         # If it is not within the rate limit raise an error
         if not is_within_rate_limit:
-            return Err(EmailRateLimitExceededError())
+            remaining_time = (
+                self._RATE_LIMIT_SECONDS - (datetime.now() - participant.ok_value.last_sent_verification_email).seconds
+            )
+
+            return Err(EmailRateLimitExceededError(seconds_to_retry_after=remaining_time))
 
         return Ok((participant.ok_value, team.ok_value))
 
     async def send_verification_email(
-        self, participant: Participant, team: Team | None, background_tasks: BackgroundTasks
-    ) -> None:
+        self, participant: Participant, background_tasks: BackgroundTasks, team: Team | None = None
+    ) -> Err[ParticipantNotFoundError | ValueError | Exception] | None:
+        """
+        Sends a verification email to admin and random participants and adds a timestamp when the last verification
+        email has been sent, for rate-limiting purposes
+        Args:
+            participant: the participant who should receive the email. Admin or random participant only, invite_link
+                participants are automatically verified.
+            team: the team which the admin participant created. Should be omitted when sending an email to a random
+                participant, as random teams are created only after hackathon registration closes.
+            background_tasks: This is passed initially from the routes as part of the FastAPI Dependency Injection
+                system. It is auto-wired for us. We use background tasks as we want to send an email only once we
+                have returned a response. In this way we are able to keep our response times sub-second as we don't
+                wait for the email to be sent, which could take more than 2 seconds.
+        Returns:
+            An Err if adding of the updating the participant document last_sent_verification_email property fails, or
+            sending the verification emails fails. Otherwise, returns None.
+        """
 
-        if environ["ENV"] == "TEST":
+        # Don't send emails when we are running tests
+        if is_test_env():
             return None
 
         # Build the payload for the Jwt token
@@ -295,42 +331,74 @@ class HackathonService:
         jwt_token = JwtUtility.encode_data(data=payload)
 
         # Append the Jwt to the endpoint
-        # TODO: Change the path to a front-end path where we will send a post request towards this endpoint
-        if environ["ENV"] == "PROD":
-            verification_link = f"https://thehub-aubg.com/api/v3/hackathon/participants/verify?jwt_token={jwt_token}"
-        else:
+        if is_prod_env():
+            verification_link = f"https://{DOMAIN}{self._PARTICIPANTS_VERIFICATION_ROUTE}?jwt_token={jwt_token}"
+        elif is_dev_env():
             verification_link = (
-                f"https://dev.thehub-aubg.com/api/v3/hackathon/participants/verify?jwt_token={jwt_token}"
+                f"https://{SUBDOMAIN}.{DOMAIN}{self._PARTICIPANTS_VERIFICATION_ROUTE}?jwt_token={jwt_token}"
             )
+        else:
+            verification_link = f"https://{DOMAIN}:{PORT}{self._PARTICIPANTS_VERIFICATION_ROUTE}?jwt_token={jwt_token}"
 
-        background_tasks.add_task(
-            self._mailing_service.send_participant_verification_email,
-            participant,
-            verification_link,
-            team.name if team else None,
+        # Update the last_sent_verification_email field for rate-limiting purposes
+        result = await self._participant_repo.update(
+            obj_id=str(participant.id), obj_fields=UpdateParticipantParams(last_sent_verification_email=datetime.now())
         )
 
-        # Update the last sent field
-        await self._participant_repo.update(
-            obj_id=str(participant.id), obj_fields=UpdateParticipantParams(last_sent_email=datetime.now())
+        # If the update fails, do not send the email and return the error
+        if is_err(result):
+            return result
+
+        LOG.info("Sending verification email...", email=participant.email)
+        err = self._mail_service.send_participant_verification_email(
+            participant=participant,
+            verification_link=verification_link,
+            background_tasks=background_tasks,
+            team_name=team.name if team else None,
         )
+        if err is not None:
+            return err
 
-    async def send_successful_registration_email(
-        self, participant: Participant, team: Team | None, background_tasks: BackgroundTasks
-    ) -> None:
+        return None
 
-        if environ["ENV"] == "TEST":
+    def send_successful_registration_email(
+        self, participant: Participant, background_tasks: BackgroundTasks, team: Team | None = None
+    ) -> Err[ValueError] | None:
+        """
+        Sends an email confirming the successful registration of a participant.
+        Args:
+            participant: The participant who should receive the email. Could be admin, random or invite_link
+            participant.
+            team: the team which the admin participant created. Should be omitted when sending an email to a random
+             or invite_link participants.
+            background_tasks: This is passed initially from the routes as part of the FastAPI Dependency Injection
+                system. It is auto-wired for us. We use background tasks as we want to send an email only once we
+                have returned a response. In this way we are able to keep our response times sub-second as we don't
+                wait for the email to be sent, which could take more than 2 seconds.
+        Returns:
+            An Err if sending the successful registration email fails
+        """
+        # Don't send emails when we are running tests
+        if is_test_env():
             return None
 
         if team is None:
-            # If we are dealing with a random participant or a participant with a link we dont need to create an
-            # invite link
-            background_tasks.add_task(
-                self._mailing_service.send_participant_successful_registration_email,
-                participant,
-                None,
-                None,
+            # When dealing with random participants we no team_name and invite_link should be present in the email
+            err = self._mail_service.send_participant_successful_registration_email(
+                participant=participant, background_tasks=background_tasks
             )
+            if err is not None:
+                return err
+
+            return None
+
+        if participant.is_admin is False:
+            # When dealing with invite_link participants we no invite_link should be present in the email
+            err = self._mail_service.send_participant_successful_registration_email(
+                participant=participant, team_name=team.name, background_tasks=background_tasks
+            )
+            if err is not None:
+                return err
 
             return None
 
@@ -342,15 +410,18 @@ class HackathonService:
         jwt_token = JwtUtility.encode_data(data=payload)
 
         # Append the Jwt to the endpoint
-        # TODO: Change the path to a front-end path where we will send a post request towards this endpoint
-        if environ["ENV"] == "PROD":
-            invite_link = f"https://thehub-aubg.com/hackathon/register?jwt_token={jwt_token}"
+        if is_prod_env():
+            invite_link = f"https://{DOMAIN}{self._PARTICIPANTS_REGISTRATION_ROUTE}?jwt_token={jwt_token}"
+        elif is_dev_env():
+            invite_link = f"https://{SUBDOMAIN}.{DOMAIN}{self._PARTICIPANTS_REGISTRATION_ROUTE}?jwt_token={jwt_token}"
         else:
-            invite_link = f"https://dev.thehub-aubg.com/hackthon/register?jwt_token={jwt_token}"
+            invite_link = f"https://{DOMAIN}:{PORT}{self._PARTICIPANTS_REGISTRATION_ROUTE}?jwt_token={jwt_token}"
 
-        background_tasks.add_task(
-            self._mailing_service.send_participant_successful_registration_email,
-            participant,
-            invite_link,
-            team.name,
+        LOG.info("Sending successful registration email...", email=participant.email)
+        err = self._mail_service.send_participant_successful_registration_email(
+            participant=participant, background_tasks=background_tasks, invite_link=invite_link, team_name=team.name
         )
+        if err is not None:
+            return err
+
+        return None
