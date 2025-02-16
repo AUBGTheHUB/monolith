82d09d9ae59580e9429070013032051f00ff33cf
Aleksandar <al.ivanov1908@gmail.com>
2025-02-16 18:54:11 +0000

    refactor

--- services/web/src/config.ts
+++ services/web/src/config.ts
@@ -1,20 +1,4 @@
-export type envType = 'DEV' | 'LOCAL' | 'PROD';
-
-const ENV: envType = import.meta.env.VITE_ENV || 'LOCAL';
-export const API_URLS = {
-    DEV: import.meta.env.VITE_API_URL_DEV,
-    LOCAL: import.meta.env.VITE_API_URL_LOCAL,
-    PROD: import.meta.env.VITE_API_URL_PROD,
-};
-
-export const API_URL = API_URLS[ENV] || API_URLS['LOCAL'];
-
-export const FEATURE_SWITCHES = [
-    { name: 'MentorsSwitch', state: false },
-    { name: 'JurySwitch', state: false },
-    { name: 'SponsorsSwitch', state: false },
-    { name: 'RegSwitch', state: false },
-];
+import { API_URL, FEATURE_SWITCHES } from './constants';
 
 export async function useFetchFeatureSwitches() {
     try {
