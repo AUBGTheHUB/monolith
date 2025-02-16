c7554b1d32f5dafc40ff17af0a9a52238eb817d9
Teodor Kovachev <tedox.k@gmail.com>
2025-02-16 10:12:46 +0000

    resolved comments

--- services/web/src/website/HackathonPage/LandingSection/LandingSection.tsx
+++ services/web/src/website/HackathonPage/LandingSection/LandingSection.tsx
@@ -1,6 +1,6 @@
 export default function LandingSection() {
     return (
-        <div className="relative w-full pb-[12rem] md:pb-[5rem] lg:pb-[10rem] xl:pb-[3rem] lg:b-[8rem]">
+        <div className="relative w-full pb-[15rem] md:pb-[8rem] lg:pb-[20rem] xl:pb-[12rem] lg:b-[10rem]">
             <img className="w-full z-0" src="/public/hackLanding/space.svg"></img>
             <img className="w-auto z-10 absolute z-0 top-0 right-0" src="/public/hackLanding/death_star.svg"></img>
             <div className="w-full  z-10 absolute lg:top-[11rem] md:top-[8rem] top-[3rem] inset-x-0 flex justify-center items-center">
--- services/web/src/website/HackathonPage/MissionSection/MissionSection.tsx
+++ services/web/src/website/HackathonPage/MissionSection/MissionSection.tsx
@@ -2,8 +2,11 @@ export default function MissionSection() {
     return (
         <div className="absolute xxl:top-[45rem] xl:top-[30rem]  xs:top-[10rem] lg:top-[20rem] md:top-[22rem] sm:top-[15rem] top-[3rem] w-full lg:min-h-[50rem] min-h-[5rem] pt-[4rem]  bg-gradient-to-b from-stone-950/0 from-10% via-hackBg/50 via-25%  to-hackBg to-50% z-20 flex justify-center">
             <div className="w-4/5 relative lg:top-[11rem] top-[5rem] inset-x-0  flex-column ">
-                <img className="lg:w-auto w-1/3" src="/public/hackMission/mission.svg"></img>
-                <div className=" text-mont text-[#A9B4C3] lg:py-20 py-10 lg:text-[15px] text-[7px]">
+                <div className="sm:text-4xl text-3xl sm:mb-20 mb-10 flex items-center ">
+                    <img src="./n.png" alt="" className="w-[1.6rem]" />
+                    <p className="text-white ml-5 tracking-[0.2em] font-normal">MISSION</p>
+                </div>
+                <div className=" text-mont text-[#A9B4C3] lg:py-20 py-10 lg:text-[15px] text-[7px] md:text-[13px]">
                     <p>
                         Over the course of 52 hours, you will work in teams to brainstorm and prototype your project
                         idea based on the topic we will reveal during the Opening Ceremony, as well as pitch your
@@ -11,13 +14,13 @@ export default function MissionSection() {
                         mentors on hand to offer guidance and support throughout the event. Plus, there will be awesome
                         prizes and food to keep you fueled and motivated!
                     </p>
-                    <p className="pt-10 md:pt-2 md:pb-2 lg:pb-30">
+                    <p className="pt-10 md:pt-2 md:pb-2 lg:pb-30 ">
                         HackAUBG is more than just a hackathon - it’s a community of innovators, collaborators, and
                         problem-solvers. We believe that together, we can create a better future through technology and
                         creativity.
                     </p>
                 </div>
-                <img className="pb-60" src="/public/hackMission/underline.svg"></img>
+                <img className="w-full pb-80 lg:pb-[30rem]" src="/public/hackMission/underline.svg"></img>
             </div>
         </div>
     );
