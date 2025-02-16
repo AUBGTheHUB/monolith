9331a830ae685f430f363ea6334165e5f0d604f6
Lyubomira Staninska <106514492+Bubomira@users.noreply.github.com>
2025-02-16 12:38:06 +0200

    #875 navigation for hack aubg page (#893)
    * #875 navigation for hack aubg page
    
    * #875 font and hover effect
    
    * #875 added mobile and toggle navigation with icons
    
    * #875 links fixed
    
    * #835 added participate now btn to mobile nav and transition to normal nav
    
    * #835 removed heading
    
    * #875 mobile button
    
    ---------
    
    Co-authored-by: Aleks Ivanov <al.ivanov1908@gmail.com>

--- services/web/src/website/HackathonPage/HackathonPage.tsx
+++ services/web/src/website/HackathonPage/HackathonPage.tsx
@@ -1,4 +1,6 @@
 import { Fragment } from 'react/jsx-runtime';
+
+import { Navigation } from './Navigation/Navigation';
 import { Footer } from './Footer/HackathonFooter';
 import ScheduleSection from './ScheduleSection/ScheduleSection';
 import { Recap } from './components/Recap';
@@ -6,6 +8,7 @@ import { Recap } from './components/Recap';
 export const HackathonPage = () => {
     return (
         <Fragment>
+            <Navigation />
             <ScheduleSection />
             <Recap />
             <Footer />
---
+++ services/web/src/website/HackathonPage/Navigation/DesktopNav.tsx
@@ -0,0 +1,33 @@
+export const DesktopNavComponent = () => {
+    const NAV_ITEM_A = 'text-white font-light font-mont';
+    const NAV_ITEM_A_EFFECT =
+        'hover:text-white relative after:content-[""] after:absolute after:w-full after:scale-x-0 after:h-[2px] after:bottom-[-4px] after:left-0 after:bg-white after:origin-bottom-right after:transition-transform after:duration-300 hover:after:scale-x-100 hover:after:origin-bottom-left';
+    return (
+        <div className="w-full h-[10%] bg-transparent border-b-2 border-gray-600 py-5">
+            <div className="w-full flex flex-row justify-center items-center">
+                <div className="flex flex-row w-[70%] gap-7">
+                    <a href="#about" className={`${NAV_ITEM_A} ${NAV_ITEM_A_EFFECT}`}>
+                        About
+                    </a>
+                    <a href="#schedule" className={`${NAV_ITEM_A} ${NAV_ITEM_A_EFFECT}`}>
+                        Schedule
+                    </a>
+                    <a href="#grading-criteria" className={`${NAV_ITEM_A} ${NAV_ITEM_A_EFFECT}`}>
+                        Grading Criteria
+                    </a>
+                    <a href="#faq" className={`${NAV_ITEM_A} ${NAV_ITEM_A_EFFECT}`}>
+                        FAQ
+                    </a>
+                </div>
+                <div>
+                    <a
+                        href="#participate-now"
+                        className={`${NAV_ITEM_A} border-2 border-sky-600 rounded-3xl px-4 py-1 hover:text-white hover:bg-sky-600 transition-colors duration-500`}
+                    >
+                        Participate now
+                    </a>
+                </div>
+            </div>
+        </div>
+    );
+};
