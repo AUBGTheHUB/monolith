2dfa8a560bbdfefcc1aac33e3704250a1def14a4
Aleksandar <al.ivanov1908@gmail.com>
2025-02-16 22:13:31 +0000

    remove things for dev

--- services/web/src/config.ts
+++ services/web/src/config.ts
@@ -1,16 +1,11 @@
-import { API_URL, ENV_MODE, FEATURE_SWITCHES } from './constants';
-
-const username = import.meta.env.VITE_DEV_USER;
-const password = import.meta.env.VITE_DEV_PASS;
+import { API_URL, FEATURE_SWITCHES } from './constants';
 
 export async function useFetchFeatureSwitches() {
     console.log('From env', API_URL);
 
     try {
         const headers = new Headers();
-        if (ENV_MODE === 'DEV') {
-            headers.set('Authorization', 'Basic ' + btoa(username + ':' + password));
-        }
+
         const response = await fetch(`${API_URL}/feature-switches`, {
             method: 'GET',
             headers: headers,
