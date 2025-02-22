af00e5f4e31221a7154512f22a1b5bbe22a931a0
Teodor Kovachev <tedox.k@gmail.com>
2025-02-22 12:07:16 +0000

    adjusted landing  page

--- services/web/src/website/HackathonPage/LandingSection/LandingSection.tsx
+++ services/web/src/website/HackathonPage/LandingSection/LandingSection.tsx
@@ -1,8 +1,8 @@
 export default function LandingSection() {
     return (
-        <div className="relative w-full pb-[12rem] sm:pb-[30rem] md:pb-[8rem] lg:pb-[30rem] xl:pb-[15rem] lg:b-[10rem] xs:pb-[18rem]">
+        <div className="relative w-full h-[90%] pb-[12rem] sm:pb-[30rem] md:pb-[22rem] lg:pb-[20rem] xl:pb-[25rem] lg:b-[10rem] xs:pb-[18rem]">
             <img className="w-full z-0" src="/public/hackLanding/space.svg"></img>
-            <img className="w-full z-0 block lg:hidden" src="/public/hackLanding/space.svg"></img>
+            <img className="w-full z-0 block xl:hidden" src="/public/hackLanding/space.svg"></img>
             <img className="w-full z-0 block xs:hidden" src="/public/hackLanding/space.svg"></img>
             <img
                 className="lg:w-auto z-10 absolute z-0 lg:top-0 right-0 top-[20rem] w-1/2 "
--- services/web/src/website/HackathonPage/MissionSection/MissionSection.tsx
+++ services/web/src/website/HackathonPage/MissionSection/MissionSection.tsx
@@ -1,12 +1,12 @@
 export default function MissionSection() {
     return (
-        <div className="absolute xxl:top-[50girem] xl:top-[30rem]  xs:top-[20rem] lg:top-[35rem] md:top-[50rem] sm:top-[30rem] top-[19rem] w-full lg:min-h-[50rem] min-h-[5rem] pt-[4rem]  bg-gradient-to-b from-stone-950/0 from-10% via-hackBg/50 via-25%  to-hackBg to-50% z-20 flex justify-center">
+        <div className="absolute xxl:top-[50rem] xl:top-[50rem]  xs:top-[20rem] lg:top-[50rem] md:top-[54rem] sm:top-[30rem] top-[19rem] w-full lg:min-h-[50rem] min-h-[5rem] pt-[4rem]  bg-gradient-to-b from-stone-950/0 from-10% via-hackBg/50 via-25%  to-hackBg to-50% z-20 flex justify-center">
             <div className="w-4/5 relative lg:top-[11rem] top-[5rem] inset-x-0  flex-column ">
                 <div className="sm:text-4xl text-3xl sm:mb-5 mb-5 flex items-center ">
                     <img src="./n.png" alt="" className="w-[1.6rem]" />
                     <p className="text-white ml-5 tracking-[0.2em] font-normal">MISSION</p>
                 </div>
-                <div className=" text-mont text-[#A9B4C3] lg:py-20 py-10 lg:text-[20px] text-[10px] md:text-[15px]">
+                <div className=" text-mont text-[#A9B4C3] lg:py-20 py-10 sm:text-lg">
                     <p>
                         Over the course of 52 hours, you will work in teams to brainstorm and prototype your project
                         idea based on the topic we will reveal during the Opening Ceremony, as well as pitch your
