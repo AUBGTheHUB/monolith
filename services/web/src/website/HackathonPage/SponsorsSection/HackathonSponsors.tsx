698d2c4c48ccbf0bfd311c208333a429f5ba987a
Isak Bego <isakbego@gmail.com>
2025-03-01 13:55:17 +0200

    filter, sort and display sponsors on desktop

--- services/web/src/website/HackathonPage/SponsorsSection/HackathonSponsors.tsx
+++ services/web/src/website/HackathonPage/SponsorsSection/HackathonSponsors.tsx
@@ -1,32 +1,65 @@
+import { Sponsors } from './constants';
+
 type SponsorRank = 'Platinum' | 'Gold' | 'Silver';
 
-function SponsorSection({ rank }: { rank: SponsorRank }) {
+export interface HackathonSponsorProps {
+    rank: SponsorRank;
+    name: string;
+    logoSrc: string;
+}
+
+function SponsorCard({ sponsor }: { sponsor: HackathonSponsorProps }) {
+    return (
+        <div className="border border-[#233340] bg-white rounded-lg min-w-40 max-w-60 min-h-10 aspect-square w-full sm:w-1/5 p-4">
+            <img src={sponsor.logoSrc} alt={sponsor.name} className="w-full h-full object-contain" />
+        </div>
+    );
+}
+
+function SponsorSection({ rank, sponsors }: { rank: SponsorRank; sponsors: HackathonSponsorProps[] }) {
     const rankColor = rank === 'Platinum' ? '#19A0F0' : rank === 'Gold' ? '#FFDE06' : '#92B1C9';
     return (
-        <div>
-            <div className="text-xl py-4" style={{ color: rankColor }}>
-                <h3>{rank}</h3>
+        <div className="pb-20">
+            <div>
+                <div className="text-xl py-4" style={{ color: rankColor }}>
+                    <h3>{rank}</h3>
+                </div>
+                <div className="w-full h-[1px] bg-[#233340] mb-10 relative">
+                    <div className="bg-[#AEC1D0] h-[1px] w-10 aboslute left-0"></div>
+                    <div className="bg-[#AEC1D0] h-[5px] w-[5px] absolute rounded-full right-0 top-[-2.5px]"></div>
+                    <div className="bg-[#AEC1D0] h-[5px] w-[5px] absolute rounded-full right-2 top-[-2.5px]"></div>
+                    <div className="bg-[#AEC1D0] h-[5px] w-[5px] absolute rounded-full right-4 top-[-2.5px]"></div>
+                </div>
             </div>
-            <div className="w-full h-[1px] bg-[#233340] mb-10 relative">
-                <div className="bg-[#AEC1D0] h-[1px] w-10 aboslute left-0"></div>
-                <div className="bg-[#AEC1D0] h-[5px] w-[5px] absolute rounded-full right-0 top-[-2.5px]"></div>
-                <div className="bg-[#AEC1D0] h-[5px] w-[5px] absolute rounded-full right-2 top-[-2.5px]"></div>
-                <div className="bg-[#AEC1D0] h-[5px] w-[5px] absolute rounded-full right-4 top-[-2.5px]"></div>
+            <div className="flex flex-wrap gap-2">
+                {sponsors.map((sponsor) => (
+                    <SponsorCard sponsor={sponsor} key={sponsor.name} />
+                ))}
             </div>
         </div>
     );
 }
 
 export default function HackathonSponsors() {
+    // Filter the sponsors
+    const platinumSponsors = Sponsors.filter((sponsor) => sponsor.rank === 'Platinum');
+    const goldSponsors = Sponsors.filter((sponsor) => sponsor.rank === 'Gold');
+    const silverSponsors = Sponsors.filter((sponsor) => sponsor.rank === 'Silver');
+
+    //Sort each category alphabetically
+    platinumSponsors.sort((a, b) => a.name.localeCompare(b.name));
+    goldSponsors.sort((a, b) => a.name.localeCompare(b.name));
+    silverSponsors.sort((a, b) => a.name.localeCompare(b.name));
+
     return (
         <div className="text-white sm:w-[80%] mx-6 sm:mx-auto py-10 sm:py-20">
             <div className="text-3xl sm:text-4xl flex items-center gap-4 mb-20">
                 <img src="./n.png" alt="" className="w-[1.6rem]" />
                 <h2>Sponsors</h2>
             </div>
-            <SponsorSection rank="Platinum" />
-            <SponsorSection rank="Gold" />
-            <SponsorSection rank="Silver" />
+            <SponsorSection rank="Platinum" sponsors={platinumSponsors} />
+            <SponsorSection rank="Gold" sponsors={goldSponsors} />
+            <SponsorSection rank="Silver" sponsors={silverSponsors} />
         </div>
     );
 }
---
+++ services/web/src/website/HackathonPage/SponsorsSection/constants.tsx
@@ -0,0 +1,94 @@
+import { HackathonSponsorProps } from './HackathonSponsors';
+
+export const Sponsors: HackathonSponsorProps[] = [
+    {
+        name: 'Base Line',
+        rank: 'Platinum',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/baselinefixed.png',
+    },
+    {
+        name: 'Betahaus Sofia',
+        rank: 'Gold',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/betahaus-sofia-logo.png',
+    },
+    {
+        name: 'Bianor',
+        rank: 'Silver',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/Bianor-logo.png',
+    },
+    {
+        name: 'Bosh',
+        rank: 'Platinum',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/boshnew.svg',
+    },
+    {
+        name: 'Carbon',
+        rank: 'Gold',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/carbon.png',
+    },
+    {
+        name: 'Centio',
+        rank: 'Silver',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/centio.png',
+    },
+    {
+        name: 'Codexio',
+        rank: 'Platinum',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/codexio-logo.png',
+    },
+    {
+        name: 'Commerzbank',
+        rank: 'Gold',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/Commerzbank-logo.png',
+    },
+    {
+        name: 'Dev.bg',
+        rank: 'Silver',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/devBgLogo.png',
+    },
+    {
+        name: 'DevStyleR',
+        rank: 'Platinum',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/DevStyleR-logo.png',
+    },
+    {
+        name: 'eAcademia',
+        rank: 'Gold',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/eAcademia.png',
+    },
+    {
+        name: 'Eviden',
+        rank: 'Silver',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/eviden.png',
+    },
+    {
+        name: 'Gameloft',
+        rank: 'Platinum',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/Gameloft-logo.png',
+    },
+    {
+        name: 'HackSoft',
+        rank: 'Gold',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/ITCE-logo.png',
+    },
+    {
+        name: 'M2M',
+        rank: 'Silver',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/m2m.png',
+    },
+    {
+        name: 'MentorMate',
+        rank: 'Platinum',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/Tuk-Tam-logo.png',
+    },
+    {
+        name: 'Musala Soft',
+        rank: 'Gold',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/yettel.svg',
+    },
+    {
+        name: 'Resolute',
+        rank: 'Silver',
+        logoSrc: 'https://hubarskibucket.s3.eu-central-1.amazonaws.com/resolute-logo.png',
+    },
+];
