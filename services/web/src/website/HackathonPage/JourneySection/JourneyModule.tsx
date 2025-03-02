c82380f3efe345e26f98caf169f572624b64d460
Teodor Kovachev <tedox.k@gmail.com>
2025-03-02 16:53:07 +0000

    @alex

--- services/web/src/website/HackathonPage/JourneySection/JourneyModule.tsx
+++ services/web/src/website/HackathonPage/JourneySection/JourneyModule.tsx
@@ -3,7 +3,7 @@ type JourneyModule = {
     text: string;
 };
 
-function JourneyModule({ title, text }: JourneyModule) {
+function DesktopJourneyModule({ title, text }: JourneyModule) {
     return (
         <div className="scroll-section text-white">
             <div className="border-2 border-[#233340] rounded-md p-6 bg-[#000912] w-[100vh] relative">
@@ -20,4 +20,20 @@ function JourneyModule({ title, text }: JourneyModule) {
     );
 }
 
-export default JourneyModule;
+function MobileJourneyModule({ title, text }: JourneyModule) {
+    return (
+        <div className="w-full text-white">
+            <div className="border-2 border-[#233340] rounded-md p-6 bg-[#000912] w-3/4 mx-auto relative">
+                <div className="absolute left-[0.30rem] top-[20%] w-1 h-24 bg-white rounded-lg -translate-x-2"></div>
+                <h3 className="text-white text-xl mb-2">{title}</h3>
+                <p className="text-[#A9B4C3] md:text-lg text-xs">{text}</p>
+                <div className="absolute right-6 bottom-0 flex gap-2 translate-y-1/2">
+                    <div className="w-2 h-2 rounded-full bg-white"></div>
+                    <div className="w-2 h-2 rounded-full bg-white"></div>
+                    <div className="w-2 h-2 rounded-full bg-white"></div>
+                </div>
+            </div>
+        </div>
+    );
+}
+export { MobileJourneyModule, DesktopJourneyModule };
--- services/web/src/website/HackathonPage/JourneySection/JourneySection.tsx
+++ services/web/src/website/HackathonPage/JourneySection/JourneySection.tsx
@@ -1,7 +1,7 @@
-import { useRef, useLayoutEffect } from 'react';
+import { useRef, useLayoutEffect, useEffect, useState, Fragment } from 'react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
-import JourneyModule from './JourneyModule';
+import { DesktopJourneyModule, MobileJourneyModule } from './JourneyModule';
 import journeyEntries from './StaticContent/journeyEntries.json';
 
 gsap.registerPlugin(ScrollTrigger);
@@ -10,7 +10,19 @@ function JourneySection() {
     const triggerRef = useRef<HTMLDivElement>(null);
     const itemsRef = useRef<HTMLDivElement[]>([]);
     const mm = useRef(gsap.matchMedia()); // Store MatchMedia instance
+    const [isDesktop, setIsDesktop] = useState(window.innerWidth >= 768);
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsDesktop(window.innerWidth >= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
     // Helper function to add refs to items
     const addToRefs = (el: HTMLDivElement) => {
         if (el && !itemsRef.current.includes(el)) {
@@ -71,31 +83,54 @@ function JourneySection() {
     }, []);
 
     return (
-        <div className="w-full relative">
-            {/* Sticky Title */}
-            <div className="sticky md:h-[50rem] h-[48rem]  top-0 z-50 pt-[6rem] md:pt-[6rem] ml-[9%] bg-transparent">
-                <div className="sm:text-4xl text-3xl sm:mb-20 mb-10 flex items-center space-x-4 p-4">
-                    <img src="./JourneySection/symbol.svg" alt="" className="w-[1.6rem]" />
-                    <p className="text-white tracking-[0.2em]">JOURNEY</p>
-                </div>
-            </div>
-
-            {/* Animated Section */}
-            <section className="scroll-section-outer mr-20 gap-[20rem] min-h-[40rem]">
-                <div ref={triggerRef} className="mr-0 relative w-full h-screen">
-                    {journeyEntries.map((entry, index) => (
-                        <div
-                            key={index}
-                            ref={addToRefs}
-                            className="absolute top-0 left-0 w-full"
-                            style={{ zIndex: index + 1 }}
-                        >
-                            <JourneyModule title={entry.title} text={entry.text} />
+        <Fragment>
+            {isDesktop ? (
+                <div className="w-full relative">
+                    {/* Sticky Title */}
+                    <div className="sticky md:h-[50rem] h-[48rem]  top-0 z-50 pt-[6rem] md:pt-[6rem] ml-[9%] bg-transparent">
+                        <div className="sm:text-4xl text-3xl sm:mb-20 mb-10 flex items-center space-x-4 p-4">
+                            <img src="./JourneySection/symbol.svg" alt="" className="w-[1.6rem]" />
+                            <p className="text-white tracking-[0.2em]">JOURNEY</p>
                         </div>
-                    ))}
+                    </div>
+
+                    {/* Animated Section */}
+                    <section className="scroll-section-outer mr-20 gap-[20rem] min-h-[40rem]">
+                        <div ref={triggerRef} className="mr-0 relative w-full h-screen">
+                            {journeyEntries.map((entry, index) => (
+                                <div
+                                    key={index}
+                                    ref={addToRefs}
+                                    className="absolute top-0 left-0 w-full"
+                                    style={{ zIndex: index + 1 }}
+                                >
+                                    <DesktopJourneyModule title={entry.title} text={entry.text} />
+                                </div>
+                            ))}
+                        </div>
+                    </section>
                 </div>
-            </section>
-        </div>
+            ) : (
+                <div className="w-full relative">
+                    <div className="pt-[6rem] md:pt-[6rem] ml-[9%] bg-transparent">
+                        <div className="sm:text-4xl text-3xl sm:mb-20 mb-10 flex items-center space-x-4 p-4">
+                            <img src="./JourneySection/symbol.svg" alt="" className="w-[1.6rem]" />
+                            <p className="text-white tracking-[0.2em]">JOURNEY</p>
+                        </div>
+                    </div>
+
+                    <section className=" min-h-[40rem] relative w-full">
+                        <div className="relative w-full flex flex-col gap-[5rem] object-center">
+                            {journeyEntries.map((entry, index) => (
+                                <div key={index} className="w-full" style={{ zIndex: index + 1 }}>
+                                    <MobileJourneyModule title={entry.title} text={entry.text} />
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                </div>
+            )}
+        </Fragment>
     );
 }
 
