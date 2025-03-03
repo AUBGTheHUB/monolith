1af6ab65c3fa0b423775a2dfc2392d22265a4cc9
Klevi Dervishi <116805095+cl3vy@users.noreply.github.com>
2025-03-03 21:32:07 +0100

    Journey section done (#905)
    ---------
    
    Co-authored-by: Teodor Kovachev <tedox.k@gmail.com>
    Co-authored-by: Isak Bego <isakbego@gmail.com>
    Co-authored-by: Ivan Obreshkov <ivanobreshkov12@gmail.com>

--- services/web/src/website/HackathonPage/HackathonPage.tsx
+++ services/web/src/website/HackathonPage/HackathonPage.tsx
@@ -10,6 +10,7 @@ import MissionSection from './MissionSection/MissionSection';
 import AwardsSection from './AwardsSection/AwardsSection';
 import { Recap } from './components/Recap';
 import { useFeatureSwitches } from '@/config';
+import JourneySection from './JourneySection/JourneySection';
 import HackathonFAQSection from './FAQSection/HackathonFAQ';
 
 export const HackathonPage = () => {
@@ -22,6 +23,8 @@ export const HackathonPage = () => {
                 <LandingSection />
                 <MissionSection />
             </div>
+
+            <JourneySection />
             <MentorsSection mentorsSwitch={featureSwitches.MentorsSwitch} />
             <JurySection jurySwitch={featureSwitches.JurySwitch} />
             <ScheduleSection />
