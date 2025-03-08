aaa0f5da06f39c003e38d90a3d7f11f2214ccc7e
Aleks4o <aleks4o@Aleks4os-MacBook-Pro.local>
2025-03-08 23:13:44 +0000

    make star smaller

--- services/web/src/website/HackathonPage/LandingSection/LandingSection.tsx
+++ services/web/src/website/HackathonPage/LandingSection/LandingSection.tsx
@@ -3,7 +3,7 @@ import MissionSection from '../MissionSection/MissionSection';
 export default function LandingSection() {
     return (
         <div
-            className=" w-full h-auto lg:pb-[45rem] max-[1024px]:min-h-[100vh] pb-[55rem] relative"
+            className=" w-full h-auto lg:pb-[55rem] max-[1024px]:min-h-[100vh] pb-[55rem] relative"
             style={{
                 backgroundImage: 'url("/verifyPage/background.png")',
                 backgroundSize: 'cover',
@@ -12,7 +12,7 @@ export default function LandingSection() {
             }}
         >
             <img
-                className="lg:w-auto absolute z-10 lg:top-0 right-0 top-[25rem] w-[70%]"
+                className="lg:w-[40%] absolute z-10 lg:top-0 right-0 top-[25rem] w-[70%]"
                 src="/hackLanding/death_star.svg"
             />
             <div className="w-full flex justify-center  min-h-[100vh] lg:min-h-[80vh]">
