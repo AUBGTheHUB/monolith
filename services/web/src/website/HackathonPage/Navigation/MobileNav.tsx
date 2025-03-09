27380ca1902d328553d52aa4af2839c3f868f572
Isak Bego <isakbego@gmail.com>
2025-03-09 22:08:44 +0100

    make mobile nav close when a nav option is selected

--- services/web/src/website/HackathonPage/Navigation/MobileNav.tsx
+++ services/web/src/website/HackathonPage/Navigation/MobileNav.tsx
@@ -8,11 +8,14 @@ import faq from './images/faq.svg';
 import participate from './images/participate.svg';
 
 import { Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger } from '@/components/ui/sheet';
+import { useState } from 'react';
 
 export const MobileNavComponent = () => {
     const NAV_ITEM_A = 'text-black hover:text-black';
     const NAV_ITEM = 'text-[18pt] text-left leading-[1.5em] my-4';
     const NAV_ITEM_IMG = 'mt-[5px] float-left mr-[9px] h-[25px] w-[25px] align-middle';
+
+    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <nav className="sticky h-[10vh] top-0 z-[100] bg-[rgba(0,0,0,0.5)]" aria-label="Mobile Navigation">
@@ -22,33 +25,33 @@ export const MobileNavComponent = () => {
                 </a>
                 <h2 className="text-white font-medium text-2xl mr-[10px]">The Hub</h2>
                 <div className="h-[50px]">
-                    <Sheet>
+                    <Sheet open={isOpen}>
                         <SheetTitle />
                         <SheetDescription />
                         <SheetTrigger className="h-[50px]">
-                            <img src={hamburgerMenuIcon} />
+                            <img src={hamburgerMenuIcon} onClick={() => setIsOpen(true)} />
                         </SheetTrigger>
                         <SheetContent className="z-[100]">
                             <div className={NAV_ITEM}>
-                                <a href="#mission" className={NAV_ITEM_A}>
+                                <a href="#mission" className={NAV_ITEM_A} onClick={() => setIsOpen(false)}>
                                     <img src={about} className={NAV_ITEM_IMG} />
                                     About
                                 </a>
                             </div>
                             <div className={NAV_ITEM}>
-                                <a href="#schedule" className={NAV_ITEM_A}>
+                                <a href="#schedule" className={NAV_ITEM_A} onClick={() => setIsOpen(false)}>
                                     <img src={schedule} className={NAV_ITEM_IMG} />
                                     Schedule
                                 </a>
                             </div>
                             <div className={NAV_ITEM}>
-                                <a href="#grading-criteria" className={NAV_ITEM_A}>
+                                <a href="#grading-criteria" className={NAV_ITEM_A} onClick={() => setIsOpen(false)}>
                                     <img src={grading} className={NAV_ITEM_IMG} />
                                     Grading Criteria
                                 </a>
                             </div>
                             <div className={NAV_ITEM}>
-                                <a href="#faq" className={NAV_ITEM_A}>
+                                <a href="#faq" className={NAV_ITEM_A} onClick={() => setIsOpen(false)}>
                                     <img src={faq} className={NAV_ITEM_IMG} />
                                     FAQ
                                 </a>
