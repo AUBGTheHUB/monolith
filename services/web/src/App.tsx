bb17e1efe0c6034376339b2637977128cfd2f321
MagdalenaZh <magdalenazheleva1@gmail.com>
2025-03-10 16:46:46 +0000

    delete old app page

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { OldAppPage } from './website/OldAppPage/OldAppPage';
 import { MainPage } from './website/MainPage/MainPage';
 import { HackathonPage } from './website/HackathonPage/HackathonPage';
 import { VerificationPage } from './website/VerificationPage/VerificationPage';
@@ -12,7 +11,6 @@ function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <Routes>
-                <Route path="/old" element={<OldAppPage />} />
                 <Route path="/" element={<MainPage />} />
                 <Route path="/hackathon" element={<HackathonPage />} />
                 <Route path="/hackathon/registration" element={<FormPage />} />
