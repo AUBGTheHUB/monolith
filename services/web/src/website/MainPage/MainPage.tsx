52a208235afe522261fc6a9e6105d0259cff07c1
Isak Bego <isakbego@gmail.com>
2025-03-10 21:20:26 +0100

    fix the placement of the blobs

--- services/web/src/website/MainPage/MainPage.tsx
+++ services/web/src/website/MainPage/MainPage.tsx
@@ -15,7 +15,7 @@ export const MainPage = () => {
             <LandingSection />
             <div className="relative overflow-hidden">
                 <div className="about-section bg-white rounded-2xl py-8 z-10 relative">
-                    <div className="sm:w-3/5 w-11/12 mx-auto">
+                    <div className="sm:w-3/5 w-11/12 mx-auto z-10">
                         <IdentitySection />
                         <PastEventSection />
                     </div>
@@ -23,17 +23,12 @@ export const MainPage = () => {
                 <img
                     src="/landingSection/gradient-top.png"
                     alt="a gradient"
-                    className="absolute pointer-events-none h-[93rem] right-[0rem] z-0"
+                    className="absolute pointer-events-none hidden sm:block h-[80rem] w-full top-[-30rem] right-[0rem] z-0"
                 />
                 <img
                     src="/meetTheTeam/gradient-meet-the-team.png"
-                    className="absolute pointer-events-none h-[1490.43px] w-[1505.76px] bottom-[0rem] right-[-8rem] rotate-210 z-0"
-                />
-                <img
-                    src="/footer/footer-gradient.png"
-                    alt="a gradient"
-                    className="absolute pointer-events-none h-[53rem] w-[56rem] right-[-8rem] top-[-30rem] z-0"
-                />
+                    className="absolute pointer-events-none hidden sm:block h-[80rem] w-full bottom-[40rem] right-[0rem] rotate-210 z-0"
+                ></img>
             </div>
             <div className="bg-transparent pt-[7.5rem] pb-[7rem] relative overflow-hidden" id="meet-team">
                 <MeetTheTeam />
@@ -46,7 +41,7 @@ export const MainPage = () => {
                 ></img>
                 <img
                     src="/footer/footer-gradient.png"
-                    className="absolute pointer-events-none h-[852px] top-[-35rem] right-[15rem] rotate-45"
+                    className="absolute h-[70rem] bottom-[-15rem] right-[-23rem] pointer-events-none w-screen"
                 />
             </div>
             <Footer />
