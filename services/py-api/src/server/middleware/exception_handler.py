2434bfaa49c768f65f9b683406a049e1de884d6e
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2025-04-13 23:24:38 +0300

    Fix docs and route_dependenices

--- services/py-api/src/server/middleware/exception_handler.py
+++ services/py-api/src/server/middleware/exception_handler.py
@@ -8,7 +8,8 @@ class ExceptionHandler:
     def __init__(self, app: FastAPI) -> None:
         @app.exception_handler(HTTPException)
         async def custom_exception_handler(request: Request, exc: HTTPException) -> Response:
-            """This is needed to keep consistency in Error format in Swagger. Instead of "error", the default key is
-            the JSON response is "detail"
+            """
+            This is needed to keep consistency in Error format in Swagger. Instead of "error", the default key is
+            the JSON response is "detail" when we raise an HTTPException. This middleware changes this key to "error".
             """
             return Response(status_code=exc.status_code, response_model=ErrResponse(error=exc.detail))
--- services/py-api/src/server/routes/route_dependencies.py
+++ services/py-api/src/server/routes/route_dependencies.py
@@ -9,11 +9,12 @@ from typing import Annotated
 
 from bson import ObjectId
 from fastapi import Header, HTTPException, Path
-from httpx import AsyncClient
+from result import is_err
 from starlette import status
+from starlette.requests import Request
 from structlog.stdlib import get_logger
 
-from src.environment import DOMAIN, is_local_env, PORT, is_test_env
+from src.database.repository.feature_switch_repository import FeatureSwitchRepository
 from src.service.hackathon.hackathon_service import HackathonService
 
 LOG = get_logger()
@@ -45,27 +46,14 @@ def validate_obj_id(object_id: Annotated[str, Path()]) -> None:
         raise HTTPException(detail="Wrong Object ID format", status_code=400)
 
 
-async def is_registration_open() -> None:
-    if is_local_env() or is_test_env():
-        base_url = f"https://{DOMAIN}:{PORT}/api/v3"
-        verify_ssl = False
-    else:
-        base_url = f"https://{DOMAIN}/api/v3"
-        verify_ssl = True
+async def is_registration_open(request: Request) -> None:
+    fs_repo: FeatureSwitchRepository = request.app.state.fs_repo
 
-    async with AsyncClient(base_url=base_url, verify=verify_ssl) as client:
-        resp = await client.get(f"/feature-switches/{HackathonService.REG_ALL_PARTICIPANTS_SWITCH}")
-
-    if resp.status_code != 200:
-        LOG.error(
-            "Error while fetching FS before registration request is accepted",
-            feature=HackathonService.REG_ALL_PARTICIPANTS_SWITCH,
-            status_code=resp.status_code,
-        )
+    result = await fs_repo.get_feature_switch(HackathonService.REG_ALL_PARTICIPANTS_SWITCH)
+    if is_err(result):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="An unexpected error occurred")
 
-    resp_data = resp.json()
-    if not resp_data.get("feature", {}).get("state", False):
+    if result.ok_value.state is False:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail="Registration is closed")
 
 
