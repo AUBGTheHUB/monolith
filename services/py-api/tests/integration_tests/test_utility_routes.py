eb4553bd961dacaa0a211d82770c0efb6f364593
Isak Bego <isakbego@gmail.com>
2025-04-18 18:54:23 +0300

    fix team and utility routes

--- services/py-api/tests/integration_tests/test_utility_routes.py
+++ services/py-api/tests/integration_tests/test_utility_routes.py
@@ -8,14 +8,20 @@ PING_ENDPOINT_URL = "/api/v3/ping"
 
 @pytest.mark.asyncio
 async def test_ping_endpoint_service_unavailable(async_client: AsyncClient) -> None:
-    with mock.patch("src.database.db_manager.DatabaseManager.async_ping_db", return_value=Err("Test err")):
+    with mock.patch("src.database.mongo.db_manager.MongoDatabaseManager.async_ping_db", return_value=Err("Test err")):
+        # When
         resp = await async_client.get(PING_ENDPOINT_URL)
+
+        # Then
         assert resp.status_code == 503
         assert resp.json() == {"error": "Database not available!"}
 
 
 @pytest.mark.asyncio
 async def test_ping_endpoint(async_client: AsyncClient) -> None:
+    # When
     resp = await async_client.get(PING_ENDPOINT_URL)
+
+    # Then
     assert resp.status_code == 200
     assert resp.json() == {"message": "pong"}
