c125290ddbe8410c77aa088ee49bbb199ed53402
ivakalol <ivko0527@gmail.com>
2025-11-06 11:37:24 +0200

    feat: Implement admin page dashboard (Closes #1084)

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -4,6 +4,7 @@ import { MainPage } from './website/MainPage/MainPage';
 import { HackathonPage } from './website/HackathonPage/HackathonPage';
 import { VerificationPage } from './website/VerificationPage/VerificationPage';
 import { FormPage } from './website/RegistrationFormPage/RegistrationFormPage';
+import { DashboardPage } from './website/AdminPanelPage/DashboardPage/DashboardPage.tsx';
 
 function App() {
     const queryClient = new QueryClient();
@@ -15,6 +16,7 @@ function App() {
                 <Route path="/hackathon" element={<HackathonPage />} />
                 <Route path="/hackathon/registration" element={<FormPage />} />
                 <Route path="/hackathon/verification" element={<VerificationPage />} />
+                <Route path="/dashboard" element={<DashboardPage />} />
             </Routes>
         </QueryClientProvider>
     );
