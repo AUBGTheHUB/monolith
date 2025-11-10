72f80a71dba979ae9e8eef83cb2914bf1b21b1a8
Georgi Nikolov <gnnikoll@gmail.com>
2025-11-10 20:12:23 +0000

    Force lf line endings

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,7 +5,6 @@ npm run prepare
 # Installs gum - helps with creating interactive terminal user interfaces
 make install-gum
 # Enables the .venv folder to appear inside the project - currently ./services/py_api - poetry installs all the deps on this
-poetry config virtualenvs.in-project true
 # This makes sure we are using the current python version configured by pyenv. https://python-poetry.org/docs/managing-environments/
 poetry config virtualenvs.prefer-active-python true
 # Installs the node_modules inside ./services/web
