75bd71813aec811aa7b14de6b850ff0b4020781e
Georgi Nikolov <gnnikoll@gmail.com>
2025-11-19 11:18:26 +0000

    Fix poetry version issue

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,20 +1,20 @@
 # Installs the node_modules in the project root
 npm install
+
 # Installs husky - pre-commit manager for .js, .css, .html
 npm run prepare
+
 # Installs gum - helps with creating interactive terminal user interfaces
 make install-gum
+
 # Enables the .venv folder to appear inside the project - currently ./services/py_api - poetry installs all the deps on this
 # This makes sure we are using the current python version configured by pyenv. https://python-poetry.org/docs/managing-environments/
-#poetry config virtualenvs.prefer-active-python true
-#poetry config virtualenvs.prefer-active-python true
-
 poetry config virtualenvs.create true
 poetry config virtualenvs.in-project true   # optional
 
-
 # Installs the node_modules inside ./services/web
 make install-web
+
 # Installs the backend dependencies
 cd ./services/py-api/
 poetry install
