b536837913ecc2db01b277a2324ea98750adb8b0
SamuilMladenov <samuil.v.mladenov@gmail.com>
2025-11-23 18:24:10 +0000

    remove redundant unit test

--- services/py-api/tests/unit_tests/admin/test_admin_sponsors_route.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from httpx import AsyncClient
-
-
-@pytest.mark.asyncio
-async def test_admin_sponsors_list_returns_501(async_client: AsyncClient) -> None:
-    resp = await async_client.get("/api/v3/admin/sponsors")
-    assert resp.status_code == 501
-    assert resp.json()["detail"] == "Not implemented"
