ce45ab4aa8eb8f33a357c29a0cb24f077ca8598d
Ivan Obreshkov <ivanobreshkov12@gmail.com>
2025-12-22 00:55:51 +0200

    Rewrite Dockerfile, to remove go downlaod, fix issues and use mount caches

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+
+# Manually load NVM because non-interactive shells ignore .bashrc
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+if command -v pyenv 1>/dev/null 2>&1; then
+  eval "$(pyenv init -)"
+fi
 
 #linux devcontainer ownership workaround (when ran as root ownership of files is not $USER):
 git config --global --add safe.directory /workspaces/monolith
@@ -7,9 +18,6 @@ npm install
 
 # Installs husky - pre-commit manager for .js, .css, .html
 npm run prepare
-
-# Installs gum - helps with creating interactive terminal user interfaces
-make install-gum
 
 # Enables the .venv folder to appear inside the project - currently ./services/py_api - poetry installs all the deps on this
 # This makes sure we are using the current python version configured by pyenv. https://python-poetry.org/docs/managing-environments/
