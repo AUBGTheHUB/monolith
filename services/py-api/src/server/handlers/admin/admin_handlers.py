505fae255b43e99fadc5981c997df22cd57d79d8
SamuilMladenov <samuil.v.mladenov@gmail.com>
2026-01-03 20:43:12 +0000

    admin panel handlers redesign

--- services/py-api/src/server/handlers/admin/admin_handlers.py
+++ services/py-api/src/server/handlers/admin/admin_handlers.py
@@ -1,5 +1,3 @@
-from dataclasses import dataclass
-
 from src.service.admin.sponsors_service import SponsorsService
 from src.service.admin.mentors_service import MentorsService
 from src.service.admin.judges_service import JudgesService
@@ -7,22 +5,18 @@ from src.service.admin.hub_members_service import HubMembersService
 from src.service.admin.past_events_service import PastEventsService
 
 
-@dataclass(kw_only=True, frozen=True)
-class AdminHandlers:
-    sponsors_service: SponsorsService
-    mentors_service: MentorsService
-    judges_service: JudgesService
-    hub_members_service: HubMembersService
-    past_events_service: PastEventsService
+# Sponsors Handlers
+class SponsorsHandlers(BaseException):
+    def __init__(self, service: SponsorsService) -> None:
+        self._service = service
 
-    # Sponsors
+    async def create_sponsor(self) -> None:
+        raise NotImplementedError()
+
     async def list_sponsors(self) -> None:
         raise NotImplementedError()
 
     async def get_sponsor(self, sponsor_id: str) -> None:
-        raise NotImplementedError()
-
-    async def create_sponsor(self) -> None:
         raise NotImplementedError()
 
     async def update_sponsor(self, sponsor_id: str) -> None:
@@ -31,14 +25,19 @@ class AdminHandlers:
     async def delete_sponsor(self, sponsor_id: str) -> None:
         raise NotImplementedError()
 
-    # Mentors
+
+# Mentors Handlers
+class MentorsHandlers(BaseException):
+    def __init__(self, service: MentorsService) -> None:
+        self._service = service
+
+    async def create_mentor(self) -> None:
+        raise NotImplementedError()
+
     async def list_mentors(self) -> None:
         raise NotImplementedError()
 
     async def get_mentor(self, mentor_id: str) -> None:
-        raise NotImplementedError()
-
-    async def create_mentor(self) -> None:
         raise NotImplementedError()
 
     async def update_mentor(self, mentor_id: str) -> None:
@@ -47,14 +46,19 @@ class AdminHandlers:
     async def delete_mentor(self, mentor_id: str) -> None:
         raise NotImplementedError()
 
-    # Judges
+
+# Judges Handlers
+class JudgesHandlers(BaseException):
+    def __init__(self, service: JudgesService) -> None:
+        self._service = service
+
+    async def create_judge(self) -> None:
+        raise NotImplementedError()
+
     async def list_judges(self) -> None:
         raise NotImplementedError()
 
     async def get_judge(self, judge_id: str) -> None:
-        raise NotImplementedError()
-
-    async def create_judge(self) -> None:
         raise NotImplementedError()
 
     async def update_judge(self, judge_id: str) -> None:
@@ -63,14 +67,19 @@ class AdminHandlers:
     async def delete_judge(self, judge_id: str) -> None:
         raise NotImplementedError()
 
-    # Hub Members
+
+# Hub Members Handlers
+class HubMembersHandlers(BaseException):
+    def __init__(self, service: HubMembersService) -> None:
+        self._service = service
+
+    async def create_hub_member(self) -> None:
+        raise NotImplementedError()
+
     async def list_hub_members(self) -> None:
         raise NotImplementedError()
 
     async def get_hub_member(self, member_id: str) -> None:
-        raise NotImplementedError()
-
-    async def create_hub_member(self) -> None:
         raise NotImplementedError()
 
     async def update_hub_member(self, member_id: str) -> None:
@@ -79,14 +88,19 @@ class AdminHandlers:
     async def delete_hub_member(self, member_id: str) -> None:
         raise NotImplementedError()
 
-    # Past Events
+
+# Past Events Handlers
+class PastEventsHandlers(BaseException):
+    def __init__(self, service: PastEventsService) -> None:
+        self._service = service
+
+    async def create_past_event(self) -> None:
+        raise NotImplementedError()
+
     async def list_past_events(self) -> None:
         raise NotImplementedError()
 
     async def get_past_event(self, event_id: str) -> None:
-        raise NotImplementedError()
-
-    async def create_past_event(self) -> None:
         raise NotImplementedError()
 
     async def update_past_event(self, event_id: str) -> None:
