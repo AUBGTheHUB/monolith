52e2c03cb557ab220366d2365a00ef8b135729d3
ivakalol <ivko0527@gmail.com>
2026-01-10 16:05:42 +0200

    Renamed admin-card.tsx -> adminCard.tsx

--- services/web/src/website/AdminPanelPage/DashboardPage/pages/JudgesPage/JudgesPage.tsx
+++ services/web/src/website/AdminPanelPage/DashboardPage/pages/JudgesPage/JudgesPage.tsx
@@ -2,7 +2,7 @@ import { Fragment, useState } from 'react';
 import { Link } from 'react-router';
 import { MOCK_JUDGES } from '@/lib/judges.mock';
 import { Judge } from '@/lib/types';
-import { AdminCard } from '@/components/ui/admin-card';
+import { AdminCard } from '@/components/ui/adminCard';
 import { Card } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { Helmet } from 'react-helmet';
