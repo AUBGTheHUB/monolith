acefcb80aa081b51f6a728a054eb3dfcb73475d0
ssoti-0 <ssotirov40@gmail.com>
2026-01-11 17:44:38 +0200

    Fix: Replace is_authorized with is_auth in all route files

--- services/py-api/src/server/routes/admin/past_events_routes.py
+++ services/py-api/src/server/routes/admin/past_events_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from src.server.routes.route_dependencies import is_authorized
+from src.server.routes.route_dependencies import is_auth
 
 from src.server.handlers.admin.past_events_handlers import PastEventsHandlers
 
@@ -8,22 +8,22 @@ def register_past_events_routes(http_handler: PastEventsHandlers) -> APIRouter:
     past_events_router = APIRouter(prefix="/past-events", tags=["past-events"])
 
     past_events_router.add_api_route(
-        "", endpoint=http_handler.get_all_past_events, methods=["GET"], dependencies=[Depends(is_authorized)]
+        "", endpoint=http_handler.get_all_past_events, methods=["GET"], dependencies=[Depends(is_auth)]
     )
     past_events_router.add_api_route(
-        "/{event_id}", endpoint=http_handler.get_past_event, methods=["GET"], dependencies=[Depends(is_authorized)]
+        "/{event_id}", endpoint=http_handler.get_past_event, methods=["GET"], dependencies=[Depends(is_auth)]
     )
     past_events_router.add_api_route(
-        "", endpoint=http_handler.create_past_event, methods=["POST"], dependencies=[Depends(is_authorized)]
+        "", endpoint=http_handler.create_past_event, methods=["POST"], dependencies=[Depends(is_auth)]
     )
     past_events_router.add_api_route(
-        "/{event_id}", endpoint=http_handler.update_past_event, methods=["PATCH"], dependencies=[Depends(is_authorized)]
+        "/{event_id}", endpoint=http_handler.update_past_event, methods=["PATCH"], dependencies=[Depends(is_auth)]
     )
     past_events_router.add_api_route(
         "/{event_id}",
         endpoint=http_handler.delete_past_event,
         methods=["DELETE"],
-        dependencies=[Depends(is_authorized)],
+        dependencies=[Depends(is_auth)],
     )
 
     return past_events_router
