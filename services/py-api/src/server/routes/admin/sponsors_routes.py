6c77c095de5dbb4d31c38555fc227251ff3a71e6
StoyanChorbov <stoyan.chorbov@protonmail.com>
2026-01-14 11:06:17 +0000

    Returned authentication requirement for routes

--- services/py-api/src/server/routes/admin/sponsors_routes.py
+++ services/py-api/src/server/routes/admin/sponsors_routes.py
@@ -8,20 +8,19 @@ def register_sponsor_routes(http_handler: SponsorsHandlers) -> APIRouter:
     sponsors_router = APIRouter(prefix="/sponsors", tags=["sponsors"])
 
     sponsors_router.add_api_route(
-        "", endpoint=http_handler.get_all_sponsors, methods=["GET"]
+        "", endpoint=http_handler.get_all_sponsors, methods=["GET"] , dependencies=[Depends(is_authorized)]
     )
     sponsors_router.add_api_route(
-        "/{sponsor_id}", endpoint=http_handler.get_sponsor, methods=["GET"]
+        "/{sponsor_id}", endpoint=http_handler.get_sponsor, methods=["GET"] , dependencies=[Depends(is_authorized)]
     )
     sponsors_router.add_api_route(
-        "", endpoint=http_handler.create_sponsor, methods=["POST"]
+        "", endpoint=http_handler.create_sponsor, methods=["POST"] , dependencies=[Depends(is_authorized)]
     )
     sponsors_router.add_api_route(
-        "/{sponsor_id}", endpoint=http_handler.update_sponsor, methods=["PATCH"]
+        "/{sponsor_id}", endpoint=http_handler.update_sponsor, methods=["PATCH"] , dependencies=[Depends(is_authorized)]
     )
     sponsors_router.add_api_route(
-        "/{sponsor_id}", endpoint=http_handler.delete_sponsor, methods=["DELETE"]
+        "/{sponsor_id}", endpoint=http_handler.delete_sponsor, methods=["DELETE"] , dependencies=[Depends(is_authorized)]
     )
-    # , dependencies=[Depends(is_authorized)]
 
     return sponsors_router
