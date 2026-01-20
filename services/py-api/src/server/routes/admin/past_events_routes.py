9bee848a1d6f721f95732f0d3cc92fc7f2847301
ssoti-0 <ssotirov40@gmail.com>
2026-01-20 11:17:40 +0200

    Fix past_events_routes to use is_auth and correct router

--- services/py-api/src/server/routes/admin/past_events_routes.py
+++ services/py-api/src/server/routes/admin/past_events_routes.py
@@ -2,16 +2,10 @@ from fastapi import APIRouter, Depends
 from src.server.routes.route_dependencies import is_auth
 
 from src.server.handlers.admin.past_events_handlers import PastEventsHandlers
-from src.server.routes.route_dependencies import is_authorized, validate_obj_id
-from src.server.schemas.response_schemas.admin.past_event_schemas import (
-    AllPastEventsResponse,
-    PastEventResponse,
-)
-from src.server.schemas.response_schemas.schemas import ErrResponse
 
 
 def register_past_events_routes(http_handler: PastEventsHandlers) -> APIRouter:
-    events_router = APIRouter(prefix="/events", tags=["events"])
+    past_events_router = APIRouter(prefix="/past-events", tags=["past-events"])
 
     past_events_router.add_api_route(
         "", endpoint=http_handler.get_all_past_events, methods=["GET"], dependencies=[Depends(is_auth)]
@@ -25,12 +19,11 @@ def register_past_events_routes(http_handler: PastEventsHandlers) -> APIRouter:
     past_events_router.add_api_route(
         "/{event_id}", endpoint=http_handler.update_past_event, methods=["PATCH"], dependencies=[Depends(is_auth)]
     )
-
-    events_router.add_api_route(
-        "/{object_id}",
+    past_events_router.add_api_route(
+        "/{event_id}",
         endpoint=http_handler.delete_past_event,
         methods=["DELETE"],
         dependencies=[Depends(is_auth)],
     )
 
-    return events_router
+    return past_events_router
