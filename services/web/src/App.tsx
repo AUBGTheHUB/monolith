8b3c519a7c7eebe772efb692a9e4c60663bd37ef
Aleks Ivanov <al.ivanov1908@gmail.com>
2026-01-24 00:08:19 +0200

    rework structure (#1172)

--- services/web/src/App.tsx
+++ services/web/src/App.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { MainPage } from './website/MainPage/MainPage';
-import { HackathonPage } from './website/HackathonPage7.0/HackathonPage';
+import { HackathonPage as Hackathon7 } from './website/HackathonPage7.0/HackathonPage';
 import { VerificationPage } from './website/VerificationPage/VerificationPage';
 import { FormPage } from './website/RegistrationFormPage/RegistrationFormPage';
 import { LoginPage } from './website/AdminPanelPage/LoginPage/LoginPage';
+import { HackathonPage } from './website/HackathonPage8.0/HackathonPage';
 
 function App() {
     const queryClient = new QueryClient();
@@ -13,6 +14,7 @@ function App() {
         <QueryClientProvider client={queryClient}>
             <Routes>
                 <Route path="/" element={<MainPage />} />
+                <Route path="/hackathon7.0" element={<Hackathon7 />} />
                 <Route path="/hackathon" element={<HackathonPage />} />
                 <Route path="/hackathon/registration" element={<FormPage />} />
                 <Route path="/hackathon/verification" element={<VerificationPage />} />
