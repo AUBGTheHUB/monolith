7c48c689187d83d3e3d7c0386b0143de6e4509d7
Stoyan Chorbov <56247845+StoyanChorbov@users.noreply.github.com>
2026-01-25 15:39:47 +0200

    Added crud for sponsors in the admin panel (#1150)
    * Added Sponsor model
    
    * Added sponsor handler
    
    * Updated sponsors repo
    
    * Added implementations for sponsor handlers
    
    * Fixed spelling mistakes and incorrect types
    
    * Returned authentication requirement for routes
    
    * Update
    
    * Added tests
    
    * Added missing session in delete
    
    * Added missing sessions
    
    * Fixed casting/unwraping
    
    * Returned commented test
    
    * Fixed wrong mock type

--- services/py-api/src/service/admin/sponsors_service.py
+++ services/py-api/src/service/admin/sponsors_service.py
@@ -1,25 +1,27 @@
 from result import Result, Err
 
-from src.database.model.admin.sponsor_model import Sponsor
+from src.database.model.admin.sponsor_model import Sponsor, UpdateSponsorParams
 from src.database.repository.admin.sponsors_repository import SponsorsRepository
 from src.server.schemas.request_schemas.admin.sponsor_schemas import SponsorPostReqData, SponsorPatchReqData
-
+from src.exception import SponsorNotFoundError
 
 class SponsorsService:
     def __init__(self, repo: SponsorsRepository) -> None:
         self._repo = repo
 
     async def get_all(self) -> Result[list[Sponsor], Exception]:
-        return Err(NotImplementedError())
+        return await self._repo.fetch_all()
 
-    async def get(self, sponsor_id: str) -> Result[Sponsor, Exception]:
-        return Err(NotImplementedError())
+    async def get(self, sponsor_id: str) -> Result[Sponsor, SponsorNotFoundError | Exception]:
+        return await self._repo.fetch_by_id(sponsor_id)
 
     async def create(self, data: SponsorPostReqData) -> Result[Sponsor, Exception]:
-        return Err(NotImplementedError())
+        sponsor = Sponsor(**data.model_dump())
+        return await self._repo.create(sponsor)
 
-    async def update(self, sponsor_id: str, data: SponsorPatchReqData) -> Result[Sponsor, Exception]:
-        return Err(NotImplementedError())
+    async def update(self, sponsor_id: str, data: SponsorPatchReqData) -> Result[Sponsor, SponsorNotFoundError | Exception]:
+        params = UpdateSponsorParams(**data.model_dump())
+        return await self._repo.update(sponsor_id, params)
 
-    async def delete(self, sponsor_id: str) -> Result[Sponsor, Exception]:
-        return Err(NotImplementedError())
+    async def delete(self, sponsor_id: str) -> Result[Sponsor, SponsorNotFoundError | Exception]:
+        return await self._repo.delete(sponsor_id)
