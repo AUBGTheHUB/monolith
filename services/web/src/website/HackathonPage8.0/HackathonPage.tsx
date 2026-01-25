0b32cbb7eca0aa2e063d15e6275296c427727b27
El-ina <elina.todorova2006@gmail.com>
2026-01-25 12:30:07 +0000

    Reverted the changes in routing

--- services/web/src/website/HackathonPage8.0/HackathonPage.tsx
+++
@@ -1,9 +0,0 @@
-import AwardsSection from './AwardsSection/AwardsSection';
-
-export const HackathonPageNew = () => {
-    return (
-        <div className="bg-[#000a12]">
-            <AwardsSection />
-        </div>
-    );
-};
