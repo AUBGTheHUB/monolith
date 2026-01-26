8a4f1bf35930507b20f718c83a86d8038e1e286d
El-ina <elina.todorova2006@gmail.com>
2026-01-26 14:32:04 +0000

    Restored HackathonPage.tsx to default

--- services/web/src/website/HackathonPage8.0/HackathonPage.tsx
+++ services/web/src/website/HackathonPage8.0/HackathonPage.tsx
@@ -1,21 +1,21 @@
-// import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet';
 import { AwardsSection } from './AwardsSection/AwardsSection';
-// import { FAQSection } from './FAQSection/FAQSection';
-// import { FooterSection } from './FooterSection/FooterSection';
-// import { GradingSection } from './GradingSection/GradingSection';
-// import { JourneySection } from './JourneySection/JourneySection';
-// import { LandingSection } from './LandingSection/LandingSection';
-// import { MentorsAndJudgesSection } from './MentorsAndJudgesSection/MentorsAndJudgesSection';
-// import { MissionSection } from './MissionSection/MissionSection';
-// import { NavigationSection } from './NavigationSection/NavigationSection';
-// import { RecapSection } from './RecapSection/RecapSection';
-// import { ScheduleSection } from './ScheduleSection/ScheduleSection';
-// import { SponsorsSection } from './SponsorsSection/SponsorsSection';
+import { FAQSection } from './FAQSection/FAQSection';
+import { FooterSection } from './FooterSection/FooterSection';
+import { GradingSection } from './GradingSection/GradingSection';
+import { JourneySection } from './JourneySection/JourneySection';
+import { LandingSection } from './LandingSection/LandingSection';
+import { MentorsAndJudgesSection } from './MentorsAndJudgesSection/MentorsAndJudgesSection';
+import { MissionSection } from './MissionSection/MissionSection';
+import { NavigationSection } from './NavigationSection/NavigationSection';
+import { RecapSection } from './RecapSection/RecapSection';
+import { ScheduleSection } from './ScheduleSection/ScheduleSection';
+import { SponsorsSection } from './SponsorsSection/SponsorsSection';
 
 export const HackathonPage = () => {
     return (
         <div className="bg-[#000a12]">
-            {/* <Helmet>
+            <Helmet>
                 <title>Hackathon 7.0</title>
                 <link rel="icon" href="/faviconHack.ico" />
                 <NavigationSection />
@@ -30,9 +30,7 @@ export const HackathonPage = () => {
                 <SponsorsSection />
                 <FAQSection />
                 <FooterSection />
-            </Helmet> */}
-
-            <AwardsSection />
+            </Helmet>
         </div>
     );
 };
