e7c4a2399144181510fcee2928762565c32eb749
Georgi Nikolov <gnnikoll@gmail.com>
2026-01-28 18:48:33 +0200

    Finish verticalLine component; fix bug in FAQSection

--- services/web/src/components/ui/verticalLine.tsx
+++ services/web/src/components/ui/verticalLine.tsx
@@ -1,3 +1,7 @@
-function VerticalLine(isRight: boolean, isBlack: boolean) {}
-
-export default VerticalLine;
+export const VerticalLine = ({ isRight, isBlack }: { isRight: boolean; isBlack: boolean }) => {
+    const color = isBlack ? 'bg-[#000000]' : 'bg-[#FFFDF5]';
+    const position = isRight ? 'right-[39px]' : 'left-[39px]';
+    return (
+        <div className={`pointer-events-none absolute top-0 bottom-0 ${position} w-[7px] ${color} hidden md:block`} />
+    );
+};
--- services/web/src/website/HackathonPage8.0/FAQSection/FAQSection.tsx
+++ services/web/src/website/HackathonPage8.0/FAQSection/FAQSection.tsx
@@ -1,26 +1,23 @@
 import { useState } from 'react';
 import { FAQItem } from './FAQItem';
 import { faqs } from './data';
+import { VerticalLine } from '@/components/ui/verticalLine.tsx';
 
 export const FAQSection = () => {
     const [openFaqId, setOpenFaqId] = useState<number | null>(null);
 
     return (
-        <section id="faq" className="relative w-full py-32 rounded-[20px] overflow-hidden bg-[#151313]">
+        <section id="faq" className="relative w-full min-h-dvh rounded-[20px] overflow-hidden bg-[#151313]">
             {/* Background image */}
             <div
                 className="absolute inset-0 bg-cover bg-center blur-[0.4px] opacity-[0.25]"
                 style={{ backgroundImage: "url('/rocksBG.png')" }}
             />
 
-            {/* LEFT vertical line */}
-            <div className="pointer-events-none absolute top-0 bottom-0 left-[39px] w-[7px] bg-[#FFFDF5] hidden md:block" />
-
-            {/* RIGHT vertical line */}
-            <div className="pointer-events-none absolute top-0 bottom-0 right-[39px] w-[7px] bg-[#FFFDF5] hidden md:block" />
-
+            <VerticalLine isRight={true} isBlack={false} />
+            <VerticalLine isRight={false} isBlack={false} />
             {/* Content */}
-            <div className="relative z-10 mx-20 max-w-[1400px] px-8">
+            <div className="relative z-10 mx-20 max-w-[1400px] px-8 py-10">
                 <div className="flex items-center gap-[5px]">
                     <img src="/yellow_icon.svg" alt="FAQ icon" className="h-[42px] w-auto" />
                     <h2 className="font-orbitron text-[40px] leading-[100%] text-[#FFFDF5]">FAQ</h2>
