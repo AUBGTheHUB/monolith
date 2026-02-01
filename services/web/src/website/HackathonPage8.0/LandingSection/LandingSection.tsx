47bec68f156307245fe02670286f5eedd55a2e72
Bubomira <libomirastaninska@abv.bg>
2026-02-01 10:03:54 +0000

    init

--- services/web/src/website/HackathonPage8.0/LandingSection/LandingSection.tsx
+++ services/web/src/website/HackathonPage8.0/LandingSection/LandingSection.tsx
@@ -1,3 +1,3 @@
 export const LandingSection = () => {
-    return <></>;
+    return <div className="bg-[rgba(26,28,25,1)] min-h-[80vh]"></div>;
 };
--- services/web/src/website/HackathonPage8.0/NavigationSection/NavigationSection.tsx
+++ services/web/src/website/HackathonPage8.0/NavigationSection/NavigationSection.tsx
@@ -1,3 +1,17 @@
 export const NavigationSection = () => {
-    return <></>;
+    return (
+        <div className={`w-full h-[10%] bg-[rgba(26,28,25,1)] border-gray-600 py-2 sticky top-0 z-[100]`}>
+            <div className="w-full flex flex-row justify-center items-center">
+                <div className="flex flex-row w-[70%] gap-7">
+                    <a href="#about">About</a>
+                    <a href="#schedule">Schedule</a>
+                    <a href="#grading-criteria">Grading Criteria</a>
+                    <a href="#faq">FAQ</a>
+                </div>
+                <div>
+                    <a href="/hackathon/registration">Participate now</a>
+                </div>
+            </div>
+        </div>
+    );
 };
