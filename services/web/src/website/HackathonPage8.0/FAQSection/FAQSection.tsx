706f9f641681d84a93d5a61c363ceae54aaeda73
Georgi Nikolov <gnnikoll@gmail.com>
2026-02-03 00:37:07 +0200

    Restored faq to initial state and fixed responsiveness

--- services/web/src/website/HackathonPage8.0/FAQSection/FAQSection.tsx
+++ services/web/src/website/HackathonPage8.0/FAQSection/FAQSection.tsx
@@ -7,23 +7,23 @@ export const FAQSection = () => {
     const [openFaqId, setOpenFaqId] = useState<number | null>(null);
 
     return (
-        <section id="faq" className="relative w-full min-h-dvh rounded-[20px] overflow-hidden bg-[#151313]">
+        <section id="faq" className="relative min-h-dvh w-full py-28 rounded-[20px] overflow-hidden bg-[#151313]">
             {/* Background image */}
             <div
                 className="absolute inset-0 bg-cover bg-center blur-[0.4px] opacity-[0.25]"
                 style={{ backgroundImage: "url('/rocksBG.png')" }}
             />
 
-            <VerticalBar isRight={true} isBlack={false} />
             <VerticalBar isRight={false} isBlack={false} />
+            <VerticalBar isRight={true} isBlack={false} />
             {/* Content */}
-            <div className="relative z-10 mx-20 max-w-[1400px] px-8 py-12">
+            <div className="relative z-10 mx-auto max-w-full px-8 md:px-44">
                 <div className="flex items-center gap-[5px]">
                     <img src="/yellow_icon.svg" alt="FAQ icon" className="h-[42px] w-auto" />
                     <h2 className="font-orbitron text-[40px] leading-[100%] text-[#FFFDF5]">FAQ</h2>
                 </div>
 
-                <div className="mt-24">
+                <div className="pt-20">
                     {faqs.map((faq) => (
                         <FAQItem
                             key={faq.id}
