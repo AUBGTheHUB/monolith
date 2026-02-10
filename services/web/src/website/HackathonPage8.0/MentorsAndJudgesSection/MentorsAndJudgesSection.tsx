a0934993e426908eb5c2334c4d56960a355d9096
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-02-10 09:11:07 +0000

    feat(1157): create page

--- services/web/src/website/HackathonPage8.0/MentorsAndJudgesSection/MentorsAndJudgesSection.tsx
+++ services/web/src/website/HackathonPage8.0/MentorsAndJudgesSection/MentorsAndJudgesSection.tsx
@@ -1,3 +1,23 @@
+import redPin from './assets/red-pin.svg';
+import yellowPin from './assets/yellow-pin.svg';
+import { Carousel } from './components/Caroussel';
+
 export const MentorsAndJudgesSection = () => {
-    return <></>;
+    return (
+        <section
+            style={{
+                backgroundImage: "url('/rocksBG.png')",
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                backgroundRepeat: 'no-repeat',
+                backgroundColor: 'rgba(0,0,0,0.5)', // apply 0.5 opacity without affecting child elements
+                backgroundBlendMode: 'overlay',
+            }}
+            className="text-white w-full py-28 rounded-[20px] overflow-hidden flex flex-col items-center justify-center gap-[100px]"
+        >
+            <Carousel title="MENTORS" imgSrc={yellowPin} />
+
+            <Carousel title="JUDGES" imgSrc={redPin} />
+        </section>
+    );
 };
