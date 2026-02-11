5d41ba330e94b445071234c6d1c2f8390319fdee
Georgi Nikolov <gnnikoll@gmail.com>
2026-02-11 19:25:14 +0200

    Add roles to routes

--- services/py-api/src/server/routes/admin/mentors_routes.py
+++ services/py-api/src/server/routes/admin/mentors_routes.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends
-
+from src.database.model.admin.hub_admin_model import Role
 from src.server.handlers.admin.mentor_handlers import MentorsHandlers
-from src.server.routes.route_dependencies import is_authorized, validate_obj_id
+from src.server.routes.auth.role_checker import RoleChecker
+from src.server.routes.route_dependencies import validate_obj_id
 from src.server.schemas.response_schemas.admin.mentor_schemas import MentorResponse, MentorsResponse
 from src.server.schemas.response_schemas.schemas import ErrResponse
-from src.server.routes.route_dependencies import is_authorized
 
 
 def register_mentors_routes(http_handler: MentorsHandlers) -> APIRouter:
@@ -32,7 +32,7 @@ def register_mentors_routes(http_handler: MentorsHandlers) -> APIRouter:
         endpoint=http_handler.create_mentor,
         methods=["POST"],
         responses={201: {"model": MentorResponse}, 400: {"model": ErrResponse}, 401: {"model": ErrResponse}},
-        dependencies=[Depends(is_authorized)],
+        dependencies=[Depends(RoleChecker([Role.BOARD]))],
     )
     mentors_router.add_api_route(
         path="/{object_id}",
@@ -44,7 +44,7 @@ def register_mentors_routes(http_handler: MentorsHandlers) -> APIRouter:
             401: {"model": ErrResponse},
             404: {"model": ErrResponse},
         },
-        dependencies=[Depends(is_authorized), Depends(validate_obj_id)],
+        dependencies=[Depends(RoleChecker([Role.BOARD])), Depends(validate_obj_id)],
     )
     mentors_router.add_api_route(
         path="/{object_id}",
@@ -56,7 +56,7 @@ def register_mentors_routes(http_handler: MentorsHandlers) -> APIRouter:
             401: {"model": ErrResponse},
             404: {"model": ErrResponse},
         },
-        dependencies=[Depends(is_authorized), Depends(validate_obj_id)],
+        dependencies=[Depends(RoleChecker([Role.BOARD])), Depends(validate_obj_id)],
     )
 
     return mentors_router
--- services/py-api/src/server/routes/admin/sponsors_routes.py
+++ services/py-api/src/server/routes/admin/sponsors_routes.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
-
+from src.database.model.admin.hub_admin_model import Role
 from src.server.handlers.admin.sponsors_handlers import SponsorsHandlers
-from src.server.routes.route_dependencies import is_authorized, validate_obj_id
+from src.server.routes.auth.role_checker import RoleChecker
+from src.server.routes.route_dependencies import validate_obj_id
 from src.server.schemas.response_schemas.admin.sponsor_schemas import SponsorResponse, SponsorsResponse
 from src.server.schemas.response_schemas.schemas import ErrResponse
 
@@ -19,7 +20,6 @@ def register_sponsor_routes(http_handler: SponsorsHandlers) -> APIRouter:
         responses={
             200: {"model": SponsorResponse},
             400: {"model": ErrResponse},
-            401: {"model": ErrResponse},
             404: {"model": ErrResponse},
         },
         dependencies=[Depends(validate_obj_id)],
@@ -34,7 +34,7 @@ def register_sponsor_routes(http_handler: SponsorsHandlers) -> APIRouter:
             401: {"model": ErrResponse},
             404: {"model": ErrResponse},
         },
-        dependencies=[Depends(is_authorized)],
+        dependencies=[Depends(RoleChecker([Role.BOARD]))],
     )
     sponsors_router.add_api_route(
         path="/{object_id}",
@@ -46,7 +46,7 @@ def register_sponsor_routes(http_handler: SponsorsHandlers) -> APIRouter:
             401: {"model": ErrResponse},
             404: {"model": ErrResponse},
         },
-        dependencies=[Depends(is_authorized), Depends(validate_obj_id)],
+        dependencies=[Depends(RoleChecker([Role.BOARD])), Depends(validate_obj_id)],
     )
     sponsors_router.add_api_route(
         path="/{object_id}",
@@ -58,7 +58,7 @@ def register_sponsor_routes(http_handler: SponsorsHandlers) -> APIRouter:
             401: {"model": ErrResponse},
             404: {"model": ErrResponse},
         },
-        dependencies=[Depends(is_authorized), Depends(validate_obj_id)],
+        dependencies=[Depends(RoleChecker([Role.BOARD])), Depends(validate_obj_id)],
     )
 
     return sponsors_router
