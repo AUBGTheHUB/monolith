eee64cdd7363d7f42d8c92194a1d37aa6dc49788
Georgi Nikolov <gnnikoll@gmail.com>
2026-02-15 15:53:43 +0200

    Fix visual glitch

--- services/web/src/website/HackathonPage8.0/LandingSection/LandingSection.tsx
+++ services/web/src/website/HackathonPage8.0/LandingSection/LandingSection.tsx
@@ -28,17 +28,17 @@ export default function LandingSection() {
             <div className="h-full flex justify-center items-center">
                 <div className="font-orbitron z-10">
                     <div className="flex flex-col items-center justify-center text-center gap-1">
-                        <p className="text-sm xs:text-lg md:text-3xl tracking-[0.3em] font-semibold text-[rgb(49,46,46)]">
+                        <p className="text-sm xs:text-lg md:text-3xl tracking-[0.3em] font-semibold text-[rgb(49,46,46)] transform-gpu">
                             THE HUB&apos;S
                         </p>
-                        <h1 className="text-6xl bg-clip-text text-transparent bg-[radial-gradient(circle_at_50%_0%,_#FBC42C_0.1%,_#F6741C_50%,_#D91313_100%)] font-extrabold drop-shadow-md md:text-8xl">
+                        <h1 className="text-6xl bg-clip-text text-transparent bg-[radial-gradient(circle_at_50%_0%,_#FBC42C_0.1%,_#F6741C_50%,_#D91313_100%)] font-extrabold drop-shadow-md md:text-8xl  transform-gpu">
                             HACK
                         </h1>
                         <div className="flex flex-col sm:flex-row sm:gap-5 ">
-                            <h2 className="text-6xl bg-clip-text text-transparent bg-[radial-gradient(circle_at_70%_0%,_#F6741C_0.1%,_#D91313_60%,_#AA1616_100%)] font-extrabold drop-shadow-md md:text-8xl">
+                            <h2 className="text-6xl bg-clip-text text-transparent bg-[radial-gradient(circle_at_70%_0%,_#F6741C_0.1%,_#D91313_60%,_#AA1616_100%)] font-extrabold drop-shadow-md md:text-8xl  transform-gpu">
                                 AUBG
                             </h2>
-                            <h2 className="text-6xl bg-clip-text text-transparent bg-[radial-gradient(circle_at_0%_0%,_#D91313_60%,_#AA1616_70%)] font-extrabold drop-shadow-md md:text-8xl">
+                            <h2 className="text-6xl bg-clip-text text-transparent bg-[radial-gradient(circle_at_0%_0%,_#D91313_60%,_#AA1616_70%)] font-extrabold drop-shadow-md md:text-8xl  transform-gpu">
                                 8.0
                             </h2>
                         </div>
