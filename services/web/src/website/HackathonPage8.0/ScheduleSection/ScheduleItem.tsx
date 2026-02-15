981602d275a5e2a147dca7020897d09b578eb73a
El-ina <elina.todorova2006@gmail.com>
2026-02-15 13:31:16 +0000

    Fixed table rows' font sizes

--- services/web/src/website/HackathonPage8.0/ScheduleSection/ScheduleItem.tsx
+++ services/web/src/website/HackathonPage8.0/ScheduleSection/ScheduleItem.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { ScheduleItemProps } from './types';
+import './ScheduleItem.css';
 
 export const ScheduleItem: React.FC<ScheduleItemProps> = ({ time, event, isLast }) => (
     <div className={!isLast ? 'mb-[1rem] md:mb-[1.5rem]' : ''}>
         <div className="flex justify-between items-center py-[0.75rem] md:py-[1.25rem] text-white">
-            <span className="text-[1rem] md:text-[1.4rem] lg:text-[1.6rem] font-oxanium leading-[1.25]">{event}</span>
-            <span className="text-[1rem] md:text-[1.4rem] lg:text-[1.6rem] font-oxanium font-bold leading-[1.25]">
-                {time}
-            </span>
+            <span className="font-oxanium leading-[1.25] schedule-item-text">{event}</span>
+            <span className="font-oxanium font-bold leading-[1.25] schedule-item-text">{time}</span>
         </div>
         {!isLast && (
             <div
