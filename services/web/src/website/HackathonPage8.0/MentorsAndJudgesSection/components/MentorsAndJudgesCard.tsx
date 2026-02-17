cf821bda5f2ef9619df91c53152f9d29005777f0
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-02-17 16:34:42 +0000

    fix(1157): fix carousel slides

--- services/web/src/website/HackathonPage8.0/MentorsAndJudgesSection/components/MentorsAndJudgesCard.tsx
+++ services/web/src/website/HackathonPage8.0/MentorsAndJudgesSection/components/MentorsAndJudgesCard.tsx
@@ -1,18 +1,25 @@
 import { cn } from '@/lib/utils';
 
-export function MentorsAndJudgesCard({ className }: { className?: string }) {
-    /* TODO:
-    - add props for the mentor/judge's name, title, company, and picture
-    - add a background image for the card (from BE)
-    */
+interface MentorsAndJudgesCardProps {
+    name: string;
+    company: string;
+    avatar_url: string;
+    job_title?: string;
+    className?: string;
+    isLoading?: boolean;
+}
 
+export function MentorsAndJudgesCard({ className, name, job_title, company, avatar_url }: MentorsAndJudgesCardProps) {
     return (
         <div className={cn('relative h-[295px] w-[255px] rounded-3xl border border-white overflow-hidden', className)}>
-            <img src="/schedule_bg.webp" className="object-fill" />
+            <img src={avatar_url} className="object-fill" />
 
             <div className="absolute right-0 bottom-[42px] pl-[16px] bg-[#1B1919] opacity-91 w-[230px] h-[80px] flex flex-col items-start justify-center">
-                <span className="font-orbitron text-[18px] tracking-widest font-semibold">John Doe</span>
-                <span className="font-oxanium text-[12px] text-[#FBAF45]">CEO of Company | SoftServe</span>
+                <span className="font-orbitron text-[18px] tracking-widest font-semibold">{name}</span>
+                <span className="font-oxanium text-[12px] text-[#FBAF45]">
+                    {job_title ? `${job_title} | ` : ''}
+                    {company}
+                </span>
             </div>
         </div>
     );
