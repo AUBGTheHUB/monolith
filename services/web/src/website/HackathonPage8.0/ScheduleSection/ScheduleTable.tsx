35d439eda4d7538abca7cf08b0a0865b1baec845
Elina Malchinikolova <elina.todorova2006@gmail.com>
2026-02-17 10:37:44 +0200

    #1158 Implemented Hackathon Schedule Section (#1189)
    * Added font imports (WILL BE REMOVED)
    
    * Edited HackathonPage.tsx for testing purposes (WILL BE REMOVED)
    
    * Title section finished
    
    * The table now looks exactly like the one from the Figma design
    
    * Made the component responsive
    
    * Splitted the ScheduleSection component into microcomponents (Header, Tabs (these are just the weekdays btns), Table); created a config file
    
    * Fixed a border radius discrepancy in the buttons
    
    * Changed the file structure to match the established one and reverted HackathonPage.tsx to its original state
    
    * Renamed the files as per the new structure and removed the vertical lines implementation
    
    * Moved the subcomponents to ScheduleItem.tsx
    
    * Implemented the vertical lines component
    
    * Fix vague import to work on gh action
    
    * Move library import to /web
    
    * Fix global package.json
    
    * Moved the schedule items' types to types.ts
    
    * Changed the design so that it's closer to Figma
    
    * Removed the border and the shadow from the header
    
    * Edited the buttons' colors and border thickness so as to better match the Figma design
    
    * Simplified tabs.tsx
    
    * Fixed schedule header sizing and alignment
    
    * Edited the buttons row so that their dimensions and borders match the Figma design
    
    * Fixed the buttons' dimensions
    
    * Redid the sizings
    
    * Scaled font sizes down... again
    
    * Splitted components into separate files
    
    * Fixed table rows' font sizes
    
    * Made the tabs' font size the same as the table rows
    
    * Made table width consistent with the mission item's width
    
    * Items alignment is now the same as the Mission section
    
    * Fixed table width
    
    * Fixed the responsive layout and changed the background color
    
    * Brought the separate inline styles to the ScheduleStyle.css file
    
    * Styles file now style.css
    
    ---------
    
    Co-authored-by: Georgi Nikolov <gnnikoll@gmail.com>

---
+++ services/web/src/website/HackathonPage8.0/ScheduleSection/ScheduleTable.tsx
@@ -0,0 +1,26 @@
+import { TabsContent } from '@/components/ui/tabs';
+import { scheduleData } from './data';
+import { ScheduleItem } from './ScheduleItem';
+import { Day } from './types';
+import './style.css';
+
+export const ScheduleTable = () => (
+    <>
+        {(['Friday', 'Saturday', 'Sunday'] as Day[]).map((day) => (
+            <TabsContent key={day} value={day} className="w-full">
+                <div className="rounded-[1rem] md:rounded-[1.25rem] w-full schedule-table">
+                    <div className="px-[1.5rem] md:px-[2.25rem] py-[1rem] md:py-[1.313rem]">
+                        {scheduleData[day].map((event, index) => (
+                            <ScheduleItem
+                                key={index}
+                                time={event.time}
+                                event={event.event}
+                                isLast={index === scheduleData[day].length - 1}
+                            />
+                        ))}
+                    </div>
+                </div>
+            </TabsContent>
+        ))}
+    </>
+);
