21533e1b10b5289894d51e4a81a222d6925fd5db
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-02-17 16:35:16 +0000

    feature(1157): add live data

--- services/web/src/website/HackathonPage8.0/MentorsAndJudgesSection/components/Caroussel/index.tsx
+++ services/web/src/website/HackathonPage8.0/MentorsAndJudgesSection/components/Caroussel/index.tsx
@@ -1,22 +1,24 @@
-import type { EmblaOptionsType } from 'embla-carousel';
-import useEmblaCarousel from 'embla-carousel-react';
-import * as React from 'react';
 import { cn } from '@/lib/utils';
+import { Judge } from '@/types/judge';
+import { Mentor } from '@/types/mentor';
+import type { EmblaOptionsType } from 'embla-carousel';
 import ClassNames from 'embla-carousel-class-names';
+import useEmblaCarousel from 'embla-carousel-react';
+import { useCallback, useEffect, useState } from 'react';
 import { CarousselButton } from '../CarousselButton';
 import { MentorsAndJudgesCard } from '../MentorsAndJudgesCard';
 import './index.css';
 
-export const OPTIONS: EmblaOptionsType = {
+const OPTIONS: EmblaOptionsType = {
     loop: false,
     align: 'center',
     breakpoints: {
-        '(max-width: 600px)': {
+        '(max-width: 800px)': {
             // Small screens (Mobile)
             align: 'center',
             slidesToScroll: 1,
         },
-        '(min-width: 600px) and (max-width: 1500px)': {
+        '(min-width: 800px) and (max-width: 1500px)': {
             // Medium screens (Tablets)
             align: 'center',
             slidesToScroll: 'auto',
@@ -29,24 +31,23 @@ export const OPTIONS: EmblaOptionsType = {
     },
 };
 
-type CarouselProps = {
+type CarouselProps<T extends Mentor | Judge> = {
     title: string;
     imgSrc: string;
     className?: string;
-    slides?: number;
+    data?: T[];
 };
 
-export function Carousel({ className, title, slides = 12, imgSrc }: CarouselProps) {
+export function Carousel<T extends Mentor | Judge>({ className, title, imgSrc, data }: CarouselProps<T>) {
     const [emblaRef, emblaApi] = useEmblaCarousel(OPTIONS, [ClassNames()]);
 
-    const goToPrev = React.useCallback(() => emblaApi?.scrollPrev(), [emblaApi]);
-    const goToNext = React.useCallback(() => emblaApi?.scrollNext(), [emblaApi]);
+    const goToPrev = useCallback(() => emblaApi?.scrollPrev(), [emblaApi]);
+    const goToNext = useCallback(() => emblaApi?.scrollNext(), [emblaApi]);
 
-    // Optional: keep buttons in sync with embla state (works even with loop=true)
-    const [canPrev, setCanPrev] = React.useState(false);
-    const [canNext, setCanNext] = React.useState(false);
+    const [canPrev, setCanPrev] = useState(false);
+    const [canNext, setCanNext] = useState(false);
 
-    React.useEffect(() => {
+    useEffect(() => {
         if (!emblaApi) return;
 
         const update = () => {
@@ -65,13 +66,13 @@ export function Carousel({ className, title, slides = 12, imgSrc }: CarouselProp
     }, [emblaApi]);
 
     return (
-        <div className={cn('embla flex justify-center flex-col gap-8 w-full sm:px-16 md:px-28 xl:px-40', className)}>
+        <div className={cn('embla flex justify-center flex-col gap-8 w-[80%]', className)}>
             {/* HEADER */}
 
             <div className="flex flex-col sm:flex-row items-center justify-between">
                 <div className="flex flex-row items-center gap-[18px]">
-                    <img src={imgSrc} alt="Judges Pin" className="h-[42px] w-auto" />
-                    <span className="text-[40px] font-orbitron tracking-widest">{title}</span>
+                    <img src={imgSrc} alt="Hunger Games Pin" className="h-[42px] w-auto" />
+                    <span className="text-[40px] font-orbitron tracking-[0.3em]">{title}</span>
                 </div>
 
                 <div className="hidden sm:flex justify-center gap-3">
@@ -82,17 +83,23 @@ export function Carousel({ className, title, slides = 12, imgSrc }: CarouselProp
 
             {/* CAROUSEL */}
 
-            <div className="embla__viewport" ref={emblaRef}>
-                <div className="embla__container sm:w-[600px] md:w-[900px] lg:w-[1100px]">
-                    {Array.from({ length: slides }).map(
-                        (
-                            _,
-                            idx, // TODO: replace after real data becomes available
-                        ) => (
-                            <div className="embla__slide" key={idx}>
-                                <MentorsAndJudgesCard />
+            <div className={cn('embla__viewport')} ref={emblaRef}>
+                <div className="embla__container">
+                    {data ? (
+                        data?.map((element, idx) => (
+                            <div
+                                className={cn(
+                                    'embla__slide',
+                                    'ease-linear duration-200',
+                                    !data ? 'opacity-0' : 'opacity-100',
+                                )}
+                                key={idx}
+                            >
+                                <MentorsAndJudgesCard {...element} isLoading={!!data} />
                             </div>
-                        ),
+                        ))
+                    ) : (
+                        <MentorsAndJudgesCard name="Loading..." company="" avatar_url="" />
                     )}
                 </div>
             </div>
