aef045dc6352e9cffe2bd54f3c0141c5faa6e997
Toma Bourov <63297306+babajaga3@users.noreply.github.com>
2026-02-23 12:38:44 +0000

    feature(1253): update types

--- services/web/src/types/hub-member.ts
+++ services/web/src/types/hub-member.ts
@@ -1,12 +1,39 @@
-export interface HubMember {
-    id: string;
-    name: string;
-    position: string;
-    departments: string[];
-    avatar_url: string;
-    social_links: {
-        linkedin?: string | null;
-        github?: string | null;
-        website?: string | null;
-    };
+import { z } from 'zod';
+import { zDateTime } from './utils';
+
+export enum Departments {
+    All = 'All',
+    Board = 'Board',
+    PR = 'PR',
+    Design = 'Design',
+    Development = 'Development',
+    Marketing = 'Marketing',
+    Logistics = 'Logistics',
 }
+
+export const zBaseHubMember = z.strictObject({
+    id: z.string(),
+    name: z.string(),
+    position: z.string(),
+    departments: z.array(z.nativeEnum(Departments)),
+    avatar_url: z.string(),
+    social_links: z.strictObject({
+        linkedin: z.string().url().optional(),
+        github: z.string().url().optional(),
+        website: z.string().url().optional(),
+    }),
+});
+
+export type BaseHubMember = z.infer<typeof zBaseHubMember>;
+
+export const zHubMember = zBaseHubMember.extend({
+    id: z.string(),
+    username: z.string(),
+    member_type: z.string(), // todo add enum once known
+    position: z.string(), // todo add enum once known
+    site_role: z.string(), // todo add enum once known
+    created_at: zDateTime,
+    updated_at: zDateTime,
+})
+
+export type HubMember = z.infer<typeof zHubMember>;
