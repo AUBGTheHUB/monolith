22a161ac817c94f87fbab7531ce70edf0d28df69
ivakalol <ivko0527@gmail.com>
2026-02-24 19:42:17 +0200

    Checkboxes -> circle + centered

--- services/web/src/components/ui/radio-group.tsx
+++ services/web/src/components/ui/radio-group.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as RadioGroupPrimitive from '@radix-ui/react-radio-group';
-import { Check } from 'lucide-react';
+import { Circle } from 'lucide-react';
 
 import { cn } from '@/lib/utils';
 
@@ -20,13 +20,13 @@ const RadioGroupItem = React.forwardRef<
         <RadioGroupPrimitive.Item
             ref={ref}
             className={cn(
-                'aspect-square h-4 w-4 rounded-sm border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
+                'aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 flex items-center justify-center',
                 className,
             )}
             {...props}
         >
             <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-                <Check className="h-3 w-3 text-current" strokeWidth={3} />
+                <Circle className="h-2.5 w-2.5 fill-current text-current" />
             </RadioGroupPrimitive.Indicator>
         </RadioGroupPrimitive.Item>
     );
