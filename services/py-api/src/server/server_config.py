3ed4a7d93977774d5c4085e99cf7f0dc93cad389
Georgi Nikolov <gnnikoll@gmail.com>
2026-02-28 16:49:26 +0200

    revert unneccesarry change

--- services/py-api/src/server/server_config.py
+++ services/py-api/src/server/server_config.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from pathlib import Path
-
+from os import cpu_count
 from structlog.stdlib import get_logger
 from uvicorn import run
 
@@ -63,5 +63,5 @@ def start_server() -> None:
         # https://stackoverflow.com/questions/65278110/how-does-gunicorn-distribute-requests-across-sync-workers
         # As cpu_count could return None we use 0 instead, as 2 * None would produce an error
         # Also "workers" flag is ignored when reloading is enabled (It is ignored for LOCAL env)
-        workers=5,
+        workers=2 * (cpu_count() or 0) + 1,
     )
