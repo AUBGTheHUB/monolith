aa466a4cf5b2aa45cfae92112c420eb0d45900cc
Georgi Nikolov <gnnikoll@gmail.com>
2026-03-03 17:15:06 +0200

    Revert zod models

--- services/web/src/types/hub-member.ts
+++ services/web/src/types/hub-member.ts
@@ -1,25 +1,12 @@
-import { z } from 'zod';
-
-export enum Departments {
-    All = 'All',
-    PR = 'PR',
-    Design = 'Design',
-    Development = 'Development',
-    Marketing = 'Marketing',
-    Logistics = 'Logistics',
+export interface HubMember {
+    id: string;
+    name: string;
+    position: string;
+    departments: string[];
+    avatar_url: string;
+    social_links: {
+        linkedin?: string | null;
+        github?: string | null;
+        website?: string | null;
+    };
 }
-
-export const zBaseHubMember = z.strictObject({
-    id: z.string(),
-    name: z.string(),
-    position: z.string(),
-    departments: z.array(z.nativeEnum(Departments)),
-    avatar_url: z.string(),
-    social_links: z.strictObject({
-        linkedin: z.string().url().optional(),
-        github: z.string().url().optional(),
-        website: z.string().url().optional(),
-    }),
-});
-
-export type BaseHubMember = z.infer<typeof zBaseHubMember>;
