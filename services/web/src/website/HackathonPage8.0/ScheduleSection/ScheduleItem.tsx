276cf25e6251215ad9127edd247c969043ff6828
Georgi Nikolov <gnnikoll@gmail.com>
2026-03-08 22:44:44 +0200

    Fix schedule design and update data

--- services/web/src/website/HackathonPage8.0/ScheduleSection/ScheduleItem.tsx
+++ services/web/src/website/HackathonPage8.0/ScheduleSection/ScheduleItem.tsx
@@ -3,8 +3,8 @@ import { ScheduleItemProps } from './types';
 import './style.css';
 
 export const ScheduleItem: React.FC<ScheduleItemProps> = ({ time, event, isLast }) => (
-    <div className={!isLast ? 'mb-[1rem] md:mb-[1.5rem]' : ''}>
-        <div className="flex justify-between items-center py-[0.75rem] md:py-[1.25rem] text-white">
+    <div className={!isLast ? 'mb-[0.75rem] md:mb-[1rem]' : ''}>
+        <div className="flex justify-between items-center py-[0.5rem] md:py-[0.9rem] text-white">
             <span className="font-oxanium leading-[1.25] table-row-text">{event}</span>
             <span className="font-oxanium font-bold leading-[1.25] table-row-text">{time}</span>
         </div>
