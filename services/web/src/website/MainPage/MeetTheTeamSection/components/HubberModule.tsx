96da10660233dad0711efb527b215abb2fafce50
Georgi Nikolov <gnnikoll@gmail.com>
2026-03-11 01:17:48 +0200

    add background

--- services/web/src/website/MainPage/MeetTheTeamSection/components/HubberModule.tsx
+++ services/web/src/website/MainPage/MeetTheTeamSection/components/HubberModule.tsx
@@ -17,12 +17,27 @@ export default function HubberModule({ imgSrc, name }: HubberModuleType) {
             )}
 
             <div
-                className={`flex flex-col min-w-28 max-w-80 max-[1122px]:max-w-full h-70 p-2 border-[1px] rounded-[15px] border-[rgb(255,255,255,0.2)] font-semibold transition-opacity duration-500 ease-in-out ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
+                className={`flex flex-col min-w-28 max-w-80 max-[1122px]:max-w-full h-auto p-2 border-[1px] rounded-[15px] border-[rgb(255,255,255,0.2)] font-semibold transition-opacity duration-500 ease-in-out ${
+                    isLoaded ? 'opacity-100' : 'opacity-0'
+                }`}
             >
-                <div className="h-[170px] w-full">
-                    <img src={imgSrc} alt={name} className="object-cover object-top w-full h-full rounded-xl" />
+                {/* Responsive Height:
+                    - h-[240px] on mobile (taller/narrower look)
+                    - md:h-[170px] on tablets and desktops (original look)
+                */}
+                <div className="relative h-[240px] md:h-[170px] w-full overflow-hidden rounded-xl">
+                    {/* Background SVG - scaled up to cover all gaps */}
+                    <img
+                        src="/meetTheTeam/hubber-bg.svg"
+                        alt=""
+                        className="absolute inset-0 w-full h-full object-cover scale-110 -z-10"
+                    />
+
+                    {/* Main Portrait */}
+                    <img src={imgSrc} alt={name} className="relative z-10 object-cover object-top w-full h-full" />
                 </div>
-                <div className="h-1/6 py-3 gap-2">
+
+                <div className="py-3 px-1">
                     <h3 className="text-secondary line-clamp-1">{name}</h3>
                 </div>
             </div>
