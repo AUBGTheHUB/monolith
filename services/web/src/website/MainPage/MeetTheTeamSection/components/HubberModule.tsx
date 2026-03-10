ffb6da949d4c9327ec9605acf9b5c54d1b55cea8
Georgi Nikolov <gnnikoll@gmail.com>
2026-03-10 23:30:34 +0200

    Object top

--- services/web/src/website/MainPage/MeetTheTeamSection/components/HubberModule.tsx
+++ services/web/src/website/MainPage/MeetTheTeamSection/components/HubberModule.tsx
@@ -20,7 +20,7 @@ export default function HubberModule({ imgSrc, name }: HubberModuleType) {
                 className={`flex flex-col min-w-28 max-w-80 max-[1122px]:max-w-full h-70 p-2 border-[1px] rounded-[15px] border-[rgb(255,255,255,0.2)] font-semibold transition-opacity duration-500 ease-in-out ${isLoaded ? 'opacity-100' : 'opacity-0'}`}
             >
                 <div className="h-[170px] w-full">
-                    <img src={imgSrc} alt={name} className="object-contain w-full h-full rounded-xl" />
+                    <img src={imgSrc} alt={name} className="object-cover object-top w-full h-full rounded-xl" />
                 </div>
                 <div className="h-1/6 py-3 gap-2">
                     <h3 className="text-secondary line-clamp-1">{name}</h3>
